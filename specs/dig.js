9ae18d479b3b49ec7d75a977b0ee3dc0ffd5c30c
Dipak Parmar <hi@dipak.tech>
2020-10-23 23:12:18 -0700

    Added MX command autocomplete for dig

--- specs/dig.js
+++ specs/dig.js
@@ -1,70 +1,82 @@
 var completionSpec = {
-    name: "dig",
-    description: "Domain Information Groper",
-    subcommands: [
-      {
-        name: "A",
-        description: "Query Domain A Record",
-        insertValue: "A ",
-        options: [
-          {
-            name: ["+short", ""],
-            insertValue: "+short {cursor}",
-            description: "only print meaningful results",
-            args: {},
-          },
-        ],
-      },
-      {
-        name: "NS",
-        description: "Query MX Record",
-        insertValue: "NS ",
-        options: [
-          {
-            name: ["+short", ""],
-            insertValue: "+short {cursor}",
-            description: "only print meaningful results",
-            args: {},
-          },
-        ],
-      },
-      {
-        name: "SOA",
-        description: "Query SOA Record",
-        insertValue: "SOA ",
-        options: [
-          {
-            name: ["+short", ""],
-            insertValue: "+short {cursor}",
-            description: "only print meaningful results",
-            args: {},
-          },
-        ],
-      },
-      {
-        name: "TTL",
-        description: "Query TTL Record",
-        insertValue: "TTL ",
-        options: [
-          {
-            name: ["+short", ""],
-            insertValue: "+short {cursor}",
-            description: "only print meaningful results",
-            args: {},
-          },
-        ],
-      },
-      {
-        name: "ANY +noall +answer",
-        description: "Query ALL DNS Records",
-        insertValue: "ANY +noall +answer ",
-      },
-      {
-        name: "+nocomments +noquestion +noauthority +noadditional +nostats",
-        description: "Query only answer section",
-        insertValue:
-          "+nocomments +noquestion +noauthority +noadditional +nostats ",
-      },
-    ],
-  };
-  
+  name: "dig",
+  description: "Domain Information Groper",
+  subcommands: [
+    {
+      name: "A",
+      description: "Query Domain A Record",
+      insertValue: "A ",
+      options: [
+        {
+          name: ["+short", ""],
+          insertValue: "+short {cursor}",
+          description: "only print meaningful results",
+          args: {},
+        },
+      ],
+    },
+    {
+      name: "MX",
+      description: "Query Domain MX Record",
+      insertValue: "MX ",
+      options: [
+        {
+          name: ["+short", ""],
+          insertValue: "+short {cursor}",
+          description: "only print meaningful results",
+          args: {},
+        },
+      ],
+    },
+    {
+      name: "NS",
+      description: "Query MX Record",
+      insertValue: "NS ",
+      options: [
+        {
+          name: ["+short", ""],
+          insertValue: "+short {cursor}",
+          description: "only print meaningful results",
+          args: {},
+        },
+      ],
+    },
+    {
+      name: "SOA",
+      description: "Query SOA Record",
+      insertValue: "SOA ",
+      options: [
+        {
+          name: ["+short", ""],
+          insertValue: "+short {cursor}",
+          description: "only print meaningful results",
+          args: {},
+        },
+      ],
+    },
+    {
+      name: "TTL",
+      description: "Query TTL Record",
+      insertValue: "TTL ",
+      options: [
+        {
+          name: ["+short", ""],
+          insertValue: "+short {cursor}",
+          description: "only print meaningful results",
+          args: {},
+        },
+      ],
+    },
+    {
+      name: "ANY +noall +answer",
+      description: "Query ALL DNS Records",
+      insertValue: "ANY +noall +answer ",
+    },
+    {
+      name: "+nocomments +noquestion +noauthority +noadditional +nostats",
+      description: "Query only answer section",
+      insertValue:
+        "+nocomments +noquestion +noauthority +noadditional +nostats ",
+    },
+  ],
+};
