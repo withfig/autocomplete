f5b675e244468df5d39cabd80c93fe3ced948af8
Brendan Falk <brendan@withfig.com>
2020-12-28 22:34:39 -0800

    remove firebase stuff and cleaned up

--- specs/cargo.js
+++ specs/cargo.js
@@ -1,7 +1,6 @@
 var completionSpec = {
   name: "cargo",
   description: "CLI Interface for Cargo",
-  args: {},
   subcommands: [
     {
       name: "build",
@@ -55,83 +54,12 @@ var completionSpec = {
         {
           name: ["--offline"],
           description: "run without network",
-          args: {},
         },
       ],
       args: {
         template: "files",
       },
-    },
-    {
-      name: "deploy",
-      description: "deploy project to firebase server",
-      options: [
-        {
-          name: ["-h", "--help"],
-          description: "output usage info",
-        },
-        {
-          name: ["-p", "--public"],
-          insertValue: "-p {cursor}",
-          description:
-            "override the Hosting public directory specified in firebase.json",
-          args: {},
-        },
-        {
-          name: ["-m", "--message"],
-          insertValue: "-m {cursor}",
-          description: "use the message",
-          args: {},
-        },
-        {
-          name: ["-f", "--force"],
-          description:
-            "delete Cloud Functions missing from the current working directory without confirmation",
-          args: {},
-        },
-        {
-          name: ["--only"],
-          insertValue: "--only {cursor}",
-          description:
-            "only serve specified targets (valid targets are: hosting, functions)",
-          args: {},
-        },
-        {
-          name: ["--except"],
-          insertValue: "--except {cursor}",
-          description:
-            "serve all except specified targets (valid targets are: hosting, functions)",
-          args: {},
-        },
-      ],
-    },
-    {
-      name: "b",
-      description: "compile local package and dependencies",
-      options: [
-        {
-          name: ["-h", "--help"],
-          description: "output usage info",
-        },
-        {
-          name: ["--bins"],
-          description: "build all binaries",
-        },
-        {
-          name: ["--all-targets"],
-          description: "activate all available features",
-        },
-        {
-          name: ["--all"],
-          description: "alias for workspace",
-        },
-        {
-          name: ["-j, --jobs"],
-          description: "number of CPUS",
-          insertValue: "-j {cursor}",
-        },
-      ],
-    },
+    }
   ],
   options: [
     {
