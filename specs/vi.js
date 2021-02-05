8933ca66fefd3bd3cd8bd9fe4058535d36c2324d
Gerard de Brieder <smeevil@me.com>
2021-02-05 16:51:35 +0100

    Add vi.js to autocomplete

---
+++ specs/vi.js
@@ -0,0 +1,9 @@
+var completionSpec = {
+
+    name: "vi",
+    description: "Vi[m] is an one of two powerhouse text editors in the Unix world, the other being EMACS",
+    args: {
+        template: "filepaths"
+    },
+    subcommands: []
+}
