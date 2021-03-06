f92fda49439da9a4ae954dfdb0fa01cc1421682b
Roland <rolandtshen@gmail.com>
2021-03-05 17:04:14 -0800

    add ruby specs

---
+++ specs/rake.js
@@ -0,0 +1,59 @@
+var completionSpec = {
+    name: 'rake',
+    description: 'a ruby build program with capabilities similar to make',
+    args: {
+      name: "targets",
+      variadic: true
+    },
+    options: [
+      {
+        name: [ '-n' , '--dry-run' ],
+        description: 'Do a dry run without executing actions.'
+      },
+      {
+        name: [ '-H', '--help' ],
+        description: 'Display this help message.'
+      },
+      {
+        name: [ '-I', '--libdir=LIBDIR' ],
+        description: 'Include LIBDIR in the search path for required modules.'
+      },
+      {
+        name: [ '-P', '--prereqs' ],
+        description: 'Display the tasks and dependencies, then exit.'
+      },
+      {
+        name: [ '-q', '--quiet' ],
+        description: 'Do not log messages to standard output'
+      },
+      {
+        name: [ '-f', '--rakefile=FILE' ],
+        description: 'Use FILE as the rakefile.'
+      },
+      {
+        name: [ '-r', '--require=MODULE' ],
+        description: 'Require MODULE before executing rakefile.'
+      },
+      {
+        name: [ '-s', '--silent' ],
+        description: "Like --quiet, but also suppresses the 'in directory' announcement."
+      },
+      {
+        name: [ '-T', '--tasks' ],
+        description: 'Display the tasks and dependencies, then exit.'
+      },
+      {
+        name: [ '-t', '--trace' ],
+        description: 'Turn on invoke/execute tracing, enable full backtrace.'
+      },
+      { name: [ '-h', '--usage' ], description: 'Display usage.' },
+      {
+        name: [ '-v', '--verbose' ],
+        description: 'Log message to standard output (default).'
+      },
+      {
+        name: [ '-V','--version' ],
+        description: 'Display the program version.'
+      }
+    ]
+  }
---
+++ specs/rbenv.js
@@ -0,0 +1,84 @@
+var completionSpec = {
+    name: 'rbenv',
+    description: '',
+    subcommands: [
+      {
+        name: 'commands',
+        description: 'List all available rbenv commands',
+        options: [
+          {
+            name: ["--sh"]
+          },
+          {
+            name: ["--no-sh"]
+          }
+        ],
+        subcommands: []
+      },
+      {
+        name: 'global',
+        description: 'Set or show the global Ruby version',
+        options: [],
+        subcommands: [],
+        args: {}
+      },
+      {
+        name: 'install',
+        description: 'Install a Ruby version using ruby-build',
+        options: [
+          {
+            name: [ '--version' ],
+            description: 'Show version of ruby-build ',
+            args: {}
+          }
+        ],
+        subcommands: []
+      },
+      {
+        name: 'local',
+        description: 'Set or show the local application-specific Ruby version',
+        options: [],
+        subcommands: []
+      },
+      {
+        name: 'rehash',
+        description: 'Rehash rbenv shims (run this after installing executables)',
+        options: [],
+        subcommands: []
+      },
+      { name: 'shell', description: 'Set or show the shell-specific Ruby version', subcommands: [] },
+      {
+        name: 'uninstall',
+        description: 'Uninstall a specific Ruby version',
+        options: [
+          {
+            name: [ '-f' ],
+            description: 'If the version does not exist, do not display an error message. ',
+            args: {}
+          }
+        ],
+        subcommands: []
+      },
+      {
+        name: 'versions',
+        description: 'List installed Ruby versions',
+        options: [],
+        subcommands: []
+      },
+      {
+        name: 'whence',
+        description: 'List all Ruby versions that contain the given executable',
+        options: [],
+        subcommands: []
+      },
+      {
+        name: 'which',
+        description: 'Display the full path to an executable',
+        options: [],
+        subcommands: [],
+        args: [
+          { name: "command" }
+        ]
+      }
+    ]
+  }
