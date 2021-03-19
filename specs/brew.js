aa79b925bda768adf9a6ce3048def1399246d8f0
vicentube <appeleando@gmail.com>
2021-03-18 23:29:52 -0400

    Change brew install subcommand argument generator

--- specs/brew.js
+++ specs/brew.js
@@ -34,7 +34,7 @@ var completionSpec = {
                 name: "formula",
                 description: "Formula or cask to install",
                 generators: {
-                    script: "ls -1 /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks",
+                    script: "homebrew_path=$(brew --prefix); ls -1 $homebrew_path/Library/Taps/homebrew/homebrew-core/Formula $homebrew_path/Library/Taps/homebrew/homebrew-cask/Casks",
                     postProcess: function (out) {
 
                         return out.split('\n').map(formula => {
@@ -49,7 +49,6 @@ var completionSpec = {
                 }
             }
         },
-
         {
             name: "uninstall",
             description: "Uninstall <formula>",
