acba6136dc6bc76f025ddda1a3ad8e64d5561b0c
Tim Raderschad <tim.raderschad@gmail.com>
2021-03-19 20:48:57 +0100

    feat: setup lint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+    rules: {
+        // Add some rules here
+    },
+};
