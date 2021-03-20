74907eb32792149d638f8744c9b95a23a7c810db
Tom <quiibzdev@gmail.com>
2021-03-20 17:44:53 +0100

    Update schemas/fig.d.ts
    Co-authored-by: Tim Raderschad <tim.raderschad@gmail.com>

--- schemas/fig.d.ts
+++ schemas/fig.d.ts
@@ -27,7 +27,7 @@ declare namespace Fig {
          * The text that’s rendered in each row of the dropdown.
          *
          * @remarks
-         * Fig uses the name prop for parsing purposes. It is important the name
+         * Fig uses the name prop for parsing purposes. It is important that the name
          * prop exactly matches the CLI tool. If you want to customise it what is
          * says in the dropdown, please use `displayName`.
          */
