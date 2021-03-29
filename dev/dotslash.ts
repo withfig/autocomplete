ffa31f41fe8a33583a2639b4aef6f97387379659
Tim Raderschad <tim.raderschad@gmail.com>
2021-03-29 22:18:04 +0200

    feat: reset specs and dev folder

--- dev/dotslash.ts
+++
@@ -1,8 +0,0 @@
-// use to complete on files when they are the first string in a branch
-export const dotslashCompletionSpec: Fig.Spec = {
-    name: 'dotslash',
-    description: 'Change the shell working directory',
-    args: {
-        template: 'filepaths',
-    },
-};
