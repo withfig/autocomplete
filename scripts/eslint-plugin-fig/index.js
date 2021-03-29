791ceae50d749e95fa885ec7f742a84f2dc94261
Tim Raderschad <tim.raderschad@gmail.com>
2021-03-29 20:47:11 +0200

    feat: add custom eslint rules

---
+++ scripts/eslint-plugin-fig/index.js
@@ -0,0 +1,60 @@
+const rmRfRegexp = /rm \-(?:f(?:(?: \-)?r)?|r(?: \-f|f)?)/;
+
+module.exports = {
+    rules: {
+        'no-malicious-script': {
+            meta: {
+                type: 'problem',
+            },
+            create: function (context) {
+                return {
+                    Property(node) {
+                        if (node.key.name === 'script') {
+                            const scriptValue = node.value.value;
+                            if (rmRfRegexp.test(scriptValue)) {
+                                context.report({
+                                    node,
+                                    message: 'No Malicious Script allowed',
+                                });
+                            }
+                        }
+                    },
+                };
+            },
+        },
+        'no-name-equals': {
+            meta: {
+                type: 'problem',
+                fixable: 'code',
+            },
+            create: function (context) {
+                return {
+                    Property(node) {
+                        if (node.key.name === 'name') {
+                            const value = node.value;
+                            if (value.type === 'Literal' && value.value.endsWith('=')) {
+                                context.report({
+                                    node,
+                                    message: 'The name property must not include `=`',
+                                    fix: function (fixer) {
+                                        const [, end] = value.range;
+                                        return fixer.replaceTextRange([end - 2, end - 1], '');
+                                    },
+                                });
+                            }
+                        }
+                    },
+                };
+            },
+        },
+    },
+    configs: {
+        recommended: {
+            plugins: ['fig'],
+            rules: {
+                'fig/no-malicious-script': 'error',
+                'fig/no-name-equals': 'error',
+            },
+        },
+    },
+};
