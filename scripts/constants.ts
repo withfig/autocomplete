af49d2d4bdd7da8c7e359614fd1638e60f41f045
Tom <quiibzdev@gmail.com>
2021-03-31 16:19:18 +0200

    fix: Set boilerplace spec prettier/eslint compliant

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -3,6 +3,6 @@ export const SOURCE_FOLDER_NAME = "dev";
 export const DESTINATION_FOLDER_NAME = "specs";
 
 export const getBoilerplateSpecContent = (specName: string) => `
-export const completion: Fig.Spec =  {
-   name: "${specName}"
-}`;
+export const completion: Fig.Spec = {
+  name: "${specName}"
+};`;
