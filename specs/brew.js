fbf84798d94ea651ec78da5887a2907394e1f680
Vicente Úbeda <appeleando@gmail.com>
2021-04-06 01:13:35 -0400

    Fix search path in brew install subcommand generator

--- specs/brew.js
+++ specs/brew.js
@@ -51,7 +51,7 @@ var completionSpec = {
                 name: "formula",
                 description: "Formula or cask to install",
                 generators: {
-                    script: "ls -1 /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks",
+                    script: "HBPATH=$(brew --repository); ls -1 $HBPATH/Library/Taps/homebrew/homebrew-core/Formula $HBPATH/Library/Taps/homebrew/homebrew-cask/Casks",
                     postProcess: function (out) {
                         return out.split("\n").map(function (formula) {
                             return {
