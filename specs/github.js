aa886a134b49072f958e3b51fffa5fa7354b9029
Tim Raderschad <tim.raderschad@gmail.com>
2021-04-09 21:27:19 +0200

    feat: migrate to typescript

--- specs/github.js
+++ specs/github.js
@@ -1,61 +1,62 @@
 var completionSpec = {
-  name: "github",
-  description: "Open a git repository in GitHub Desktop",
-  args: {
-    name: "path",
-    isOptional: true,
-    generators: { template: "folders" },
-  },
-  options: [
-    {
-      name: "--help",
-      description: "Show the help page for a command",
-      args: {
-        name: "command",
-        isOptional: true,
-        suggestions: ["clone", "open", "help"],
-      },
-    },
-  ],
-  subcommands: [
-    {
-      name: "clone",
-      description: "Clone a repository",
-      insertValue: "clone ",
-      args: {
-        name: "url|slug",
-        suggestions: [
-          { name: "https://github.com/", icon: "fig://icon?type=git" },
-          { name: "https://gitlab.com/", icon: "fig://icon?type=git" },
-          { name: "https://bitbucket.com/", icon: "fig://icon?type=git" },
-        ],
-      },
-      options: [
-        {
-          name: ["--branch", "-b"],
-          description: "The branch to checkout after cloning",
-          args: { name: "branch" },
-        },
-      ],
-    },
-    {
-      name: "open",
-      insertValue: "open ",
-      description: "Open a git repository in GitHub Desktop",
-      args: {
+    name: "github",
+    description: "Open a git repository in GitHub Desktop",
+    args: {
         name: "path",
         isOptional: true,
         generators: { template: "folders" },
-      },
     },
-    {
-      name: "help",
-      description: "Show the help page for a command",
-      args: {
-        name: "command",
-        isOptional: true,
-        suggestions: ["clone", "open", "help"],
-      },
-    },
-  ],
+    options: [
+        {
+            name: "--help",
+            description: "Show the help page for a command",
+            args: {
+                name: "command",
+                isOptional: true,
+                suggestions: ["clone", "open", "help"],
+            },
+        },
+    ],
+    subcommands: [
+        {
+            name: "clone",
+            description: "Clone a repository",
+            insertValue: "clone ",
+            args: {
+                name: "url|slug",
+                suggestions: [
+                    { name: "https://github.com/", icon: "fig://icon?type=git" },
+                    { name: "https://gitlab.com/", icon: "fig://icon?type=git" },
+                    { name: "https://bitbucket.com/", icon: "fig://icon?type=git" },
+                ],
+            },
+            options: [
+                {
+                    name: ["--branch", "-b"],
+                    description: "The branch to checkout after cloning",
+                    args: { name: "branch" },
+                },
+            ],
+        },
+        {
+            name: "open",
+            insertValue: "open ",
+            description: "Open a git repository in GitHub Desktop",
+            args: {
+                name: "path",
+                isOptional: true,
+                generators: { template: "folders" },
+            },
+        },
+        {
+            name: "help",
+            description: "Show the help page for a command",
+            args: {
+                name: "command",
+                isOptional: true,
+                suggestions: ["clone", "open", "help"],
+            },
+        },
+    ],
 };
+
