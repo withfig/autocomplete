c72a5f9f8d0c5b053366fa24484a069a279afd0a
Tim Raderschad <tim.raderschad@gmail.com>
2021-04-10 15:08:05 +0200

    test

--- specs/vi.js
+++
@@ -1,9 +0,0 @@
-var completionSpec = {
-    name: "vi",
-    description: "Vi[m] is an one of two powerhouse text editors in the Unix world, the other being EMACS",
-    args: {
-        template: "filepaths",
-    },
-    subcommands: [],
-};
-
