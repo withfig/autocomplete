bc0bc445ff39d609845ee02329215fe1e7bc8189
Tim Raderschad <tim.raderschad@gmail.com>
2021-04-10 16:30:44 +0200

    feat: build specs on master merge

--- specs/cd.js
+++
@@ -1,9 +0,0 @@
-var completionSpec = {
-    name: "cd",
-    description: "Change the shell working directory.",
-    args: {
-        template: "folders",
-        // suggestions: [{ name: "-", description: "switch to the last used folder" }],
-    },
-};
-
