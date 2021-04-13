685976d519ae11db24d82a5f35b94bb644ab6171
Tim Raderschad <tim.raderschad@gmail.com>
2021-04-13 16:51:02 +0200

    feat: add invalid name chars rule

--- scripts/eslint-plugin-fig-linter/index.js
+++ scripts/eslint-plugin-fig-linter/index.js
@@ -3,6 +3,7 @@ module.exports = {
     "no-malicious-script": require("./rules/no-malicious-script"),
     "no-name-equals": require("./rules/no-name-equals"),
     "no-invalid-option": require("./rules/no-invalid-option"),
+    "no-invalid-name": require("./rules/no-invalid-name"),
   },
   configs: {
     recommended: {
@@ -11,6 +12,7 @@ module.exports = {
         "fig-linter/no-malicious-script": "error",
         "fig-linter/no-name-equals": "error",
         "fig-linter/no-invalid-option": "error",
+        "fig-linter/no-invalid-name": "error",
       },
     },
   },
