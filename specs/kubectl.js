73d47ca036dfbec15b973c0216229026092439b7
Brendan Falk <brendan@withfig.com>
2021-04-13 17:57:56 -0700

    updated python

--- specs/kubectl.js
+++ specs/kubectl.js
@@ -4086,7 +4086,7 @@ var completionSpec = {
                             args: {},
                         },
                         {
-                            name: ["--wanker"],
+                            name: ["--template"],
                             description: "Template string or path to template file to use when -o=go-template, -o=go-template-file. The template format is golang templates [http://golang.org/pkg/text/template/#pkg-overview].",
                             args: {},
                         },
