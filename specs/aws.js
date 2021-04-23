391ea8b232445e8efc3b28a46c095fec51f8237d
Kevin Marker <kmarker@gmail.com>
2021-04-23 11:14:01 -0500

    wip

---
+++ specs/aws.js
@@ -0,0 +1,39 @@
+var instances = {
+	script:
+		"aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId[]' --output text",
+	postProcess: function (out) {
+		if (out.startsWith("fatal")) {
+			return [];
+		}
+		return out.split("\t").map((instance) => {
+			return { name: instance.trim(), description: "instance" };
+		});
+	},
+};
+
+var completionSpec = {
+	name: "aws",
+	description: "aws cli commands",
+	subcommands: [
+		{
+			name: "ec2",
+			description: "ec2 commands",
+			subcommands: [
+				{
+					name: "start-instances",
+					description: "Starts an EC2 instance",
+					options: [
+						{
+							name: "--instance-ids",
+							insertValue: "--instance-ids",
+							description: "instance ids",
+						},
+					],
+					args: {
+						generators: instances,
+					},
+				},
+			],
+		},
+	],
+};
