d96ed3c28f05337ec4bddf5752b300a5321e370e
Kevin Marker <kmarker@gmail.com>
2021-04-25 13:32:28 -0500

    added icons and refactor

--- dev/aws.ts
+++ dev/aws.ts
@@ -1,13 +1,17 @@
 const awsGenerators: Record<string, Fig.Generator> = {
   instances: {
     script:
-      "aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId[]' --output text",
+      "aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].InstanceId[]' --output text",
     postProcess: function (out) {
       if (out.startsWith("fatal")) {
         return [];
       }
       return out.split("\t").map((instance) => {
-        return { name: instance.trim(), description: "instance" };
+        return {
+          name: instance.trim(),
+          icon: "fig://icon?type=aws",
+          description: "instance",
+        };
       });
     },
   },
@@ -15,25 +19,29 @@ const awsGenerators: Record<string, Fig.Generator> = {
 
 export const completionSpec: Fig.Spec = {
   name: "aws",
+  icon: "fig://icon?type=aws",
   description: "aws cli commands",
   subcommands: [
     {
       name: "ec2",
+      icon: "fig://icon?type=aws",
       description: "ec2 commands",
       subcommands: [
         {
           name: "start-instances",
+          icon: "fig://icon?type=aws",
           description: "Starts an EC2 instance",
           options: [
             {
               name: "--instance-ids",
+              icon: "fig://icon?type=aws",
               insertValue: "--instance-ids",
               description: "instance ids",
+              args: {
+                generators: awsGenerators.instances,
+              },
             },
           ],
-          args: {
-            generators: awsGenerators.instances,
-          },
         },
       ],
     },
--- specs/aws.js
+++ specs/aws.js
@@ -1,37 +1,45 @@
 var awsGenerators = {
     instances: {
-        script: "aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId[]' --output text",
+        script: "aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].InstanceId[]' --output text",
         postProcess: function (out) {
             if (out.startsWith("fatal")) {
                 return [];
             }
             return out.split("\t").map(function (instance) {
-                return { name: instance.trim(), description: "instance" };
+                return {
+                    name: instance.trim(),
+                    icon: "fig://icon?type=aws",
+                    description: "instance",
+                };
             });
         },
     },
 };
 var completionSpec = {
     name: "aws",
+    icon: "fig://icon?type=aws",
     description: "aws cli commands",
     subcommands: [
         {
             name: "ec2",
+            icon: "fig://icon?type=aws",
             description: "ec2 commands",
             subcommands: [
                 {
                     name: "start-instances",
+                    icon: "fig://icon?type=aws",
                     description: "Starts an EC2 instance",
                     options: [
                         {
                             name: "--instance-ids",
+                            icon: "fig://icon?type=aws",
                             insertValue: "--instance-ids",
                             description: "instance ids",
+                            args: {
+                                generators: awsGenerators.instances,
+                            },
                         },
                     ],
-                    args: {
-                        generators: awsGenerators.instances,
-                    },
                 },
             ],
         },
