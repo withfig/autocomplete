f67a5b5edc3d7eaef24f736e3207d81c5463bd71
Matt Schrage <matthewschrage@gmail.com>
2021-04-28 21:25:00 -0700

    fix: trim whitespace from make targets

--- dev/make.ts
+++ dev/make.ts
@@ -5,7 +5,7 @@ const listTargets: Fig.Generator = {
     const targets = [];
     for (let i = 1; i < lines.length; i++) {
       targets.push({
-        name: lines[i],
+        name: lines[i].trim(),
         description: "make target",
         icon: "🎯",
         priority: 80,
