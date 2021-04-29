1a09891e0ed2f1b634b3d32b8300c169ff2fb698
Tom <quiibzdev@gmail.com>
2021-04-29 20:23:31 +0200

    feat: Add Next.js spec

---
+++ specs/next.js
@@ -0,0 +1,114 @@
+var completionSpec = {
+    name: "next",
+    description: "Next.js CLI to start, build and export your application",
+    icon: "https://nextjs.org/static/favicon/favicon-16x16.png",
+    options: [
+        {
+            name: ["-h", "--help"],
+            description: "Output usage information",
+        },
+        {
+            name: ["-v", "--version"],
+            description: "Output the version number",
+        },
+    ],
+    subcommands: [
+        {
+            name: "build",
+            description: "Create an optimized production build of your application",
+            args: [
+                {
+                    name: "dir",
+                    description: "Represent the directory of the Next.js application",
+                    template: "folders",
+                },
+            ],
+            options: [
+                {
+                    name: "--profile",
+                    description: "Enable production profiling",
+                },
+                {
+                    name: "--debug",
+                    description: "Enable more verbose build output",
+                },
+            ],
+        },
+        {
+            name: "dev",
+            description: "Start the application in development mode",
+            args: [
+                {
+                    name: "dir",
+                    description: "Represent the directory of the Next.js application",
+                    template: "folders",
+                },
+                {
+                    name: "-p",
+                    description: "A port number on which to start the application",
+                },
+                {
+                    name: "-H",
+                    description: "Hostname on which to start the application",
+                },
+            ],
+        },
+        {
+            name: "start",
+            description: "Start the application in production mode",
+            args: [
+                {
+                    name: "dir",
+                    description: "Represent the directory of the Next.js application",
+                    template: "folders",
+                },
+                {
+                    name: "-p",
+                    description: "A port number on which to start the application",
+                },
+                {
+                    name: "-H",
+                    description: "Hostname on which to start the application",
+                },
+            ],
+        },
+        {
+            name: "export",
+            description: "Exports the application for production deployment",
+            args: [
+                {
+                    name: "dir",
+                    description: "Represent the directory of the Next.js application",
+                    template: "folders",
+                },
+            ],
+            options: [
+                {
+                    name: "-s",
+                    description: "Do not print any messages to console",
+                },
+            ],
+        },
+        {
+            name: "telemetry",
+            description: "Allows you to control Next.js' telemetry collection",
+            args: [
+                {
+                    name: "status",
+                    description: "Turn Next.js' telemetry collection on or off",
+                    suggestions: [
+                        {
+                            name: "enable",
+                            description: "Enable Next.js' telemetry collection",
+                        },
+                        {
+                            name: "disable",
+                            description: "Disable Next.js' telemetry collection",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
