43e4977b09de76ea186bce76b001832ef556b342
dereklowlind <dereklowlind@gmail.com>
2021-04-30 09:55:07 -0700

    added args and removed a set of unneeded square brackets for readability

--- specs/ln.js
+++ specs/ln.js
@@ -1,19 +1,43 @@
 var completionSpec = {
     name: "ln",
-    description: "Create (hard) symbolic links to files",
-    args: {},
+    description: "Create (default hard) symbolic links to files",
+    args: [
+        {
+            name: "source_file",
+            // template: ["filepaths", "folders"], // is there an absolute path version of this? ln requires absolute path
+        },
+        {
+            name: "link_name or link_dirname",
+        },
+    ],
     options: [
         {
             name: "-s",
             description: "Create a symbolic link",
+            args: [
+                {
+                    name: "source_file",
+                },
+                {
+                    name: "link_name or link_dirname",
+                },
+            ],
         },
         {
             name: "-v",
             description: "verbose",
         },
         {
-            name: ["-F"],
+            name: "-F",
             description: "If link name already exists replace it",
+            args: [
+                {
+                    name: "source_file",
+                },
+                {
+                    name: "link_name or link_dirname",
+                },
+            ],
         },
         {
             name: "-h",
@@ -22,10 +46,26 @@ var completionSpec = {
         {
             name: "-f",
             description: "If link name already exists unlink the old one before creating the new one",
+            args: [
+                {
+                    name: "source_file",
+                },
+                {
+                    name: "link_name or link_dirname",
+                },
+            ],
         },
         {
             name: "-i",
             description: "Prompt if proposed link already exists",
+            args: [
+                {
+                    name: "source_file",
+                },
+                {
+                    name: "link_name or link_dirname",
+                },
+            ],
         },
         {
             name: "-n",
