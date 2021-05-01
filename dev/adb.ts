92faab1d2304116f5722b50abe8aec1e70e9f9fb
Alex Naiman <alex.naiman.4@gmail.com>
2021-05-01 17:06:12 +0300

    removed boilerplate comments

--- dev/adb.ts
+++ dev/adb.ts
@@ -1,6 +1,3 @@
-// To learn more about FIg's autocomplete standard visit: https://withfig.com/docs/autocomplete/building-a-spec#building-your-first-autocomplete-spec
-
-// The below is a dummy example for git. Make sure to change the file name!
 export const completion: Fig.Spec = {
   name: "adb",
   description: "Android Debug Bridge",
