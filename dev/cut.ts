10249d223a8bb6f12247318c6db43c28ff69ad00
Colwin Fernandes <colwinfernandes@gmail.com>
2021-05-05 13:27:16 +0530

    Added specs for the cut command

---
+++ dev/cut.ts
@@ -0,0 +1,76 @@
+export const completionSpec: Fig.Spec = {
+  name: "cut",
+  description: "cut out selected portions of each line of a file",
+  args: {
+    template: "filepaths",
+    isOptional: true,
+    variadic: true,
+  },
+  subcommands: [
+    {
+      name: ["-b"],
+      description: "byte positions as a comma or - separated list of numbers",
+      args: [
+        {
+          name: "list",
+          description: "specifies byte positions",
+        },
+        {
+          name: "-n",
+          description: "do not split multi-byte characters",
+          isOptional: true,
+        },
+        {
+          name: "file",
+          template: "filepaths",
+          isOptional: true,
+          variadic: true,
+        },
+      ],
+    },
+    {
+      name: ["-c"],
+      description: "column positions as a comma or - separated list of numbers",
+      args: [
+        {
+          name: "list",
+          description: "specifies column positions",
+        },
+        {
+          name: "file",
+          template: "filepaths",
+          isOptional: true,
+          variadic: true,
+        },
+      ],
+    },
+    {
+      name: ["-f"],
+      description: "field positions as a comma or - separated list of numbers",
+      args: [
+        {
+          name: "list",
+          description: "specifies column positions",
+        },
+        {
+          name: "-d",
+          description:
+            "use delim as the field delimiter character instead of the tab character.",
+          isOptional: true,
+        },
+        {
+          name: "-s",
+          description:
+            "suppress lines with no field delimiter characters.  unless specified, lines with no delimiters are passed through unmodified.",
+          isOptional: true,
+        },
+        {
+          name: "file",
+          template: "filepaths",
+          isOptional: true,
+          variadic: true,
+        },
+      ],
+    },
+  ],
+};
