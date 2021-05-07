7cf928407dba3d5866ad2601743b282e30b55529
Tom <quiibzdev@gmail.com>
2021-05-07 13:31:08 +0200

    refactor: Improve jest spec

--- specs/jest.js
+++ specs/jest.js
@@ -60,13 +60,14 @@ var completionSpec = {
             description: "The path to a Jest config file specifying how to find and execute tests",
             args: {
                 name: "path",
+                template: "filepaths",
             },
         },
         {
             name: ["--coverage"],
             displayName: "--coverage=<boolean>",
             insertValue: "--coverage=",
-            description: "The path to a Jest config file specifying how to find and execute tests",
+            description: "Enable or disable coverage, disabled by default",
             args: {
                 name: "true|false",
                 suggestions: [
@@ -104,6 +105,7 @@ var completionSpec = {
             description: "The test environment used for all tests",
             args: {
                 name: "jsdom|node|path/to/env.js",
+                template: "filepaths",
                 suggestions: [{ name: "jsdom" }, { name: "node" }],
             },
         },
@@ -204,6 +206,8 @@ var completionSpec = {
             description: "Run tests from one or more projects, found in the specified paths; also takes path globs",
             args: {
                 name: "<path1> ... <pathN>",
+                variadic: true,
+                template: "filepaths",
             },
         },
         {
@@ -221,6 +225,8 @@ var completionSpec = {
             description: "A list of paths to directories that Jest should use to search for files in",
             args: {
                 name: "<path1> ... <pathN>",
+                variadic: true,
+                template: "folders",
             },
         },
         {
@@ -246,6 +252,7 @@ var completionSpec = {
             description: "The path to a module that runs some code to configure or set up the testing framework before each test",
             args: {
                 name: "file",
+                template: "filepaths",
             },
         },
         {
@@ -294,6 +301,7 @@ var completionSpec = {
             description: "Lets you specify a custom test runner",
             args: {
                 name: "path",
+                template: "filepaths",
             },
         },
         {
@@ -303,6 +311,7 @@ var completionSpec = {
             description: "Lets you specify a custom test sequencer",
             args: {
                 name: "path",
+                template: "filepaths",
             },
         },
         {
