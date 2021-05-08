918be095b9fdaf5a3909c6239854bbc5d32f4db0
CastaChick <casta46chick@gmail.com>
2021-05-08 22:35:26 +0900

    feat: Add commonly used options of sudo #176

--- dev/sudo.ts
+++ dev/sudo.ts
@@ -1,8 +1,29 @@
 export const completion: Fig.Spec = {
   name: "sudo",
-  description: "",
+  description: "execute a command as the superuser or another user",
   subcommands: [],
-  options: [],
+  options: [
+    {
+      name: ["-g"],
+      description: "run command as the specified group name or ID",
+      args: {
+        name: "group",
+        description: "group name or ID",
+      },
+    },
+    {
+      name: ["-h"],
+      description: "display help message and exit",
+    },
+    {
+      name: ["-u"],
+      description: "run command as specified user name or ID",
+      args: {
+        name: "user",
+        description: "user name or ID",
+      },
+    },
+  ],
   // Only uncomment if sudo takes an argument
   args: {
     name: "command",
