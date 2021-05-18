aed61e491fbbd5a9d41c03dcc85a9089ed80ab10
Tim Raderschad <tim.raderschad@gmail.com>
2021-05-18 21:42:14 +0200

    feat: remove insertValue for git add

--- dev/git.ts
+++ dev/git.ts
@@ -158,7 +158,6 @@ const gitGenerators: Record<string, Fig.Generator> = {
 
         return {
           name: file,
-          insertValue: file.includes(" ") ? `'${file}'` : file,
           icon: `fig://icon?type=${ext}&color=ff0000&badge=${item.working}`,
           description: "Changed file",
           priority: 100,
@@ -1816,9 +1815,7 @@ export const completionSpec: Fig.Spec = {
           description:
             "Show the changes recorded in the stash entry as a diff.",
           insertValue: "show {cursor}",
-          options: [
-            // TODO: All log options can be options from list. Needs to be added.
-          ],
+
           args: [
             {
               name: "stash",
@@ -1888,9 +1885,6 @@ export const completionSpec: Fig.Spec = {
           name: "list",
           description: "Lists all stashed changesets",
           insertValue: "list {cursor}",
-          options: [
-            // TODO: All log options can be options from list. Needs to be added.
-          ],
         },
         {
           name: "drop",
