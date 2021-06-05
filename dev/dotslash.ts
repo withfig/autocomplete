8de8744e8f69879391f7e471ac5727287468a352
Brendan Falk <brendan@withfig.com>
2021-06-04 23:23:44 -0700

    added clarifying comment to dotslash

--- dev/dotslash.ts
+++ dev/dotslash.ts
@@ -3,6 +3,8 @@
 export const completionSpec: Fig.Spec = {
   name: "dotslash",
   args: {
+    // This was previously just "filepaths", however, we added folders so
+    // users of ohmyzsh could cd into a folder by typing the folder name without `cd`
     template: ["filepaths", "folders"],
   },
 };
