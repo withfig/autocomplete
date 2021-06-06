b2d630a3e51ab00d3253e2f57731faf6fb742f94
James Hobson | FLOQ Design <james@floq.co.uk>
2021-06-06 21:43:20 +0100

    cap, cli

--- dev/wp.ts
+++ dev/wp.ts
@@ -150,23 +150,23 @@ export const completion: Fig.Spec = {
             suggestions: [
               {
                 name: "key",
-                insertValue: "key",
+                insertValue: "my_key",
                 description: "Cache key.",
               },
               {
                 name: "value",
-                insertValue: "value",
+                insertValue: "my_value",
                 description: "Value to add to the key.",
               },
               {
                 name: "[group]",
-                insertValue: "[group]",
+                insertValue: "my_group",
                 description:
                   "Method for grouping data within the cache which allows the same key to be used across groups.",
               },
               {
                 name: "[expiration]",
-                insertValue: "[expiration]",
+                insertValue: "300",
                 description:
                   "Define how long to keep the value, in seconds. 0 means as long as possible.",
               },
@@ -340,6 +340,261 @@ export const completion: Fig.Spec = {
       // If you want to build custom suggestions for arguments check out: https://fig.io/docs/autocomplete/building-a-spec#making-advanced-suggestions
       args: [{}],
       options: global_options,
+    },
+    {
+      name: "cap",
+      description: "Adds, removes, and lists capabilities of a user role.",
+      subcommands: [
+        {
+          name: "add",
+          description: "Adds a value to the object cache.",
+          options: [
+            {
+              name: "--grant",
+              description:
+                "Adds the capability as an explicit boolean value, instead of implicitly defaulting to true.",
+            },
+          ],
+          args: {
+            variadic: true,
+            suggestions: [
+              {
+                name: "role",
+                insertValue: "'editor'",
+                description: "Key for the role.",
+              },
+              {
+                name: "cap",
+                insertValue: "'spectate'",
+                description: "One or more capabilities to add.",
+              },
+            ],
+          },
+        },
+      ],
+    },
+    {
+      name: "cli",
+      description:
+        "Reviews current WP-CLI info, checks for updates, or views defined aliases.",
+      subcommands: [
+        {
+          name: "alias",
+          description:
+            "Retrieves, sets and updates aliases for WordPress Installations.",
+          subcommands: [
+            {
+              name: "add",
+              description: "Creates an alias.",
+              options: [
+                {
+                  name: "key",
+                  description: "Key for the alias.",
+                },
+                {
+                  name: "--set-user",
+                  insertValue: "--set-user=",
+                  description: "Set user for alias.",
+                },
+                {
+                  name: "--set-url",
+                  insertValue: "--set-url=",
+                  description: "Set url for alias.",
+                },
+                {
+                  name: "--set-path",
+                  insertValue: "--set-path=",
+                  description: "Set path for alias.",
+                },
+                {
+                  name: "--set-ssh",
+                  insertValue: "--set-ssh=",
+                  description: "Set ssh for alias.",
+                },
+                {
+                  name: "--set-http",
+                  insertValue: "--set-http=",
+                  description: "Set http for alias.",
+                },
+                {
+                  name: "--grouping",
+                  insertValue: "--grouping=",
+                  description: "For grouping multiple aliases.",
+                },
+                {
+                  name: "--config",
+                  insertValue: "--config=global|project",
+                  description: "Config file to be considered for operations.",
+                },
+              ],
+            },
+            {
+              name: "delete",
+              description: "Deletes an alias.",
+              options: [
+                {
+                  name: "key",
+                  description: "Key for the alias.",
+                },
+                {
+                  name: "--config",
+                  insertValue: "--config=global|project",
+                  description: "Config file to be considered for operations.",
+                },
+              ],
+            },
+            {
+              name: "get",
+              description: "Gets the value for an alias.",
+              options: [
+                {
+                  name: "key",
+                  description: "Key for the alias.",
+                },
+              ],
+            },
+            {
+              name: "list",
+              description: "Lists available WP-CLI aliases.",
+              options: [
+                {
+                  name: "--format",
+                  insertValue: "--format=yaml|json|var_export",
+                  description: "Set user for alias.",
+                },
+              ],
+            },
+            {
+              name: "update",
+              description: "Updates an alias.",
+              options: [
+                {
+                  name: "key",
+                  description: "Key for the alias.",
+                },
+                {
+                  name: "--set-user",
+                  insertValue: "--set-user=",
+                  description: "Set user for alias.",
+                },
+                {
+                  name: "--set-url",
+                  insertValue: "--set-url=",
+                  description: "Set url for alias.",
+                },
+                {
+                  name: "--set-path",
+                  insertValue: "--set-path=",
+                  description: "Set path for alias.",
+                },
+                {
+                  name: "--set-ssh",
+                  insertValue: "--set-ssh=",
+                  description: "Set ssh for alias.",
+                },
+                {
+                  name: "--set-http",
+                  insertValue: "--set-http=",
+                  description: "Set http for alias.",
+                },
+                {
+                  name: "--grouping",
+                  insertValue: "--grouping=",
+                  description: "For grouping multiple aliases.",
+                },
+                {
+                  name: "--config",
+                  insertValue: "--config=global|project",
+                  description: "Config file to be considered for operations.",
+                },
+              ],
+            },
+          ],
+          // options: [
+          //   {
+          //     name: "--grant",
+          //     description:
+          //       "Adds the capability as an explicit boolean value, instead of implicitly defaulting to true.",
+          //   },
+          // ],
+          // args: {
+          //   variadic: true,
+          //   suggestions: [
+          //     {
+          //       name: "role",
+          //       insertValue: "'editor'",
+          //       description: "Key for the role.",
+          //     },
+          //     {
+          //       name: "cap",
+          //       insertValue: "'spectate'",
+          //       description: "One or more capabilities to add.",
+          //     },
+          //   ],
+          // },
+        },
+        {
+          name: "cache",
+          description:
+            "Retrieves, sets and updates aliases for WordPress Installations.",
+          // options: [
+          //   {
+          //     name: "--grant",
+          //     description:
+          //       "Adds the capability as an explicit boolean value, instead of implicitly defaulting to true.",
+          //   },
+          // ],
+          // args: {
+          //   variadic: true,
+          //   suggestions: [
+          //     {
+          //       name: "role",
+          //       insertValue: "'editor'",
+          //       description: "Key for the role.",
+          //     },
+          //     {
+          //       name: "cap",
+          //       insertValue: "'spectate'",
+          //       description: "One or more capabilities to add.",
+          //     },
+          //   ],
+          // },
+        },
+        {
+          name: "check-update ",
+          description:
+            "Checks to see if there is a newer version of WP-CLI available.",
+        },
+        {
+          name: "cmd-dump ",
+          description: "Dumps the list of installed commands, as JSON.",
+        },
+        {
+          name: "completions",
+          description: "Generates tab completion strings.",
+        },
+        {
+          name: "has-command",
+          description: "Detects if a command exists",
+        },
+        {
+          name: "info ",
+          description: "Prints various details about the WP-CLI environment.",
+        },
+        {
+          name: "param-dump ",
+          description:
+            "Dumps the list of global parameters, as JSON or in var_export format.",
+        },
+        {
+          name: "update ",
+          description: "Updates WP-CLI to the latest release.",
+        },
+        {
+          name: "version",
+          description: "Prints WP-CLI version.",
+        },
+      ],
     },
   ],
   // options: global_options,
