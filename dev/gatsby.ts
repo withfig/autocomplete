a606da33e3de6ac6e4723bbb12c4926ddba6c41f
Chris Forte <chris@indieridge.com>
2021-06-06 18:36:01 +0100

    remove unneccessary args from options and remove repl options

--- dev/gatsby.ts
+++ dev/gatsby.ts
@@ -10,37 +10,27 @@ export const completion: Fig.Spec = {
         {
           name: ["-H", "--host"],
           description: "Set host. Defaults to localhost",
-          args: {
-            name: "host",
-          },
+          args: {},
         },
         {
           name: ["-p", "--port"],
           description: "Set port. Defaults to env.PORT or 8000",
-          args: {
-            name: "port",
-          },
+          args: {},
         },
         {
           name: ["-o", "--open"],
           description: "Open the site in your (default) browser for you",
-          args: {
-            name: "open",
-          },
+          args: {},
         },
         {
           name: ["-S", "--https"],
           description: "Use HTTPS",
-          args: {
-            name: "https",
-          },
+          args: {},
         },
         {
           name: ["--inspect"],
           description: "Opens a port for debugging",
-          args: {
-            name: "inspect",
-          },
+          args: {},
         },
       ],
     },
@@ -48,53 +38,40 @@ export const completion: Fig.Spec = {
     {
       name: "build",
       description: "Compile your application and make it ready for deployment",
-
       args: {},
       options: [
         {
           name: ["--prefix-paths"],
           description:
             "Build site with link paths prefixed (set pathPrefix in your config)",
-          args: {
-            name: "prefix",
-          },
+          args: {},
         },
         {
           name: ["--no-uglify"],
           description:
             "Build site without uglifying JS bundles (for debugging)",
-          args: {
-            name: "no uglify",
-          },
+          args: {},
         },
         {
           name: ["--profile"],
           description: "Build site with react profiling",
-          args: {
-            name: "profile",
-          },
+          args: {},
         },
         {
           name: ["--open-tracing-config-file"],
           description: "Use Tracer configuration file",
-          args: {
-            name: "tracer",
-          },
+          args: {},
         },
         {
           name: ["--graphql-tracing"],
           description:
             "Trace every graphql resolver, may have performance implications.",
-          args: {
-            name: "inspect",
-          },
+          args: {},
         },
         {
           name: ["--no-color", "--no-colors"],
           description: "Disables colored terminal output",
-          args: {
-            name: "no color",
-          },
+          args: {},
         },
       ],
     },
@@ -102,36 +79,27 @@ export const completion: Fig.Spec = {
     {
       name: "serve",
       description: "Serve the production build of your site for testing",
-
       args: {},
       options: [
         {
           name: ["-H", "--host"],
           description: "Set host. Defaults to localhost",
-          args: {
-            name: "host",
-          },
+          args: {},
         },
         {
           name: ["-p", "--port"],
           description: "Set port. Defaults to env.PORT or 8000",
-          args: {
-            name: "port",
-          },
+          args: {},
         },
         {
           name: ["-o", "--open"],
           description: "Open the site in your (default) browser for you",
-          args: {
-            name: "open",
-          },
+          args: {},
         },
         {
           name: ["--prefix-paths"],
           description: "Serve site with link paths prefixed",
-          args: {
-            name: "prefix",
-          },
+          args: {},
         },
       ],
     },
@@ -139,13 +107,11 @@ export const completion: Fig.Spec = {
     {
       name: "clean",
       description: "Wipe out the cache (.cache folder) and public directories",
-
       args: {},
     },
     {
       name: "info",
       description: "Get helpful environment information",
-
       args: {},
     },
     {
@@ -157,9 +123,7 @@ export const completion: Fig.Spec = {
         {
           name: ["docs"],
           description: "Documentation about using and creating plugins",
-          args: {
-            name: "docs",
-          },
+          args: {},
         },
       ],
     },
@@ -167,81 +131,7 @@ export const completion: Fig.Spec = {
     {
       name: "repl",
       description: "Gatsby will prompt you to type in commands and explore",
-
       args: {},
-      options: [
-        {
-          name: ["babelrc"],
-          description: "babelrc",
-          args: {
-            name: "babelrc",
-          },
-        },
-
-        {
-          name: ["components"],
-          description: "components",
-          args: {
-            name: "components",
-          },
-        },
-
-        {
-          name: ["dataPaths"],
-          description: "dataPaths",
-          args: {
-            name: "dataPaths",
-          },
-        },
-
-        {
-          name: ["getNodes()"],
-          description: "getNodes()",
-          args: {
-            name: "getNodes()",
-          },
-        },
-
-        {
-          name: ["nodes"],
-          description: "nodes",
-          args: {
-            name: "nodes",
-          },
-        },
-
-        {
-          name: ["pages"],
-          description: "pages",
-          args: {
-            name: "pages",
-          },
-        },
-
-        {
-          name: ["schema"],
-          description: "schema",
-          args: {
-            name: "schema",
-          },
-        },
-
-        {
-          name: ["siteConfig"],
-          description: "siteConfig",
-          args: {
-            name: "siteConfig",
-          },
-        },
-
-        {
-          name: ["staticQueries"],
-          description: "staticQueries",
-          args: {
-            name: "staticQueries",
-          },
-        },
-      ],
     },
   ],
   options: [
