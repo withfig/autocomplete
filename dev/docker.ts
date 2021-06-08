47ce55c5c7a316308b4f2f4dc9efee24fa152e55
Danny Aziz <daanyaal997@gmail.com>
2021-06-08 18:45:22 +0100

    (docker) feat: complete docker swarm

--- dev/docker.ts
+++ dev/docker.ts
@@ -4786,22 +4786,248 @@ export const completionSpec: Fig.Spec = {
         {
           name: "ca",
           description: "Display and rotate the root CA",
+          options: [
+            {
+              name: "--ca-cert",
+              description:
+                "Path to the PEM-formatted root CA certificate to use for the new cluster",
+              args: {
+                name: "pem-file",
+                template: "filepaths",
+              },
+            },
+            {
+              name: "--ca-key",
+              description:
+                " Path to the PEM-formatted root CA key to use for the new cluster",
+              args: {
+                name: "pem-file",
+                template: "filepaths",
+              },
+            },
+            {
+              name: "--cert-expiry",
+              description:
+                "Validity period for node certificates (ns|us|ms|s|m|h) (default 2160h0m0s)",
+              args: {
+                name: "duration",
+              },
+            },
+            {
+              name: ["-d", "--detach"],
+              description:
+                "Exit immediately instead of waiting for the root rotation to converge",
+            },
+            {
+              name: "--external-ca",
+              description:
+                " Specifications of one or more certificate signing endpoints",
+              args: {
+                name: "external-ca",
+              },
+            },
+            {
+              name: ["-q", "--quiet"],
+              description: " Suppress progress output",
+            },
+            {
+              name: "--rotate",
+              description:
+                "Rotate the swarm CA - if no certificate or key are provided, new ones will be generated",
+            },
+          ],
         },
         {
           name: "init",
           description: "Initialize a swarm",
+          options: [
+            {
+              name: "--advertise-addr",
+              description: "Advertised address (format: <ip|interface>[:port])",
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--autolock",
+              description:
+                " Enable manager autolocking (requiring an unlock key to start a stopped manager)",
+            },
+            {
+              name: "--availability",
+              description:
+                'Availability of the node ("active"|"pause"|"drain") (default "active")',
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--cert-expiry",
+              description:
+                " Validity period for node certificates (ns|us|ms|s|m|h) (default 2160h0m0s)",
+              args: {
+                name: "duration",
+              },
+            },
+            {
+              name: "--data-path-addr",
+              description:
+                "Address or interface to use for data path traffic (format: <ip|interface>)",
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--data-path-port",
+              description:
+                "Port number to use for data path traffic (1024 - 49151). If no value is set or is set to 0, the default port (4789) is used.",
+              args: {
+                name: "uint32",
+              },
+            },
+            {
+              name: "--default-addr-pool",
+              description: " default address pool in CIDR format (default [])",
+              args: {
+                name: "ipNetSlice",
+              },
+            },
+            {
+              name: "--default-addr-pool-mask-length",
+              description:
+                " default address pool subnet mask length (default 24)",
+              args: {
+                name: "uint32",
+              },
+            },
+            {
+              name: "--dispatcher-heartbeat",
+              description:
+                "Dispatcher heartbeat period (ns|us|ms|s|m|h) (default 5s)",
+              args: {
+                name: "duration",
+              },
+            },
+            {
+              name: "--external-ca",
+              description:
+                "Specifications of one or more certificate signing endpoints",
+              args: {
+                name: "external-ca",
+              },
+            },
+            {
+              name: "--force-new-cluster",
+              description: "Force create a new cluster from current state",
+            },
+            {
+              name: "--listen-addr",
+              description:
+                "Listen address (format: <ip|interface>[:port]) (default 0.0.0.0:2377)",
+              args: {
+                name: "node-addr",
+              },
+            },
+            {
+              name: "--max-snapshots",
+              description: " Number of additional Raft snapshots to retain",
+              args: {
+                name: "uint",
+              },
+            },
+            {
+              name: "--snapshot-interval",
+              description:
+                " Number of log entries between Raft snapshots (default 10000)",
+              args: {
+                name: "uint",
+              },
+            },
+            {
+              name: "--task-history-limit",
+              description: " Task history retention limit (default 5)",
+              args: {
+                name: "int",
+              },
+            },
+          ],
         },
         {
           name: "join",
           description: "Join a swarm as a node and/or manager",
+          args: {
+            name: "HOST:PORT",
+          },
+          options: [
+            {
+              name: "--advertise-addr",
+              description:
+                " Advertised address (format: <ip|interface>[:port])",
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--availability",
+              description:
+                ' Availability of the node ("active"|"pause"|"drain") (default "active")',
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--data-path-addr",
+              description:
+                " Address or interface to use for data path traffic (format: <ip|interface>)",
+              args: {
+                name: "string",
+              },
+            },
+            {
+              name: "--listen-addr",
+              description:
+                " Listen address (format: <ip|interface>[:port]) (default 0.0.0.0:2377)",
+              args: {
+                name: "node-addr",
+              },
+            },
+            {
+              name: "--token",
+              description: "Token for entry into the swarm",
+              args: {
+                name: "string",
+              },
+            },
+          ],
         },
         {
           name: "join-token",
           description: "Manage join tokens",
+          args: {
+            name: "worker or manager",
+            suggestions: ["worker", "manager"],
+          },
+          options: [
+            {
+              name: ["-q", "--quiet"],
+              description: "Only display token",
+            },
+            {
+              name: "--rotate",
+              description: " Rotate join token",
+            },
+          ],
         },
         {
           name: "leave",
           description: "Leave the swarm",
+          options: [
+            {
+              name: ["-f", "--force"],
+              description:
+                " Force this node to leave the swarm, ignoring warnings",
+            },
+          ],
         },
         {
           name: "unlock",
@@ -4810,10 +5036,75 @@ export const completionSpec: Fig.Spec = {
         {
           name: "unlock-key",
           description: "Manage the unlock key",
+          options: [
+            {
+              name: ["-q", "--quiet"],
+              description: "Only display token",
+            },
+            {
+              name: "--rotate",
+              description: " Rotate unlock key",
+            },
+          ],
         },
         {
           name: "update",
           description: "Update the swarm",
+          options: [
+            {
+              name: "--autolock",
+              description: "Change manager autolocking setting (true|false)",
+              args: {
+                suggestions: ["true", "false"],
+              },
+            },
+            {
+              name: "--cert-expiry",
+              description:
+                "Validity period for node certificates (ns|us|ms|s|m|h) (default 2160h0m0s)",
+              args: {
+                name: "duration",
+              },
+            },
+            {
+              name: "--dispatcher-heartbeat",
+              description:
+                " Dispatcher heartbeat period (ns|us|ms|s|m|h) (default 5s)",
+              args: {
+                name: "duration",
+              },
+            },
+            {
+              name: "--external-ca",
+              description:
+                " Specifications of one or more certificate signing endpoints",
+              args: {
+                name: "external-ca",
+              },
+            },
+            {
+              name: "--max-snapshots",
+              description: "Number of additional Raft snapshots to retain",
+              args: {
+                name: "uint",
+              },
+            },
+            {
+              name: "--snapshot-interval",
+              description:
+                "Number of log entries between Raft snapshots (default 10000)",
+              args: {
+                name: "uint",
+              },
+            },
+            {
+              name: "--task-history-limit",
+              description: "Task history retention limit (default 5)",
+              args: {
+                name: "int",
+              },
+            },
+          ],
         },
       ],
     },
