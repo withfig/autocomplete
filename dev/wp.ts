6edc5391f69e0c8c78ce7a5b67d5c050fbcd736c
James Hobson | FLOQ Design <james@floq.co.uk>
2021-06-10 11:08:39 +0100

    wp i18n

--- dev/wp.ts
+++ dev/wp.ts
@@ -2865,7 +2865,142 @@ export const completion: Fig.Spec = {
       name: "i18n",
       description:
         "Provides internationalization tools for WordPress projects.",
-      subcommands: [{}],
+      subcommands: [
+        {
+          name: "make-json",
+          description:
+            "Extract JavaScript strings from PO files and add them to individual JSON files.",
+          args: [
+            {
+              name: "source",
+              // description: "Path to an existing PO file or a directory containing multiple PO files.",
+            },
+            {
+              name: "destination",
+              // description: "Path to the destination directory for the resulting JSON files. Defaults to the source directory.",
+            },
+          ],
+          options: [
+            {
+              name: "--purge",
+              description:
+                "Whether to purge the strings that were extracted from the original source file. Defaults to true, use --no-purge to skip the removal.",
+            },
+            {
+              name: "--pretty-print",
+              description: "Pretty-print resulting JSON files.",
+            },
+          ],
+        },
+        {
+          name: "make-mo",
+          description: "Create MO files from PO files.",
+          args: [
+            {
+              name: "source",
+              // description: "Path to an existing PO file or a directory containing multiple PO files.",
+            },
+            {
+              name: "destination",
+              // description: "Path to the destination directory for the resulting JSON files. Defaults to the source directory.",
+            },
+          ],
+        },
+        {
+          name: "make-pot",
+          description: "Create a POT file for a WordPress project.",
+          args: [
+            {
+              name: "source",
+              // description: "Directory to scan for string extraction.",
+            },
+            {
+              name: "destination",
+              // description: "Name of the resulting POT file.",
+            },
+          ],
+          options: [
+            {
+              name: "--slug",
+              insertValue: "--slug=",
+              description:
+                "Plugin or theme slug. Defaults to the source directory’s basename.",
+            },
+            {
+              name: "--domain",
+              insertValue: "--domain=",
+              description:
+                "Text domain to look for in the source code, unless the --ignore-domain option is used. By default, the “Text Domain” header of the plugin or theme is used. If none is provided, it falls back to the project slug.",
+            },
+            {
+              name: "--ignore-domain",
+              insertValue: "--ignore-domain=",
+              description:
+                "Ignore the text domain completely and extract strings with any text domain.",
+            },
+            {
+              name: "--merge",
+              insertValue: "--merge=",
+              description:
+                "Comma-separated list of POT files whose contents should be merged with the extracted strings. If left empty, defaults to the destination POT file. POT file headers will be ignored.",
+            },
+            {
+              name: "--subtract",
+              insertValue: "--subtract=",
+              description:
+                "Comma-separated list of POT files whose contents should act as some sort of blacklist for string extraction. Any string which is found on that blacklist will not be extracted. This can be useful when you want to create multiple POT files from the same source directory with slightly different content and no duplicate strings between them.",
+            },
+            {
+              name: "--include",
+              insertValue: "--include=",
+              description:
+                "Comma-separated list of files and paths that should be used for string extraction. If provided, only these files and folders will be taken into account for string extraction. For example, --include='src,my-file.php will ignore anything besides my-file.php and files in the src directory. Simple glob patterns can be used, i.e. --include=foo-*.php includes any PHP file with the foo- prefix. Leading and trailing slashes are ignored, i.e. /my/directory/ is the same as my/directory.",
+            },
+            {
+              name: "--exclude",
+              insertValue: "--exclude=",
+              description:
+                "Comma-separated list of files and paths that should be skipped for string extraction. For example, --exclude='.github,myfile.php would ignore any strings found within myfile.php or the .github folder. Simple glob patterns can be used, i.e. --exclude=foo-*.php excludes any PHP file with the foo- prefix. Leading and trailing slashes are ignored, i.e. /my/directory/ is the same as my/directory. The following files and folders are always excluded: node_modules, .git, .svn, .CVS, .hg, vendor, *.min.js.",
+            },
+            {
+              name: "--headers",
+              insertValue: "--headers=",
+              description:
+                "Array in JSON format of custom headers which will be added to the POT file. Defaults to empty array.",
+            },
+            {
+              name: "--skip-js",
+              description:
+                "Skips JavaScript string extraction. Useful when this is done in another build step, e.g. through Babel.",
+            },
+            {
+              name: "--skip-php",
+              description: "Skips PHP string extraction.",
+            },
+            {
+              name: "--skip-block-json",
+              description: "Skips string extraction from block.json files.",
+            },
+            {
+              name: "--skip-audit",
+              description:
+                "Skips string audit where it tries to find possible mistakes in translatable strings. Useful when running in an automated environment.",
+            },
+            {
+              name: "--file-comment",
+              insertValue: "--file-comment=",
+              description:
+                "String that should be added as a comment to the top of the resulting POT file. By default, a copyright comment is added for WordPress plugins and themes in the following manner: Copyright (C) 2018 Example Plugin Author. This file is distributed under the same license as the Example Plugin package. If a plugin or theme specifies a license in their main plugin file or stylesheet, the comment looks like this: Copyright (C) 2018 Example Plugin Author. This file is distributed under the GPLv2.",
+            },
+            {
+              name: "--package-name",
+              insertValue: "--package-name=",
+              description:
+                "Name to use for package name in the resulting POT file’s Project-Id-Version header. Overrides plugin or theme name, if applicable.",
+            },
+          ],
+        },
+      ],
     },
     {
       name: "import",
