1e01e545655b1cb57239ba9137ab7e08431ffd48
Tom <quiibzdev@gmail.com>
2021-06-18 09:38:13 +0200

    fix: Review

--- dev/gatsby.ts
+++ dev/gatsby.ts
@@ -1,3 +1,24 @@
+const sharedOptions: Fig.Option[] = [
+  {
+    name: ["-H", "--host"],
+    description: "Set host. Defaults to localhost",
+    args: {
+      name: "host",
+    },
+  },
+  {
+    name: ["-p", "--port"],
+    description: "Set port. Defaults to env.PORT or 8000",
+    args: {
+      name: "port",
+    },
+  },
+  {
+    name: ["-o", "--open"],
+    description: "Open the site in your (default) browser for you",
+  },
+];
+
 export const completion: Fig.Spec = {
   name: "gatsby",
   description: "Gatsby CLI",
@@ -5,133 +26,83 @@ export const completion: Fig.Spec = {
     {
       name: "develop",
       description: "Start the Gatsby development server",
-      args: {},
       options: [
-        {
-          name: ["-H", "--host"],
-          description: "Set host. Defaults to localhost",
-          args: {},
-        },
-        {
-          name: ["-p", "--port"],
-          description: "Set port. Defaults to env.PORT or 8000",
-          args: {},
-        },
-        {
-          name: ["-o", "--open"],
-          description: "Open the site in your (default) browser for you",
-          args: {},
-        },
+        ...sharedOptions,
         {
           name: ["-S", "--https"],
           description: "Use HTTPS",
-          args: {},
         },
         {
           name: ["--inspect"],
           description: "Opens a port for debugging",
-          args: {},
         },
       ],
     },
-
     {
       name: "build",
       description: "Compile your application and make it ready for deployment",
-      args: {},
       options: [
         {
           name: ["--prefix-paths"],
           description:
             "Build site with link paths prefixed (set pathPrefix in your config)",
-          args: {},
         },
         {
           name: ["--no-uglify"],
           description:
             "Build site without uglifying JS bundles (for debugging)",
-          args: {},
         },
         {
           name: ["--profile"],
           description: "Build site with react profiling",
-          args: {},
         },
         {
           name: ["--open-tracing-config-file"],
           description: "Use Tracer configuration file",
-          args: {},
         },
         {
           name: ["--graphql-tracing"],
           description:
             "Trace every graphql resolver, may have performance implications.",
-          args: {},
         },
         {
           name: ["--no-color", "--no-colors"],
           description: "Disables colored terminal output",
-          args: {},
         },
       ],
     },
-
     {
       name: "serve",
       description: "Serve the production build of your site for testing",
-      args: {},
       options: [
-        {
-          name: ["-H", "--host"],
-          description: "Set host. Defaults to localhost",
-          args: {},
-        },
-        {
-          name: ["-p", "--port"],
-          description: "Set port. Defaults to env.PORT or 8000",
-          args: {},
-        },
-        {
-          name: ["-o", "--open"],
-          description: "Open the site in your (default) browser for you",
-          args: {},
-        },
+        ...sharedOptions,
         {
           name: ["--prefix-paths"],
           description: "Serve site with link paths prefixed",
-          args: {},
         },
       ],
     },
-
     {
       name: "clean",
       description: "Wipe out the cache (.cache folder) and public directories",
-      args: {},
     },
     {
       name: "info",
       description: "Get helpful environment information",
-      args: {},
     },
     {
       name: "plugin",
       description: "Run commands pertaining to gatsby plugins",
-
-      args: {},
       options: [
         {
           name: ["docs"],
           description: "Documentation about using and creating plugins",
-          args: {},
         },
       ],
     },
-
     {
       name: "repl",
       description: "Gatsby will prompt you to type in commands and explore",
-      args: {},
     },
   ],
   options: [
