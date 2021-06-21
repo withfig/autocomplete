aa33e7d7a56bd8a6a42626341d450268d396c057
Tom <quiibzdev@gmail.com>
2021-06-21 18:12:19 +0200

    fix: Move scntool to xcrun

--- dev/xcrun.ts
+++ dev/xcrun.ts
@@ -6,7 +6,50 @@ export const completionSpec: Fig.Spec = {
     {
       name: "scntool",
       description: "SceneKit CLI utilities.",
-      loadSpec: "xcrun/scntool",
+      options: [
+        {
+          name: "--convert",
+          description: "File to convert.",
+          args: {
+            name: "file",
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--format",
+          description: "Format to convert to.",
+          args: {
+            suggestions: ["scn", "dae", "c3d"],
+            name: "format",
+          },
+        },
+        {
+          name: "--output",
+          description: "Path to the output file.",
+          args: {
+            name: "file",
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--force-y-up",
+          description: "Convert objects to use y axis up.",
+        },
+        {
+          name: "--force-interleaved",
+          description:
+            "Interleave the vertex data for multiple semantics in the same array to achieve better rendering performance.",
+        },
+        {
+          name: "--prefer-compressed-textures",
+          description:
+            "Use .ktx, .astc and .pvrtc files for textures if available in the asset catalog.",
+        },
+        {
+          name: "--verbose",
+          description: "Get pretty error message.",
+        },
+      ],
     },
   ],
   options: [
@@ -51,6 +94,7 @@ export const completionSpec: Fig.Spec = {
     {
       name: ["-k", "--kill-cache"],
       description: "Invalidate all existing cache entries.",
+      isDangerous: true,
     },
     {
       name: "--show-sdk-path",
