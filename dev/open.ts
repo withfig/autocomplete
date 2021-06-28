93efb894bc1ea43ec2b1860444e6bd57e075b717
Ankith Madadi <ankithm28@gmail.com>
2021-06-28 15:43:29 -0700

    Update  spec

--- dev/open.ts
+++ dev/open.ts
@@ -1,7 +1,82 @@
 export const completionSpec: Fig.Spec = {
   name: "open",
   description: "open files using default application",
+  options: [
+    {
+      name: "-a",
+      description: "Specify the application to use for opening the file",
+      args: {
+        name: "Application",
+        generators: {
+          script: "ls /Applications",
+          postProcess: function (out) {
+            return out.split("\n").map((line) => ({
+              name: line,
+              icon: "💻",
+            }));
+          },
+        },
+      },
+    },
+    {
+      name: "-b",
+      description:
+        "Specify the bundle identifier of the app to use to open the file",
+      args: {
+        name: "Bundle Identifier",
+      },
+    },
+    {
+      name: "-D",
+      description: "Reveals the enclosing folder in finder",
+    },
+    {
+      name: "-e",
+      description: "Opens the file with /Applications/TextEdit",
+    },
+    {
+      name: "-t",
+      description: "Open the file with the default text editor",
+    },
+    {
+      name: "-f",
+      description:
+        "Read input from standard input and open the results in the default text editor",
+    },
+    {
+      name: "-F",
+      description: "Opens the application without restoring windows",
+    },
+    {
+      name: "-W",
+      description: "Waits until the applications exit",
+    },
+    {
+      name: "-R",
+      description: "Reveals the file(s) in the Finder instead of opening them",
+    },
+    {
+      name: "-n",
+      description:
+        "Open a new instance of the application(s) even if one is already running",
+    },
+    {
+      name: "-g",
+      description: "Do not bring the application to the foreground",
+    },
+    {
+      name: "-h",
+      description:
+        "Searches header locations for a header whose name matches the given string and then opens it",
+    },
+    {
+      name: "--args",
+      description:
+        "All remaining arguments are passed to the opened application in the argv parameter to main().  These arguments are not opened or interpreted by the open tool",
+    },
+  ],
   args: {
     template: ["filepaths", "folders"],
+    variadic: true,
   },
 };
