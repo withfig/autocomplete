7c277e38c06354e2ba676ccfcd02a75856dc63b2
Ankith Madadi <40261054+Monkeyank@users.noreply.github.com>
2021-07-12 18:04:28 -0700

    Local package support for npx

--- dev/npx.ts
+++ dev/npx.ts
@@ -1,6 +1,28 @@
 export const completionSpec: Fig.Spec = {
   name: "npx",
   description: "Execute binaries from npm packages",
+  generateSpec: async (_context, executeShellCommand) => {
+    const commands = (
+      await executeShellCommand(
+        `until [[ -d node_modules/ ]] || [[ $PWD = '/' ]]; do cd ..; done; ls -1 node_modules/.bin/`
+      )
+    ).split("\n");
+
+    const cli = ["react-native", "tailwindcss", "next", "gltfjsx", "prisma"];
+    const subcommands = commands
+      .filter((name) => !cli.includes(name))
+      .map((name) => ({
+        name,
+        loadSpec: name,
+        icon: "fig://icon?type=command",
+      }));
+
+    return {
+      name: "npx",
+      subcommands,
+    } as Fig.Spec;
+  },
+
   subcommands: [
     {
       name: "react-native",
