575a744b6492b0040702a45af1909a0896222963
Ankith Madadi <40261054+Monkeyank@users.noreply.github.com>
2021-07-22 00:22:37 -0700

    Add description

--- dev/gradle.ts
+++ dev/gradle.ts
@@ -353,7 +353,8 @@ export const completion: Fig.Spec = {
   // ./gradlew doesn't work
   // name: ["gradle", "./gradlew"],
   name: "gradle",
-  description: "",
+  description:
+    "Gradle is an open-source build automation tool that is designed to be flexible enough to build almost any type of software",
   subcommands: [
     {
       name: "build",
