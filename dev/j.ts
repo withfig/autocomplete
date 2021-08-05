a103db1b94c6dcb411ff2f6937bed078ccb4bdc9
Wes Feller <wes@fig.io>
2021-08-05 07:35:08 -0700

    Spreading autojump options and description

--- dev/j.ts
+++ dev/j.ts
@@ -1,43 +1,8 @@
+import autojump from "./autojump";
+
 const completionSpec: Fig.Spec = {
   name: "j",
-  description: "a faster way to navigate your filesystem",
-  options: [
-    {
-      name: ["-h", "--help"],
-      description: "show the help message and exit",
-    },
-    {
-      name: ["-a", "--add"],
-      description: "add path",
-      args: [{ name: "directory", template: ["folders"] }],
-    },
-    {
-      name: ["-i", "--increase"],
-      description: "increase current directory weight",
-      args: [{ name: "weight", isOptional: true }],
-    },
-    {
-      name: ["-d", "--decrease"],
-      description: "decrease current directory weight",
-      args: [{ name: "weight", isOptional: true }],
-    },
-    {
-      name: ["--complete"],
-      description: "used for tab completion",
-    },
-    {
-      name: ["--purge"],
-      description: "remove non-existent paths from database",
-    },
-    {
-      name: ["-s", "--stat"],
-      description: "show database entries and their key weights",
-    },
-    {
-      name: ["-v", "--version"],
-      description: "show version information",
-    },
-  ],
+  ...autojump,
   args: {
     name: "directory",
     description: "directory to jump to",
