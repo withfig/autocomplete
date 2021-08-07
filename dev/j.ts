984ac429e1cc50c9253ce2c314409018785145d7
Westley Feller <feller.westley@gmail.com>
2021-08-07 02:02:48 -0700

    Support for autojump and alias j (#441)
    * Support for autojump and alias j
    
    * Spreading autojump options and description
    
    Co-authored-by: Wes Feller <wes@fig.io>

---
+++ dev/j.ts
@@ -0,0 +1,54 @@
+import autojump from "./autojump";
+
+const completionSpec: Fig.Spec = {
+  name: "j",
+  ...autojump,
+  args: {
+    name: "directory",
+    description: "directory to jump to",
+    variadic: true,
+    generators: {
+      script: 'cat "$HOME"/Library/autojump/autojump.txt',
+      postProcess: (out, ctx) => {
+        const lines = out.split("\n").map((line) => {
+          const [weight, dir] = line.split("\t");
+
+          return {
+            weight: Number(weight),
+            dir,
+          };
+        });
+
+        const args = ctx.slice(1, ctx.length - 1);
+
+        // directory arg is variadic with each subsequent arg being an
+        // additional filter. filtered will filter directories by all args.
+        const filtered = lines.filter(({ dir }) =>
+          args.every((arg) => dir.includes(arg))
+        );
+
+        return filtered.map(({ weight, dir }) => {
+          const splitPath = dir.split("/");
+          const name = splitPath[splitPath.length - 1];
+
+          // arg is variadic but we don't want to suggest something
+          // that's already been entered. This 'if' prevents redundant
+          // suggestions.
+          if (!args.includes(name)) {
+            return {
+              name,
+              description: dir,
+              // Docs state max weight is 100 but this seems
+              // to work regardless of any amount over that limit.
+              // Fig should defer assigning priority to autojump.
+              // 75 added to keep args above options.
+              priority: 75 + weight,
+            };
+          }
+        });
+      },
+    },
+  },
+};
+
+export default completionSpec;
