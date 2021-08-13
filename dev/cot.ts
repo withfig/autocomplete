db6fa77fdd76f42028900d08a6599f5e621553f5
Yavor Kolev <yavko@outlook.com>
2021-08-13 21:19:07 +0300

    Update dev/cot.ts
    Co-authored-by: Federico Ciardi <fed.ciardi@gmail.com>

--- dev/cot.ts
+++ dev/cot.ts
@@ -4,7 +4,7 @@ CotEditor is great I hope you use this!
 */
 const completionSpec: Fig.Spec = {
   name: "cot",
-  icon: "view-source:https://coteditor.com/favicon@2x.png",
+  icon: "https://coteditor.com/favicon@2x.png",
   description: "Command-line utility for CotEditor.",
   args: {
     template: "filepaths",
