abbdb9e715752d9ebc0c4e1999eba0fd68c35564
Federico Ciardi <fed.ciardi@gmail.com>
2021-08-16 22:07:55 +0200

    feat: add `z` spec

---
+++ dev/z.ts
@@ -0,0 +1,26 @@
+// https://github.com/rupa/z
+const completionSpec: Fig.Spec = {
+  name: "z",
+  args: {
+    name: "regex",
+    isVariadic: true,
+  },
+  options: [
+    {
+      name: "-c",
+      description:
+        "Restrict matches to subdirectories of the current directory",
+    },
+    { name: "-e", description: "Echo the best match, don't cd" },
+    { name: "-h", description: "Show a brief help message" },
+    { name: "-l", description: "List only" },
+    { name: "-r", description: "Match by rank only" },
+    { name: "-t", description: "Match by recent access only" },
+    {
+      name: "-x",
+      description: "Remove the current directory from the datafile",
+    },
+  ],
+};
+
+export default completionSpec;
