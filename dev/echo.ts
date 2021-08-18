e11c061e80948ed13d749092624c4d0e24e6fad8
Westley Feller <wes@fig.io>
2021-08-17 20:09:24 -0700

    Changed arg name to string and removed description
    The argument is self explanatory as echo only does one thing. The description box was very long despite how simple the argument is.

--- dev/echo.ts
+++ dev/echo.ts
@@ -2,9 +2,7 @@ const completionSpec: Fig.Spec = {
   name: "echo",
   description: "Write arguments to the standard output",
   args: {
-    name: "operands",
-    description:
-      "Write any specified operands, separated by single blank characters and followed by a newline character, to the standard output",
+    name: "string",
     isVariadic: true,
   },
   options: [
