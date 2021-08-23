b957ac5e87cbc2d61532c91d1375efd4e7f1e411
Westley Feller <wes@fig.io>
2021-08-23 08:42:38 -0700

    Fixed mv and added options
    -R and -P are not options on mv so they were removed and replaced with -f, -i, -n, and -v
    
    First arg is variadic

--- dev/mv.ts
+++ dev/mv.ts
@@ -3,20 +3,36 @@ const completionSpec: Fig.Spec = {
   description: "move & rename files and folders",
   args: [
     {
+      name: "source",
+      isVariadic: true,
       template: ["filepaths", "folders"],
     },
     {
+      name: "target",
       template: ["filepaths", "folders"],
     },
   ],
   options: [
     {
-      name: "-R",
-      description: "recursive",
+      name: "-f",
+      description:
+        "Do not prompt for confirmation before overwriting the destination path",
+      exclusiveOn: ["-i", "-n"],
     },
     {
-      name: "-P",
-      description: "Don't follow symbolic links",
+      name: "-i",
+      description:
+        "Cause mv to write a prompt to standard error before moving a file that would overwrite an existing file",
+      exclusiveOn: ["-f", "-n"],
+    },
+    {
+      name: "-n",
+      description: "Do not overwrite existing file",
+      exclusiveOn: ["-f", "-i"],
+    },
+    {
+      name: "-v",
+      description: "Cause mv to be verbose, showing files after they are moved",
     },
   ],
 };
