967e4bf4752cfe95102500b76d9cc3dfc796368e
Tim Raderschad <tim.raderschad@gmail.com>
2021-09-01 10:56:31 +0200

    feat: add graphcdn spec

---
+++ dev/graphcdn.ts
@@ -0,0 +1,66 @@
+const helpOption: Fig.Option = {
+  name: ["--help", "-h"],
+};
+
+const completionSpec: Fig.Spec = {
+  name: "graphcdn",
+  description: "Change the shell working directory",
+  subcommands: [
+    {
+      name: "login",
+      options: [
+        {
+          name: "--token",
+        },
+        helpOption,
+      ],
+    },
+    {
+      name: "init",
+      options: [
+        {
+          name: ["-y", "--yes"],
+          description: "Skip all the questions and just init",
+        },
+        helpOption,
+      ],
+    },
+    {
+      name: "pull",
+      options: [
+        {
+          name: "--service",
+          description:
+            "Name of the service to pull from (only required without graphcdn.yml)",
+          args: {
+            name: "service",
+            description: "Name of the environment to pull from",
+          },
+        },
+        {
+          name: "--env",
+          args: {
+            name: "env",
+          },
+        },
+        helpOption,
+      ],
+    },
+    {
+      name: "push",
+      options: [
+        {
+          name: "--env",
+          args: {
+            name: "env",
+          },
+        },
+        helpOption,
+      ],
+      subcommands: [{ name: "schema" }],
+    },
+  ],
+  options: [helpOption],
+};
+
+export default completionSpec;
