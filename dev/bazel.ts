bf580bb89b4b0aaf93e16c825b4203a385275e72
Tom <quiibzdev@gmail.com>
2021-09-12 15:48:32 +0200

    refactor: Resolve review

--- dev/bazel.ts
+++ dev/bazel.ts
@@ -66,27 +66,33 @@ const completionSpec: Fig.Spec = {
   options: [
     {
       name: "--autodetect_server_javabase",
+      exclusiveOn: ["--noautodetect_server_javabase"],
       description:
         "Back to the local JDK for running the bazel server and instead exits",
     },
     {
       name: "--noautodetect_server_javabase",
+      exclusiveOn: ["autodetect_server_javabase"],
       description: "Opposite of --autodetect_server_javabase",
     },
     {
       name: "--batch",
+      exclusiveOn: ["--nobatch"],
       description: "Run as just a client process without a server",
     },
     {
       name: "--nobatch",
+      exclusiveOn: ["--batch"],
       description: "Opposite of --batch",
     },
     {
       name: "--batch_cpu_scheduling",
+      exclusiveOn: ["--nobatch_cpu_scheduling"],
       description: "Only on Linux; use 'batch' CPU scheduling for Bazel",
     },
     {
       name: "--nobatch_cpu_scheduling",
+      exclusiveOn: ["--batch_cpu_scheduling"],
       description: "Opposite of --batch_cpu_scheduling",
     },
     {
@@ -97,20 +103,24 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "--block_for_lock",
+      exclusiveOn: ["--noblock_for_lock"],
       description:
         "Wait for a running command to complete, but instead exits immediately",
     },
     {
       name: "--noblock_for_lock",
+      exclusiveOn: ["--block_for_lock"],
       description: "Opposite of --block_for_lock",
     },
     {
       name: "--client_debug",
+      exclusiveOn: ["--noclient_debug"],
       description:
         "Log debug information from the client to stderr. Changing this option will not cause the server to restart",
     },
     {
       name: "--noclient_debug",
+      exclusiveOn: ["--client_debug"],
       description: "Opposite of --client_debug",
     },
     {
@@ -121,11 +131,13 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "--expand_configs_in_place",
+      exclusiveOn: ["--noexpand_configs_in_place"],
       description:
         "Changed the expansion of --config flags to be done in-place",
     },
     {
       name: "--noexpand_configs_in_place",
+      exclusiveOn: ["--expand_configs_in_place"],
       description: "Opposite of --expand_configs_in_place",
     },
     {
@@ -136,28 +148,34 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "--home_rc",
+      exclusiveOn: ["--nohome_rc"],
       description:
         "Whether or not to look for the home bazelrc file at $HOME/.bazelrc",
     },
     {
       name: "--nohome_rc",
+      exclusiveOn: ["--home_rc"],
       description:
         "Whether or not to look for the home bazelrc file at $HOME/.bazelrc",
     },
     {
       name: "--idle_server_tasks",
+      exclusiveOn: ["--noidle_server_tasks"],
       description: "Run System.gc() when the server is idle",
     },
     {
       name: "--noidle_server_tasks",
+      exclusiveOn: ["--idle_server_tasks"],
       description: "Run System.gc() when the server is idle",
     },
     {
       name: "--ignore_all_rc_files",
+      exclusiveOn: ["--noignore_all_rc_files"],
       description: "Disables all rc files",
     },
     {
       name: "--noignore_all_rc_files",
+      exclusiveOn: ["--ignore_all_rc_files"],
       description: "Doesn't disables all rc files",
     },
     {
@@ -198,11 +216,13 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "--preemptible",
+      exclusiveOn: ["--nopreemptible"],
       description:
         "If true, the command can be preempted if another command is started",
     },
     {
       name: "--nopreemptible",
+      exclusiveOn: ["--preemptible"],
       description:
         "If true, the command can be preempted if another command is started",
     },
@@ -213,56 +233,68 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "--shutdown_on_low_sys_mem",
+      exclusiveOn: ["--noshutdown_on_low_sys_mem"],
       description:
         "Linux only. If max_idle_secs is set and the build server has been idle for a while, shut down the server when the system is low on free RAM",
     },
     {
       name: "--noshutdown_on_low_sys_mem",
+      exclusiveOn: ["--shutdown_on_low_sys_mem"],
       description: "Opposite of --shutdown_on_low_sys_mem",
     },
     {
       name: "--system_rc",
+      exclusiveOn: ["--nosystem_rc"],
       description: "Whether or not to look for the system-wide bazelrc",
     },
     {
       name: "--nosystem_rc",
+      exclusiveOn: ["--system_rc"],
       description: "Whether or not to look for the system-wide bazelrc",
     },
     {
       name: "--unlimit_coredumps",
+      exclusiveOn: ["--nounlimit_coredumps"],
       description:
         "Raises the soft coredump limit to the hard limit to make coredumps of the server (including the JVM) and the client possible under common conditions",
     },
     {
       name: "--nounlimit_coredumps",
+      exclusiveOn: ["--unlimit_coredumps"],
       description:
         "Raises the soft coredump limit to the hard limit to make coredumps of the server (including the JVM) and the client possible under common conditions",
     },
     {
       name: "--watchfs",
+      exclusiveOn: ["--nowatchfs"],
       description:
         "Use the operating system's file watch service for local changes instead of scanning every file for a change",
     },
     {
       name: "--nowatchfs",
+      exclusiveOn: ["--watchfs"],
       description: "Opposite of --watchfs",
     },
     {
       name: "--windows_enable_symlinks",
+      exclusiveOn: ["--nowindows_enable_symlinks"],
       description:
         "Real symbolic links will be created on Windows instead of file copying",
     },
     {
       name: "--nowindows_enable_symlinks",
+      exclusiveOn: ["--windows_enable_symlinks"],
       description: "Opposite of --windows_enable_symlinks",
     },
     {
       name: "--workspace_rc",
+      exclusiveOn: ["--noworkspace_rc"],
       description:
         "Whether or not to look for the workspace bazelrc file at $workspace/.bazelrc",
     },
     {
       name: "--noworkspace_rc",
+      exclusiveOn: ["--workspace_rc"],
       description: "Opposite of --workspace_rc",
     },
   ],
