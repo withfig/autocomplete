bee0af838a903066db71900e0e446b0b9e000817
lukas <lukashermann28@gmail.com>
2021-09-17 11:09:24 -0400

    prettier

--- dev/pdfunite.ts
+++ dev/pdfunite.ts
@@ -1,6 +1,10 @@
 const completionSpec: Fig.Spec = {
   name: "pdfunite",
   description: "Combine multiple pdfs",
+  options: [
+    { name: "-v", description: "Print copyright and version info" },
+    { name: ["-h", "--help", "-?"], description: "Print usage information" },
+  ],
   args: {
     template: "filepaths",
     isVariadic: true,
