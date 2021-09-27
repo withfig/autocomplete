4d33d79d6939f5a3fe19d23b963b6eac4fcced67
Josh Cook <joshuacook0023@gmail.com>
2021-09-27 13:32:04 -0400

    feat: add R CMD BATCH, COMPILE, SHLIB, INSTALL spec

---
+++ src/R.ts
@@ -0,0 +1,369 @@
+const compileFiles: Fig.Generator = {
+  template: "filepaths",
+  filterTemplateSuggestions: function (paths) {
+    const possibleSuffixes: string[] = [
+      ".c",
+      ".cc",
+      ".cpp",
+      ".m",
+      ".mm",
+      ".M",
+      ".f",
+      ".f90",
+      ".f95",
+    ];
+    return paths
+      .filter((file) => {
+        for (let idx = 0; idx < possibleSuffixes.length; idx++) {
+          const element = possibleSuffixes[idx];
+          if (file.name.endsWith(element)) {
+            return true;
+          }
+        }
+        return false;
+      })
+      .map((file) => {
+        const isRFile = file.name.endsWith(".R");
+
+        return {
+          ...file,
+          priority: isRFile && 76,
+        };
+      });
+  },
+};
+
+const helpAndVersionOptions: Fig.Option[] = [
+  {
+    name: ["--help", "-h"],
+    description: "Print help message and exit",
+  },
+  {
+    name: ["--version", "-v"],
+    description: "Print version info and exit",
+  },
+];
+
+const cleanAndPrecleanOptions: Fig.Option[] = [
+  {
+    name: ["--clean", "-c"],
+    description: "Remove files created during compilation",
+  },
+  {
+    name: "--preclean",
+    description: "Remove files created during a previous run",
+  },
+];
+
+const completionSpec: Fig.Spec = {
+  name: "R",
+  description: "Start R or invoke an R tool",
+  subcommands: [
+    {
+      name: "CMD",
+      description: "Invoke an R tool",
+      subcommands: [
+        {
+          name: "BATCH",
+          description: "Batch Execution of R",
+          options: helpAndVersionOptions.concat([
+            {
+              name: "--no-timing",
+              description: "Do not report the timings",
+            },
+            {
+              name: "--",
+              description: "End processing of options",
+            },
+          ]),
+          args: [
+            {
+              name: "infile",
+              description: "A file with R code to be executed",
+              isOptional: false,
+            },
+            {
+              name: "outfile",
+              description:
+                "A file to which to write output. If not given, the name used is that of infile, with a possible .R extension stripped, and .Rout appended",
+              isOptional: true,
+            },
+          ],
+        },
+        {
+          name: "COMPILE",
+          description:
+            "Compile files for use with R (not available on Windows)",
+          options: helpAndVersionOptions,
+          args: {
+            name: "files",
+            description: "Source files to be compiled",
+            isOptional: false,
+            isVariadic: true,
+            generators: compileFiles,
+          },
+        },
+        {
+          name: "SHLIB",
+          description:
+            "Compile the given source files and then link all specified object files",
+          options: helpAndVersionOptions
+            .concat(cleanAndPrecleanOptions)
+            .concat([
+              {
+                name: ["--output", "-o"],
+                description: "Name for the built library",
+                args: {
+                  name: "LIB",
+                  description: "Full name for the built library",
+                  isOptional: false,
+                },
+              },
+              {
+                name: ["--dry-run", "-n"],
+                description: "Dry run; shows commands that would be used",
+              },
+              {
+                name: "--use-LTO",
+                description: "Use Link-Time Optimization",
+              },
+              {
+                name: "--no-use-LTO",
+                description: "Do not use Link-Time Optimization",
+              },
+              {
+                name: ["--debug", "-d"],
+                description: "Build a debug DLL (Windows only",
+              },
+            ]),
+          args: {
+            name: "files",
+            description:
+              "The object files to be included in the shared object/DLL",
+            isOptional: false,
+            isVariadic: true,
+            generators: compileFiles,
+          },
+        },
+        {
+          name: "INSTALL",
+          description: "Utility for installing add-on packages",
+          options: helpAndVersionOptions
+            .concat(cleanAndPrecleanOptions)
+            .concat([
+              {
+                name: ["--debug", "-d"],
+                description: "Turn on debugging messages",
+              },
+              {
+                name: ["--library", "-l"],
+                description: "Install packages to library tree LIB",
+                requiresEquals: true,
+                args: {
+                  name: "LIB",
+                  description: "Path to the R library tree to install to",
+                  isOptional: false,
+                },
+              },
+              {
+                name: "--no-configure",
+                description: "Do not use the package's configure script",
+              },
+              {
+                name: "--no-docs",
+                description: "Do not install HTML, LaTeX or examples help",
+              },
+              {
+                name: "--html",
+                description: "Build HTML help",
+              },
+              {
+                name: "--no-html",
+                description: "Do not build HTML help",
+              },
+              {
+                name: "--latex",
+                description: "Install LaTeX help",
+              },
+              {
+                name: "--example",
+                description: "Install R code for help examples",
+              },
+              {
+                name: "--fake",
+                description: "Do minimal install for testing purposes",
+              },
+              {
+                name: "--no-lock",
+                description:
+                  "Install on top of any existing installation without using a lock directory",
+              },
+              {
+                name: "--lock",
+                description: "Use a per-library lock directory (default)",
+              },
+              {
+                name: "--pkglock",
+                description:
+                  "Use a per-package lock directory (default for a single package)",
+              },
+              {
+                name: "--build",
+                description: "Build binaries of the installed package(s)",
+              },
+              {
+                name: "--install-tests",
+                description: "Install package-specific tests (if any)",
+              },
+              {
+                name: "--no-R",
+                description: "Suppress installation of the package R code",
+              },
+              {
+                name: "--no-libs",
+                description: "Suppress installation of the package libs",
+              },
+              {
+                name: "--no-data",
+                description: "Suppress installation of the package data",
+              },
+              {
+                name: "--no-help",
+                description: "Suppress installation of the package help",
+              },
+              {
+                name: "--no-demo",
+                description: "Suppress installation of the package demo",
+              },
+              {
+                name: "--no-exec",
+                description: "Suppress installation of the 'exec' directory",
+              },
+              {
+                name: "--no-inst",
+                description: "Suppress installation of the 'inst' directory",
+              },
+              {
+                name: "--no-multiarch",
+                description: "Build only the main architecture",
+              },
+              {
+                name: "--libs-only",
+                description: "Only install the libs directory",
+              },
+              {
+                name: "--data-compress",
+                description: "Compression to be used for lazy-loading of data",
+                args: {
+                  name: "compression",
+                  description:
+                    "Compression method: 'gzip' (default), 'none', 'bzip2' 'xz'",
+                  default: "gzip",
+                },
+              },
+              {
+                name: "--resave-data",
+                description: "Re-save data files as compactly as possible",
+              },
+              {
+                name: "--compact-docs",
+                description: "Re-compress PDF files under inst/doc",
+              },
+              {
+                name: "--with-keep.source",
+                description: "'keep.source' for R code",
+              },
+              {
+                name: "--without-keep.source",
+                description: "Do not use 'keep.source' for R code",
+              },
+              {
+                name: "--with-keep.parse.data",
+                description: "Use 'keep.parse.data' for R code",
+              },
+              {
+                name: "--without-keep.parse.data",
+                description: "Do not 'keep.parse.data' for R code",
+              },
+              {
+                name: "--byte-compile",
+                description: "Byte-compile R code",
+              },
+              {
+                name: "--no-byte-compile",
+                description: "Do not byte-compile R code",
+              },
+              {
+                name: "--staged-install",
+                description:
+                  "Install to a temporary directory and then move to the target directory (default)",
+              },
+              {
+                name: "--no-staged-install",
+                description: "Install directly to the target directory",
+              },
+              {
+                name: "--no-test-load",
+                description: "Skip test of loading installed package",
+              },
+              {
+                name: "--no-clean-on-error",
+                description: "Do not remove installed package on error",
+              },
+              {
+                name: "--merge-multiarch",
+                description:
+                  "Multi-arch by merging (from a single tarball only)",
+              },
+              {
+                name: "--use-vanilla",
+                description: "Do not read any Renviron or Rprofile files",
+              },
+              {
+                name: "--use-LTO",
+                description: "Use Link-Time Optimization",
+              },
+              {
+                name: "--no-use-LTO",
+                description: "Do not use Link-Time Optimization",
+              },
+            ]),
+          args: {
+            name: "packages",
+            description: "Directories with the package sources",
+            isOptional: false,
+            generators: {
+              template: "filepaths",
+              filterTemplateSuggestions: function (paths) {
+                return paths
+                  .filter((file) => {
+                    return (
+                      file.name.endsWith(".tar.gz") || file.name.endsWith("/")
+                    );
+                  })
+                  .map((file) => {
+                    const isRFile = file.name.endsWith(".R");
+
+                    return {
+                      ...file,
+                      priority: isRFile && 76,
+                    };
+                  });
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for R",
+    },
+  ],
+  // Only uncomment if R takes an argument
+  // args: {}
+};
+
+export default completionSpec;
