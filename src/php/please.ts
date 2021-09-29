71a801a545b1bff31611ea5f80da72ddc60723dc
Marty Friedel <1491079+martyf@users.noreply.github.com>
2021-09-29 13:59:53 +1000

    Add `php please` spec for Statamic 3
    Signed-off-by: Marty Friedel <1491079+martyf@users.noreply.github.com>

---
+++ src/php/please.ts
@@ -0,0 +1,52 @@
+const completionSpec: Fig.Spec = {
+  name: "please",
+  description: "Statamic Please command",
+  generateSpec: async (tokens, executeShellCommand) => {
+    var out = await executeShellCommand("php please list --format=json");
+    const subcommands = [];
+
+    try {
+      const commandDefinition = JSON.parse(out);
+
+      commandDefinition.commands.map((command) => {
+        subcommands.push({
+          name: command.name,
+          description: command.description,
+
+          args: Object.keys(command.definition.arguments).map((argumentKey) => {
+            const argument = command.definition.arguments[argumentKey];
+
+            return {
+              name: argument.name,
+              description: argument.description,
+              isOptional: !argument.is_required,
+            };
+          }),
+          options: Object.keys(command.definition.options).map((optionKey) => {
+            const option = command.definition.options[optionKey];
+            const names = [option.name];
+
+            if (option.shortcut !== "") {
+              names.push(option.shortcut);
+            }
+
+            return {
+              name: names,
+              description: option.description,
+            };
+          }),
+        });
+      });
+    } catch (err) {
+      //
+    }
+
+    return {
+      name: "please",
+      debounce: true,
+      subcommands,
+    };
+  },
+};
+
+export default completionSpec;
