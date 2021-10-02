526cd706bad49b62802327b18ae572f3ee647d8f
Josh Cook <joshuacook0023@gmail.com>
2021-10-02 08:26:03 -0400

    feat: R CMD Rd2txt

--- src/R.ts
+++ src/R.ts
@@ -905,6 +905,79 @@ const completionSpec: Fig.Spec = {
             generators: folders,
           },
         },
+        {
+          name: "Rd2txt",
+          description: "Convert R documentation to text",
+          options: helpAndVersionOptions.concat([
+            {
+              name: ["--type", "-t"],
+              description: "Output format type",
+              requiresEquals: true,
+              isRequired: true,
+              args: {
+                name: "type",
+                description: "Output format type",
+                suggestions: ["txt", "html", "latex", "example"],
+                default: "txt",
+              },
+            },
+            {
+              name: "--encoding",
+              description: "Encoding of the output",
+              requiresEquals: true,
+              args: {
+                name: "enc",
+                description: "Output encoding",
+                default: "UTF-8",
+              },
+            },
+            {
+              name: "--package",
+              description: "Package name",
+              requiresEquals: true,
+              args: {
+                name: "package",
+                description: "Package name",
+              },
+            },
+            {
+              name: ["--output", "-o"],
+              description: "The output file",
+              requiresEquals: true,
+              args: {
+                name: "output file",
+                description:
+                  "Name of the output file, '\"\"' to use the input file without '.Rd'",
+                default: '""',
+                suggestions: ['""', "-"],
+              },
+            },
+            {
+              name: ["--os", "--OS"],
+              description: "Set name of OS ('unix' or 'windows')",
+              requiresEquals: true,
+              args: {
+                name: "OS",
+                description: "Name of the OS",
+                suggestions: ["unix", "windows"],
+              },
+            },
+            {
+              name: "--RdMacros",
+              description: "Packages from which to get Rd macros",
+              requiresEquals: true,
+              args: {
+                name: "package list",
+                description: "List of packages",
+              },
+            },
+          ]),
+          args: {
+            name: "file",
+            description: "R doc file to convert",
+            generators: RDocGenerator,
+          },
+        },
       ],
     },
   ],
