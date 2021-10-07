82b1a920f24a8c49b2170a9b56f602649f09662c
Josh Cook <39419448+jhrcook@users.noreply.github.com>
2021-10-07 02:53:33 -0400

    feat: spec for R and Rscript commands for the R programming language (#635)
    * feat: add htop completion spec
    
    * fix: description for delay to highlighting
    
    * style: minor styling changes
    
    * feat: add Rscript completion spec
    
    * feat: add R CMD BATCH, COMPILE, SHLIB, INSTALL spec
    
    * style: remove isOptional=true args and opts
    
    * feat: finish R CMD INSTALL
    
    * feat: R CMD REMOVE
    
    * feat: R CMD build
    
    * feat: R CMD check
    
    * feat: R CMD LINK
    
    * feat: R CMD Rprof
    
    * feat: R CMD Rdconv
    
    * feat: R CMD Rd2pdf
    
    * feat: R CMD Rd2txt
    
    * feat: R CMD Stangle
    
    * feat: R CMD Sweave
    
    * feat: R CMD Rdiff
    
    * feat: R CMD config
    
    * feat: R CMD javareconf
    
    * feat: R CMD rtags
    
    * feat: R options
    
    * fix: refined attributes of some args and aptionals
    - incorporate `exclusiveOn` attribute where appropriate
    - add icons
    - isDangerous when files will be overwritten
    - remove unnecessary `isOptional` from args
    - some style changes
    
    * style: lowercase rprof subcommand name

---
+++ src/rscript.ts
@@ -0,0 +1,105 @@
+const completionSpec: Fig.Spec = {
+  name: "Rscript",
+  description: "Scripting Front-End for R",
+  icon: "https://www.r-project.org/favicon-32x32.png",
+  options: [
+    {
+      name: "-e",
+      description: "R expression to run",
+      isRepeatable: true,
+      args: {
+        name: "expression",
+        description: "R expression",
+      },
+    },
+    {
+      name: "--help",
+      description: "Print usage and exit",
+      icon: "❔",
+    },
+    {
+      name: "--version",
+      description: "Print version and exit",
+    },
+    {
+      name: "--verbose",
+      description: "Print information on progress",
+      icon: "📣",
+    },
+    {
+      name: "--no-echo",
+      description: "Run as quietly as possible",
+      icon: "🙊",
+    },
+    {
+      name: "--no-restore",
+      description: "Don't restore anything",
+    },
+    {
+      name: "--save",
+      description: "Do save workspace at the end of the session",
+      icon: "💾",
+    },
+    {
+      name: "--no-environ",
+      description: "Don't read the site and user environment files",
+    },
+    {
+      name: "--no-site-file",
+      description: "Don't read the site-wide Rprofile",
+    },
+    {
+      name: "--no-init-file",
+      description: "Don't read the user R profile",
+    },
+    {
+      name: "--restore",
+      description: "Do restore previously saved objects at startup",
+    },
+    {
+      name: "--vanilla",
+      description:
+        "Combine --no-save, --no-restore, --no-site-file --no-init-file and --no-environ",
+    },
+    {
+      name: "--default-packages",
+      description: "Comma separated list of default packages",
+      requiresEquals: true,
+      icon: "📦",
+      args: {
+        name: "packages",
+        description: "A comma-separated set of package names, or 'NULL'",
+      },
+    },
+  ],
+  args: [
+    {
+      name: "file",
+      description: "R script to run",
+      isScript: true,
+      generators: {
+        template: "filepaths",
+        filterTemplateSuggestions: function (paths) {
+          return paths
+            .filter((file) => {
+              return file.name.endsWith(".R") || file.name.endsWith("/");
+            })
+            .map((file) => {
+              const isRFile = file.name.endsWith(".R");
+              return {
+                ...file,
+                priority: isRFile && 76,
+              };
+            });
+        },
+      },
+    },
+    {
+      name: "args",
+      description: "Arguments to pass to the script",
+      isVariadic: true,
+    },
+  ],
+};
+
+export default completionSpec;
