14dea30b43cc269a4cf3f768b2536d58797a59c8
Sanonz <sanonz@126.com>
2021-10-11 14:29:27 +0800

    Optimize assimp

--- src/assimp.ts
+++ src/assimp.ts
@@ -1,29 +1,43 @@
 const helpOptions: Fig.Option[] = [
   {
     name: ["-h", "--help"],
-    description: "Detailed help on a command",
+    description: "Optional. Detailed help on a command",
   },
 ];
 
 const commonOptions: Fig.Option[] = [
   {
     name: "-cfast",
-    description: "Fast post processing preset, runs just a few important steps",
+    description:
+      "Optional. Fast post processing preset, runs just a few important steps",
   },
   {
     name: "-cdefault",
-    description: "Default post processing: runs all recommended steps",
+    description:
+      "Optional. Default post processing: runs all recommended steps",
   },
   {
     name: "-cfull",
-    description: "Fires almost all post processing steps",
+    description: "Optional. Fires almost all post processing steps",
   },
 ];
 
-const suggestionGenerator: Fig.Generator = {
+const importExtGenerator: Fig.Generator = {
   script: "assimp listext",
   postProcess: function (out) {
     return out.split(";").map((ext) => {
+      return {
+        name: ext,
+        description: "Extension",
+      };
+    });
+  },
+};
+
+const exportExtGenerator: Fig.Generator = {
+  script: "assimp listexport",
+  postProcess: function (out) {
+    return out.split("\n").map((ext) => {
       return {
         name: ext,
         description: "Extension",
@@ -37,41 +51,58 @@ const completionSpec: Fig.Spec = {
   description: "Open Asset Import Library",
   subcommands: [
     {
-      name: "info",
-      description: "Quick file stats",
-      args: {
-        name: "file",
-        description: "Print basic structure of a 3D model",
-        template: "filepaths",
-        isVariadic: true,
-      },
-      options: [
-        {
-          name: ["-r", "--raw"],
-          description: "No postprocessing, do a raw import",
-        },
-        {
-          name: ["-v", "--verbose"],
-          description: "Print verbose info such as node transform data",
-        },
-        {
-          name: ["-s", "--silent"],
-          description: "Print only minimal info",
-        },
-        ...helpOptions,
-      ],
-    },
-    {
       name: "listext",
       description: "List all known file extensions available for import",
+    },
+    {
+      name: "listexport",
+      description: "List all supported export formats",
     },
     {
       name: "knowext",
       description: "List all known file extensions available for import",
       args: {
         name: "extension",
-        description: "Check whether a particular file extension is known by us",
+        description:
+          "Required. Check whether a particular file extension is known by us",
+        generators: importExtGenerator,
       },
+    },
+    {
+      name: "exportinfo",
+      description: "Show basic information on a specific export format",
+      args: {
+        name: "extension",
+        description: "Required. Specific file extension",
+        generators: exportExtGenerator,
+      },
+    },
+    {
+      name: "info",
+      description:
+        "Load a model file and print basic statistics. Full postprocessing is applied unless the `-r` switch is specified",
+      args: {
+        name: "file",
+        description: "Required. Print basic structure of a 3D model",
+        template: "filepaths",
+        isVariadic: true,
+      },
+      options: [
+        {
+          name: ["-r", "--raw"],
+          description: "Optional. No postprocessing, do a raw import",
+        },
+        {
+          name: ["-v", "--verbose"],
+          description:
+            "Optional. Print verbose info such as node transform data",
+        },
+        {
+          name: ["-s", "--silent"],
+          description: "Optional. Print only minimal info",
+        },
+        ...helpOptions,
+      ],
     },
     {
       name: "export",
@@ -85,34 +116,21 @@ const completionSpec: Fig.Spec = {
         {
           name: "out",
           description:
-            "Relative or absolute path to write the output export to",
+            "Optional. Relative or absolute path to write the output export to",
           template: "folders",
-          isOptional: true,
         },
       ],
       options: [
         {
           name: ["-f", "--format"],
-          description: "No postprocessing, do a raw import",
+          description: "Optional. No postprocessing, do a raw import",
           args: {
             name: "format",
-            generators: suggestionGenerator,
+            generators: exportExtGenerator,
           },
         },
         ...helpOptions,
       ],
-    },
-    {
-      name: "listexport",
-      description: "List all supported export formats",
-    },
-    {
-      name: "exportinfo",
-      description: "Show basic information on a specific export format",
-      args: {
-        name: "extension",
-        description: "Specific file extension",
-      },
     },
     {
       name: "extract",
@@ -126,31 +144,37 @@ const completionSpec: Fig.Spec = {
         {
           name: "out",
           description:
-            "Relative or absolute path to write the output images to",
+            "Optional. Relative or absolute path to write the output images to",
           template: "folders",
           isOptional: true,
         },
       ],
       options: [
         {
-          name: ["-ba", "–-bmp-with-alpha"],
-          description: "Writes BMP's with alpha channel",
-        },
-        {
           name: ["-t", "--texture"],
           description: "Zero-based index of the texture to be extracted",
           args: {
             name: "index",
-            generators: suggestionGenerator,
           },
         },
         {
+          name: ["-ba", "–-bmp-with-alpha"],
+          description:
+            "Optional. Specifies whether output BMPs contain an alpha channel or not",
+        },
+        {
           name: ["-f", "--format"],
-          description: "No postprocessing, do a raw import",
+          description:
+            "Optional. Specifies the output file format. Supported formats are BMP and TGA. The default value is BMP (if a full output filename is specified, the output file format is taken from its extension)",
           args: {
             name: "format",
-            generators: suggestionGenerator,
+            suggestions: ["bmp", "tga"],
           },
+        },
+        {
+          name: ["-s", "–-nosuffix"],
+          description:
+            "Optional. Prevents the tool from adding the _img<n> suffix to all filenames. This option must be specified together with -t to ensure that just one image is written",
         },
         ...commonOptions,
         ...helpOptions,
@@ -162,13 +186,13 @@ const completionSpec: Fig.Spec = {
       args: [
         {
           name: "model",
-          description: "Relative or absolute path to the input model",
+          description: "Required. Relative or absolute path to the input model",
           template: "filepaths",
         },
         {
           name: "out",
           description:
-            "Relative or absolute path to write the output dump to. If it is omitted, the dump is written to '<model>-dump.txt'",
+            "Optional. Relative or absolute path to write the output dump to. If it is omitted, the dump is written to '<model>-dump.txt'",
           template: "folders",
           isOptional: true,
         },
@@ -176,15 +200,17 @@ const completionSpec: Fig.Spec = {
       options: [
         {
           name: ["-b", "--binary"],
-          description: "Binary output",
+          description:
+            "Optional. If true, the dump is written in binary format",
         },
         {
           name: ["-s", "--short"],
-          description: "Shortened",
+          description:
+            "Optional. If true, the dump is shortened to include only min/max values for all vertex components and animation channels",
         },
         {
           name: ["-z", "--compressed"],
-          description: "Compressed",
+          description: "Optional. If true, Compressed",
         },
         ...commonOptions,
         ...helpOptions,
@@ -196,12 +222,12 @@ const completionSpec: Fig.Spec = {
       args: [
         {
           name: "actual",
-          description: "Mini dump now",
+          description: "Required. Mini dump now",
           template: "filepaths",
         },
         {
           name: "expected",
-          description: "Archived dump from some point in the past",
+          description: "Required. Archived dump from some point in the past",
           template: "filepaths",
         },
       ],
