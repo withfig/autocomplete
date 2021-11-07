7fa2551465d27f2c185bad76717e98d29787fb31
Tom Lienard <quiibzdev@gmail.com>
2021-11-07 20:09:17 +0100

    feat(ni): add ni specs (#712)
    * feat(ni): add ni specs
    
    * fix: nrm description
    
    * feat: add --help option

---
+++ src/ni.ts
@@ -0,0 +1,56 @@
+import { npmSearchGenerator } from "./npm";
+
+const completionSpec: Fig.Spec = {
+  name: "ni",
+  description: "Use the right package manager - install",
+  options: [
+    {
+      name: "-g",
+      description:
+        "Operates in 'global' mode, so that packages are installed into the prefix folder instead of the current working directory",
+    },
+    {
+      name: "-D",
+      description: "Package will appear in your `devDependencies`",
+    },
+    {
+      name: "-P",
+      description: "Save package to your `peerDependencies`",
+    },
+    {
+      name: "-O",
+      description: "Save package to your `optionalDependencies`",
+    },
+    {
+      name: "--frozen",
+      description: "Don't generate a lockfile and fail if an update is needed",
+    },
+    {
+      name: "-C",
+      description: "Change directory",
+      args: [
+        {
+          name: "directory",
+          description: "The directory to move",
+          template: "folders",
+        },
+        {
+          name: "target",
+          description: "The target directory",
+          template: "folders",
+        },
+      ],
+    },
+    {
+      name: ["-h", "--help"],
+      description: "Output usage information",
+    },
+  ],
+  args: {
+    name: "package",
+    generators: npmSearchGenerator,
+    debounce: true,
+    isVariadic: true,
+  },
+};
+export default completionSpec;
