d395207cb6ba0e8d0c58359886d4395b400c9a1f
José Luiz Rodrigues <zeluizr@me.com>
2021-11-08 14:48:06 -0300

    array isn't necessary

--- src/supabase.ts
+++ src/supabase.ts
@@ -15,27 +15,27 @@ const completionSpec: Fig.Spec = {
       name: "init",
       description:
         "This command will create a .supabase folder which holds all the configuration for developing your project locally. You don't need to check this into version control",
-      options: [...commonOptions],
+      options: commonOptions,
     },
     {
       name: "start",
       description:
         "This command uses Docker Compose to start all the open source services of Supabase. This command will take a while to run, there are a lot of services to build",
-      options: [...commonOptions],
+      options: commonOptions,
     },
     {
       name: "stop",
       description:
         "When you are finished with Supabase, run supabase stop to stop the Docker services",
-      options: [...commonOptions],
+      options: commonOptions,
     },
     {
       name: "eject",
       description:
         "Run in any folder to create a docker folder with a docker-compose.yml. This is useful for self-hosting or adding custom configuration",
-      options: [...commonOptions],
+      options: commonOptions,
     },
   ],
-  options: [...commonOptions],
+  options: commonOptions,
 };
 export default completionSpec;
