61177b97afe86e83dccda25426a73fa125c19343
Mohammad Farouk <33427443+FaroukZeino@users.noreply.github.com>
2021-11-14 18:49:15 +0300

    feat: add vapor completion spec. (#721)
    * feat: add vapor completion spec.
    
    * Changed vaporCommands to array of strings.
    
    * Removed Fig.Suggestion type.
    
    * run lint:fix
    
    * Removed comments.

---
+++ src/vapor.ts
@@ -0,0 +1,69 @@
+const vaporCommands = ["clean", "heroku", "new", "run", "supervisor", "xcode"];
+
+const completionSpec: Fig.Spec = {
+  name: "vapor",
+  description: "Vapor Toolbox (Server-side Swift web framework)",
+  subcommands: [
+    {
+      name: "build",
+      description: "Builds an app in the console",
+    },
+    {
+      name: "clean",
+      description: "Cleans temporary files",
+    },
+    {
+      name: "heroku",
+      description: "Commands for working with Heroku",
+      subcommands: [
+        {
+          name: "init",
+          description: "Configures app for deployment to Heroku",
+        },
+        {
+          name: "push",
+          description: "Deploys app to Heroku",
+        },
+      ],
+    },
+    {
+      name: "run",
+      description:
+        "Runs an app from the console. Equivalent to `swift run --enable-test-discovery Run`",
+    },
+    {
+      name: "supervisor",
+      description: "Commands for working with Supervisord",
+      subcommands: [
+        {
+          name: "init",
+          description: "Creates a Supervisor entry for the current project",
+        },
+        {
+          name: "restart",
+          description: "Restarts current project in Supervisor",
+        },
+        {
+          name: "update",
+          description: "Updates Supervisor entry for current project",
+        },
+      ],
+    },
+    {
+      name: "xcode",
+      description: "Opens an app in Xcode",
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for vapor",
+      args: {
+        name: "command",
+        description: "Display usage information about the specified command",
+        suggestions: vaporCommands,
+      },
+    },
+  ],
+};
+export default completionSpec;
