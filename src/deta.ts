5fd614c6302f77e0d2ad455f208fadf71e09bafd
Bogdan Covrig <bogcov@gmail.com>
2021-11-19 00:29:28 +0100

    Deta spec: add details, watch, auth, pull, clone, upadte, visor, run, cron commands

--- src/deta.ts
+++ src/deta.ts
@@ -2,9 +2,13 @@
 // This spec is following the references
 // and descriptions from the Deta CLI Docs
 // https://docs.deta.sh/docs/cli/commands
+
+// Deta CLI version: v1.3.0-beta
+// https://docs.deta.sh/docs/cli/releases#v130-beta
+
 const completionSpec: Fig.Spec = {
   name: "deta",
-  description: "Deta is a CLI for managing Deta Micros",
+  description: "Deta CLI for managing Deta Micros",
   subcommands: [
     {
       name: "login",
@@ -60,7 +64,7 @@ const completionSpec: Fig.Spec = {
       description: " Create a new Deta Micro",
       args: {
         name: "path",
-        description: "Path to project directory",
+        description: "Path to new directory for the micro",
         isOptional: true,
       },
       options: [
@@ -102,6 +106,322 @@ const completionSpec: Fig.Spec = {
         },
       ],
     },
+    {
+      name: "deploy",
+      description: "Deploy new code to a Deta Micro",
+      args: {
+        name: "path",
+        description: "Path to project directory",
+        template: "folders",
+        isOptional: true,
+      },
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for deploy",
+        },
+      ],
+    },
+    {
+      name: "details",
+      description: "Get detailed information about a specific Deta micro",
+      args: {
+        name: "path",
+        description: "Path to project directory",
+        template: "folders",
+        isOptional: true,
+      },
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for details",
+        },
+      ],
+    },
+    {
+      name: "watch",
+      description: "Auto-deploy locally saved changes in real time to your Deta micro",
+      args: {
+        name: "path",
+        description: "Path to project directory",
+        template: "folders",
+        isOptional: true,
+      },
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for watch",
+        },
+      ],
+    },
+    {
+      name: "auth",
+      description: "Change auth settings for a Deta Micro",
+      subcommands:[
+        {
+          name: "disable",
+          description: "Disable HTTP Auth for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for auth disable",
+            },
+          ],
+        },
+        {
+          name: "enable",
+          description: "Enable HTTP Auth for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for auth enable",
+            },
+          ],
+        },
+        {
+          name: "create-api-key",
+          description: "Create an API key for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for auth create-api-key",
+            },
+            {
+              name: ["-d"],
+              description: "Set the api-key description",
+              args: {
+                name: "description",
+                description: "The api-key description"
+              }
+            },
+            {
+              name: ["-n"],
+              description: "Set the api-key name",
+              isRequired: true,
+              args: {
+                name: "name",
+                description: "The api-key name",
+              }
+            },
+            {
+              name: ["-o"],
+              description: "Set the api-key output file",
+              args: {
+                name: "outfile",
+                description: "The api-key output file",
+                template: "filepaths"
+              }
+            },
+          ],
+        },
+        {
+          name: "delete-api-key",
+          description: "Delete an API key for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for auth delete-api-key",
+            },
+            {
+              name: ["-n"],
+              description: "Set the api-key name",
+              isRequired: true,
+              args: {
+                name: "name",
+                description: "The api-key name",
+              }
+            },
+          ],
+        },
+      ],
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for auth",
+        },
+      ],
+    },
+    {
+      name: "pull",
+      description: "Pull the latest deployed code of a Deta Micro to your local machine",
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for pull",
+        },
+        {
+          name: ["-f"],
+          description: "Force the overwrite of existing files",
+        },
+      ],
+    },
+    {
+      name: "clone",
+      description: "Clone a Deta Micro",
+      args: {
+        name: "path",
+        description: "Path to new directory for the clone",
+        isOptional: true,
+      },
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for clone",
+        },
+        {
+          name: ["-n"],
+          description: "The name of the micro to be cloned",
+          isRequired: true,
+          args: {
+            name: "name",
+            description: "Name of the micro",
+          }
+        },
+        {
+          name: ["-p"],
+          description: "The name of the project with the micro to be cloned",
+          args: {
+            name: "project",
+            description: "Name of the project",
+          }
+        },
+      ],
+    },
+    {
+      name: "update",
+      description: "Update a Deta Micro's name or environment variables",
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for pull",
+        },
+        {
+          name: ["-n"],
+          description: "The new name of the micro",
+          args: {
+            name: "name",
+            description: "New name for the micro"
+          }
+        },
+        {
+          name: ["-r"],
+          description: "The new runtime of the micro",
+          args: {
+            name: "runtime",
+            description: "New runtime for the micro: python3.7, python3.9, node12, node14"
+          }
+        },
+        {
+          name: ["-e"],
+          description: "The new env file of the micro",
+          args: {
+            name: "env",
+            description: "Path to env file",
+            template: "filepaths"
+          }
+        },
+      ],
+    },
+    {
+      name: "visor",
+      description: "Change the Visor settings for a Deta Micro",
+      subcommands:[
+        {
+          name: "open",
+          description: "Open Micro's visor page in the browser",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for visor open",
+            },
+          ],
+        },
+        {
+          name: "enable",
+          description: "Enable Visor for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for visor enable",
+            },
+          ],
+        },
+        {
+          name: "disable",
+          description: "Disable Visor for a Deta Micro",
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for visor disable",
+            },
+          ],
+        },
+      ],
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for visor",
+        },
+      ],
+    },
+    {
+      name: "run",
+      description: "Run a Deta Micro from the CLI",
+      args: {
+        name: "action",
+        description: "The action to be performed on the micro. See docs for full examples and details"
+      },
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for run",
+        },
+        {
+          name: ["-l"],
+          description: "Show the micro logs",
+        },
+      ],
+    },
+    {
+      name: "cron",
+      description: "Change cron settings for a Deta Micro",
+      subcommands:[
+        {
+          name: "set",
+          description: "Set Deta Micro to run on a schedule",
+          args: {
+            name: "expression",
+            description: "The cron expression to be set"
+          },
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for cron set",
+            },
+          ],
+        },
+        {
+          name: "remove",
+          description: "Remove a schedule from a Deta Micro",
+          args: {
+            name: "expression",
+            description: "The cron expression to be removed"
+          },
+          options: [
+            {
+              name: ["-h"],
+              description: "Show help for cron remove",
+            },
+          ],
+        },
+      ],
+      options: [
+        {
+          name: ["-h"],
+          description: "Show help for cron",
+        },
+      ],
+    },
   ],
   options: [
     {
@@ -109,8 +429,6 @@ const completionSpec: Fig.Spec = {
       description: "Show help for deta",
     },
   ],
-  // Only uncomment if deta takes an argument
-  // args: {}
 };
 
 export default completionSpec;
