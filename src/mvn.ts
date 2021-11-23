dd5387b6087fc1cc8dbb5a667b49c8e550955816
zp4rker <iam@zp4rker.com>
2021-11-23 12:43:03 +1100

    added default phases to suggestions

--- src/mvn.ts
+++ src/mvn.ts
@@ -219,6 +219,41 @@ const completionSpec: Fig.Spec = {
   args: {
     name: "goal/phase",
     isVariadic: true,
+    suggestions: [
+      // clean lifecycle
+      "pre-clean",
+      "clean",
+      "post-clean",
+      // default lifecycle
+      "validate",
+      "initialize",
+      "generate-sources",
+      "process-sources",
+      "generate-resources",
+      "process-resources",
+      "compile",
+      "process-classes",
+      "generate-test-sources",
+      "process-test-sources",
+      "generate-test-resources",
+      "process-test-resources",
+      "test-compile",
+      "process-test-classes",
+      "test",
+      "prepare-package",
+      "package",
+      "pre-integration-test",
+      "integration-test",
+      "post-integration-test",
+      "verify",
+      "install",
+      "deploy",
+      // site lifecycle
+      "pre-site",
+      "site",
+      "post-site",
+      "site-deploy",
+    ],
   },
 };
 export default completionSpec;
