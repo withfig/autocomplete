5ff337ccf1d9ddd290db1a8a8490bba91604542b
Bogdan Covrig <bogcov@gmail.com>
2021-12-09 12:46:57 +0100

    StepZen spec: add login command

--- src/stepzen.ts
+++ src/stepzen.ts
@@ -4,21 +4,12 @@ const completionSpec: Fig.Spec = {
     "The StepZen CLI is the primary way to build, deploy and test your schemas on StepZen",
   subcommands: [
     {
-      name: "my_subcommand",
-      description: "Example subcommand",
-      subcommands: [
-        {
-          name: "my_nested_subcommand",
-          description:
-            "Nested subcommand, example usage: 'stepzen my_subcommand my_nested_subcommand'",
-        },
-      ],
-    },
-  ],
-  options: [
-    {
-      name: ["--help", "-h"],
-      description: "Show help for stepzen",
+      name: "help",
+      description: "Display help for StepZen",
+      args: {
+        name: "command",
+        description: "Command name for which to display help",
+      },
     },
   ],
   // Only uncomment if stepzen takes an argument
