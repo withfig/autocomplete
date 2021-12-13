344acdb405c4a0f556aacd794b7e7146c1507da9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-13 16:47:30 +0200

    feat: add keytool -changealias options

--- src/keytool.ts
+++ src/keytool.ts
@@ -140,7 +140,95 @@ const completionSpec: Fig.Spec = {
     {
       name: "-changealias",
       description: "Changes an entry's alias",
-      options: [...commonOptions],
+      options: [
+        ...commonOptions,
+        {
+          name: "-alias",
+          description: "Alias name of the entry to process",
+          args: {
+            name: "alias",
+          },
+        },
+        {
+          name: "-destalias",
+          description: "Destination alias",
+          args: {
+            name: "alias",
+          },
+        },
+        {
+          name: "-keypass",
+          description: "Key password",
+          args: {
+            name: "arg",
+          },
+        },
+        {
+          name: "-keystore",
+          description: "Keystore name",
+          args: {
+            name: "keystore",
+          },
+        },
+        {
+          name: "-cacerts",
+          description: "Access the cacerts keystore",
+        },
+        {
+          name: "-storepass",
+          description: "Keystore password",
+          args: {
+            name: "arg",
+          },
+        },
+        {
+          name: "-storetype",
+          description: "Keystore type",
+          args: {
+            name: "type",
+          },
+        },
+        {
+          name: "-providername",
+          description: "Provider name",
+          args: {
+            name: "name",
+          },
+        },
+        {
+          name: "-addprovider",
+          description: "Add security provider by name (e.g. SunPKCS11)",
+          args: {
+            name: "name",
+          },
+        },
+        {
+          name: "-providerclass",
+          description: "Add security provider by fully-qualified class name",
+          args: {
+            name: "class",
+          },
+        },
+        {
+          name: "-providerarg",
+          description: "Configure argument for -addprovider or -providerclass",
+          args: {
+            name: "arg",
+          },
+          dependsOn: ["-addprovider", "-providerclass"],
+        },
+        {
+          name: "-providerpath",
+          description: "Provider classpath",
+          args: {
+            name: "list",
+          },
+        },
+        {
+          name: "-protected",
+          description: "Password through protected mechanism",
+        },
+      ],
     },
     {
       name: "-delete",
