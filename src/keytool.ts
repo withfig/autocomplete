adfd20899f59da647e3a5a0cbd19fb605d03f39c
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-13 08:18:40 +0200

    feat: add common options to all keytool subcommands

--- src/keytool.ts
+++ src/keytool.ts
@@ -1,3 +1,14 @@
+const commonOptions: Fig.Option[] = [
+  {
+    name: ["-h", "-?", "--help", "-help"],
+    description: "Show help message",
+  },
+  {
+    name: "-v",
+    description: "Verbose output",
+  },
+];
+
 const completionSpec: Fig.Spec = {
   name: "keytool",
   description: "Key and Certificate Management Tool",
@@ -18,66 +29,82 @@ const completionSpec: Fig.Spec = {
     {
       name: "-certreq",
       description: "Generates a certificate request",
+      options: [...commonOptions],
     },
     {
       name: "-changealias",
       description: "Changes an entry's alias",
+      options: [...commonOptions],
     },
     {
       name: "-delete",
       description: "Deletes an entry",
+      options: [...commonOptions],
     },
     {
       name: "-exportcert",
       description: "Exports certificate",
+      options: [...commonOptions],
     },
     {
       name: "-genkeypair",
       description: "Generate a key pair",
+      options: [...commonOptions],
     },
     {
       name: "-genseckey",
       description: "Generates a secret key",
+      options: [...commonOptions],
     },
     {
       name: "-gencert",
       description: "Generates certificate from a certificate request",
+      options: [...commonOptions],
     },
     {
       name: "-importcert",
       description: "Imports a certificate or a certificate chain",
+      options: [...commonOptions],
     },
     {
       name: "-importpass",
       description: "Imports a password",
+      options: [...commonOptions],
     },
     {
       name: "-importkeystore",
       description: "Imports one or all entries from another keystore",
+      options: [...commonOptions],
     },
     {
       name: "-keypasswd",
       description: "Changes the key password of an entry",
+      options: [...commonOptions],
     },
     {
       name: "-list",
       description: "Lists entries in a keystore",
+      options: [...commonOptions],
     },
     {
       name: "-printcert",
       description: "Prints the content of a certificate",
+      options: [...commonOptions],
     },
     {
       name: "-printcertreq",
       description: "Prints the content of a certificate request",
+      options: [...commonOptions],
     },
     {
       name: "-printcrl",
       description: "Prints the content of a CRL file",
+      options: [...commonOptions],
     },
     {
       name: "-storepasswd",
       description: "Changes the store password of a keystore",
+      options: [...commonOptions],
     },
   ],
 };
