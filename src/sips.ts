dbef416063c86215042ea1fd5652801a2b7f5e7e
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-16 17:26:59 +0200

    feat: add basic sips support

---
+++ src/sips.ts
@@ -0,0 +1,10 @@
+const completionSpec: Fig.Spec = {
+  name: "sips",
+  description: "Scriptable image processing system",
+  args: {
+    name: "file",
+    template: "filepaths",
+  },
+};
+
+export default completionSpec;
