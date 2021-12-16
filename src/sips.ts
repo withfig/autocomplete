8b10b9487db2d3d433b4db6717e61ae558956218
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-16 17:31:23 +0200

    feat: add sips help, version & debug options

--- src/sips.ts
+++ src/sips.ts
@@ -5,6 +5,32 @@ const completionSpec: Fig.Spec = {
     name: "file",
     template: "filepaths",
   },
+  options: [
+    {
+      name: "--debug",
+      description: "Enable debugging output",
+    },
+    {
+      name: ["-h", "--help"],
+      description: "Show help",
+    },
+    {
+      name: ["-H", "--helpProperties"],
+      description: "Show help for properties",
+    },
+    {
+      name: "--man",
+      description: "Generate man pages",
+    },
+    {
+      name: ["-v", "--version"],
+      description: "Show the version",
+    },
+    {
+      name: "--formats",
+      description: "Show the read/write formats",
+    },
+  ],
 };
 
 export default completionSpec;
