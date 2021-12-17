92fea5026d5e858f9884c5e4d16749bd39445144
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-12-17 06:29:01 +0200

    feat: complete sips options

--- src/sips.ts
+++ src/sips.ts
@@ -133,6 +133,134 @@ const completionSpec: Fig.Spec = {
       description: "Repair any profile problems and log output to stdout",
     },
     {
+      name: ["-e", "--embedProfile"],
+      description: "Embed profile in image",
+      args: {
+        name: "profile",
+      },
+    },
+    {
+      name: ["-E", "--embedProfileIfNone"],
+      description: "Embed profile in image only if image doen't have a profile",
+      args: {
+        name: "profile",
+      },
+    },
+    {
+      name: ["-m", "--matchTo"],
+      description: "Color match image to profile",
+      args: {
+        name: "profile",
+      },
+    },
+    {
+      name: ["-M", "--matchToWithIntent"],
+      description:
+        "Color match image to profile with rendering intent perceptual | relative | saturation | absolute",
+      args: [
+        {
+          name: "profile",
+        },
+        {
+          name: "intent",
+          suggestions: ["perceptual", "relative", "saturation", "absolute"],
+        },
+      ],
+    },
+    {
+      name: "--deleteColorManagementProperties",
+      description:
+        "Delete color management properties in TIFF, PNG, and EXIF dictionaries",
+    },
+    {
+      name: ["-r", "--rotate"],
+      args: {
+        name: "degreesCW",
+      },
+    },
+    {
+      name: ["-f", "--flip"],
+      args: {
+        name: "axis",
+        suggestions: ["horizontal", "vertical"],
+      },
+    },
+    {
+      name: ["-c", "--cropToHeightWidth"],
+      description: "Crop image to fit specified size",
+      args: [
+        {
+          name: "pixelsH",
+        },
+        {
+          name: "pixelsW",
+        },
+      ],
+    },
+    {
+      name: ["-p", "--padToHeightWidth"],
+      description: "Pad image with pixels to fit specified size",
+      args: [
+        {
+          name: "pixelsH",
+        },
+        {
+          name: "pixelsW",
+        },
+      ],
+    },
+    {
+      name: "-padColor",
+      description:
+        "Use this color when padding.  White=FFFFFF, Red=FF0000, Default=Black=000000",
+      args: {
+        name: "hexcolor",
+      },
+    },
+    {
+      name: ["-z", "--resampleHeightWidth"],
+      description:
+        "Resample image at specified size. Image apsect ratio may be altered",
+      args: [
+        {
+          name: "pixelsH",
+        },
+        {
+          name: "pixelsW",
+        },
+      ],
+    },
+    {
+      name: "--resampleWidth",
+      description: "Resample image to specified width",
+      args: {
+        name: "pixelsW",
+      },
+    },
+    {
+      name: "--resampleHeight",
+      description: "Resample image to specified height",
+      args: {
+        name: "pixelsH",
+      },
+    },
+    {
+      name: ["-Z", "--resampleHeightWidthMax"],
+      description:
+        "Resample image so height and width aren't greater than specified size",
+      args: {
+        name: "pixelsWH",
+      },
+    },
+    {
+      name: ["-i", "--addIcon"],
+      description: "Add a Finder icon to image file",
+      args: {
+        name: "icon",
+        template: "filepaths",
+      },
+    },
+    {
       name: "--debug",
       description: "Enable debugging output",
     },
