0d81c69c3eda93e7fdb809b60643f1c124689014
Nicolás Massardo <nmassardot@uc.cl>
2021-12-29 18:02:56 -0300

    feat(eb): add init autocomplete

--- src/eb.ts
+++ src/eb.ts
@@ -1,6 +1,16 @@
 const completionSpec: Fig.Spec = {
   name: "eb",
   description: "AWS Elastic Beanstalk",
+  subcommands: [
+    {
+      name: "init",
+      description: "Initialize a new AWS Elastic Beanstalk application",
+      args: {
+        name: "name",
+        description: "The name of the application",
+      },
+    },
+  ],
 };
 
 export default completionSpec;
