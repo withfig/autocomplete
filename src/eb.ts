381e7407b0c71468d89d823e8e3b91a6cd35e767
Nicolás Massardo <nmassardot@uc.cl>
2021-12-29 18:03:11 -0300

    feat(eb): add init options

--- src/eb.ts
+++ src/eb.ts
@@ -9,6 +9,38 @@ const completionSpec: Fig.Spec = {
         name: "name",
         description: "The name of the application",
       },
+      options: [
+        {
+          name: ["-i", "--interactive"],
+          description:
+            "Forces EB CLI to prompt you to provide a value for every eb init command option",
+        },
+        {
+          name: ["-k", "--keyname"],
+          description: "The name of the AWS EC2 key pair",
+          args: {
+            name: "keyname",
+          },
+        },
+        {
+          name: "--modules",
+          description: "List of child directories to initialize",
+        },
+        {
+          name: ["-p", "--plataform"],
+          description: "The platform version to use",
+          args: {
+            name: "plataform-version",
+          },
+        },
+        {
+          name: "--tags",
+          description: "List of tags to apply to the application",
+          args: {
+            name: "key=value",
+          },
+        },
+      ],
     },
   ],
 };
