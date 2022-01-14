19e04e9d458cbfff52f790632c9c55733a9f1092
Josh Cook <joshuacook0023@gmail.com>
2022-01-14 17:49:55 -0500

    feat: add yank completion spec

---
+++ src/yank.ts
@@ -0,0 +1,43 @@
+const completionSpec: Fig.Spec = {
+  name: "yank",
+  description: "Yank terminal output to clipboard",
+  options: [
+    {
+      name: "-i",
+      description: "Ignore case differences between pattern and the input",
+    },
+    { name: "-l", description: "Use the default delimiters except for space" },
+    { name: "-x", description: "Use alternate screen" },
+    { name: "-v", description: "Print the version" },
+    {
+      name: "-d",
+      description:
+        "All input characters not present in delim will be recognized as fields",
+      args: {
+        name: "delim",
+        description: "Custom delimiters",
+        isOptional: false,
+      },
+    },
+    {
+      name: "-g",
+      description: "Use pattern to recognize fields",
+      args: {
+        name: "pattern",
+        description: "Pattern to recognize fields",
+        isOptional: false,
+      },
+    },
+    {
+      name: "--",
+      description: "Use a command as the yank command",
+      args: {
+        name: "command",
+        description: "Command to use as the yank command",
+        isOptional: false,
+        isVariadic: true,
+      },
+    },
+  ],
+};
+export default completionSpec;
