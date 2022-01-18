8bba8d4fc1ccbfb5cca6a56f73181c80eca3f12d
Nicolas Massardo <nmassardot@uc.cl>
2022-01-17 23:31:09 -0300

    feat(aws/apprunner): add create connection subcommand

--- src/aws/apprunner.ts
+++ src/aws/apprunner.ts
@@ -1,6 +1,12 @@
 const completionSpec: Fig.Spec = {
   name: "apprunner",
   description: "AWS apprunner commands",
+  subcommands: [
+    {
+      name: "create-connection",
+      description: "Create an app runner connection resource",
+    },
+  ],
 };
 
 export default completionSpec;
