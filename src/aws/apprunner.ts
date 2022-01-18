b8a7c568055da23b23d611aba83b8ac916baf243
Nicolas Massardo <nmassardot@uc.cl>
2022-01-17 23:31:45 -0300

    feat(apprunner/create-connection): add connection name option

--- src/aws/apprunner.ts
+++ src/aws/apprunner.ts
@@ -5,6 +5,16 @@ const completionSpec: Fig.Spec = {
     {
       name: "create-connection",
       description: "Create an app runner connection resource",
+      options: [
+        {
+          name: "--connection-name",
+          description: "Name for the new connection",
+          args: {
+            name: "name",
+            description: "Name of the connection to be created",
+          },
+        },
+      ],
     },
   ],
 };
