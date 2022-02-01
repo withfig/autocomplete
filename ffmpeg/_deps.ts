5b890cc855db9b015938b0bb6260e011abe0bd3a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-02-01 15:24:48 +0800

    feat: add initial ffmpeg autocomplete

---
+++ ffmpeg/_deps.ts
@@ -0,0 +1,8 @@
+export { default as Kia } from "https://deno.land/x/kia@0.3.0/mod.ts";
+export {
+  blue,
+  bold,
+  green,
+  red,
+  yellow,
+} from "https://deno.land/std@0.123.0/fmt/colors.ts";
