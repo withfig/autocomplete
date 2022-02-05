2093829a40151d07f6b05c92b185b8141d014b08
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-02-05 19:48:42 +0800

    fix: make version arg optional

--- src/vr.ts
+++ src/vr.ts
@@ -1,3 +1,45 @@
+// const SCRIPT_KEYWORD = "    • ";
+// const scriptGenerator: Fig.Generator = {
+//   script: "vr",
+//   postProcess: (out) => {
+//     let suggestions: Fig.Suggestion[] = [];
+
+//     try {
+//       const lines = out
+//         .split("\n")
+//         .filter(Boolean)
+//         .map((k) => {
+//           return k.replace(/\u001\[[0-9;]*m/g, "");
+//         });
+
+//       for (const idx_ in lines) {
+//         const idx = (idx_ as unknown) as number;
+
+//         if (idx >= lines.length - 2) continue;
+
+//         const line = lines[idx];
+
+//         if (line.search(SCRIPT_KEYWORD) === -1) continue;
+
+//         const scriptName = line.substring(SCRIPT_KEYWORD.length);
+//         const description = lines[idx + 1].substring(SCRIPT_KEYWORD.length - 2);
+
+//         suggestions = [
+//           ...suggestions,
+//           {
+//             name: scriptName,
+//             description,
+//           },
+//         ];
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+
+//     return suggestions;
+//   },
+// };
+
 const completionSpec: Fig.Spec = {
   name: "vr",
   description: "The npm-style script runner for Deno",
@@ -8,6 +50,7 @@ const completionSpec: Fig.Spec = {
       description: "Run a script",
       args: {
         name: "script",
+        // generators: scriptGenerator,
       },
       options: [{ name: ["-h", "--help"], description: "Show help" }],
     },
@@ -17,6 +60,7 @@ const completionSpec: Fig.Spec = {
       args: {
         name: "script",
         isVariadic: true,
+        // generators: scriptGenerator,
       },
       options: [{ name: ["-h", "--help"], description: "Show help" }],
     },
@@ -26,6 +70,7 @@ const completionSpec: Fig.Spec = {
         "Upgrade Velociraptor to the latest version or to a specific one",
       args: {
         name: "version",
+        isOptional: true,
       },
       options: [
         { name: ["-h", "--help"], description: "Show help" },
@@ -53,6 +98,7 @@ const completionSpec: Fig.Spec = {
 
   args: {
     name: "script",
+    // generators: scriptGenerator,
   },
 };
 export default completionSpec;
