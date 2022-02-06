7f6f8d6c3b8869a28f5d39e5b9e33fd576816aad
Shawn Cockburn <60285801+ShawnCockburn@users.noreply.github.com>
2022-02-06 23:35:29 +0000

    fix: code suggestions

--- src/trex.ts
+++ src/trex.ts
@@ -2,18 +2,22 @@ const dependenciesGenerator: Fig.Generator = {
   script: "cat import_map.json",
   postProcess: function (out) {
     if (out) {
-      const deps = JSON.parse(out);
+      try {
+        const deps = JSON.parse(out);
 
-      if (deps.imports) {
-        const imports = Object.entries(deps.imports);
+        if (deps.imports) {
+          const imports = Object.entries(deps.imports);
 
-        const suggestions = imports.map(([name, url]) => ({
-          name: name as string,
-          icon: "🦖",
-          description: url as string,
-        }));
+          const suggestions = imports.map(([name, url]) => ({
+            name: name as string,
+            icon: "🦖",
+            description: url as string,
+          }));
 
-        return suggestions;
+          return suggestions;
+        }
+      } catch (error) {
+        return [];
       }
     }
     return [];
@@ -22,19 +26,23 @@ const dependenciesGenerator: Fig.Generator = {
 const scriptsGenerator: Fig.Generator = {
   script: "cat run.json",
   postProcess: function (out) {
-    if (out && out !== "") {
-      const scriptsObj = JSON.parse(out);
+    if (out) {
+      try {
+        const scriptsObj = JSON.parse(out);
 
-      if (scriptsObj.scripts) {
-        const scripts = Object.entries(scriptsObj.scripts);
+        if (scriptsObj.scripts) {
+          const scripts = Object.entries(scriptsObj.scripts);
 
-        const suggestions = scripts.map(([name, command]) => ({
-          name: name as string,
-          icon: "🚀",
-          description: "trex script" as string,
-        }));
+          const suggestions = scripts.map(([name, command]) => ({
+            name: name as string,
+            icon: "🚀",
+            description: "trex script" as string,
+          }));
 
-        return suggestions;
+          return suggestions;
+        }
+      } catch (error) {
+        return [];
       }
     }
     return [];
@@ -42,7 +50,6 @@ const scriptsGenerator: Fig.Generator = {
 };
 
 const trexOptions: Record<string, Fig.Option> = {
-  help: { name: ["-h", "--help"], description: "Print help info" },
   version: { name: ["-v", "--version"], description: "Print version" },
   map: {
     name: ["-m", "--map"],
@@ -92,12 +99,7 @@ const completionSpec: Fig.Spec = {
     {
       name: ["i", "install"],
       description: "Install a package",
-      options: [
-        trexOptions.help,
-        trexOptions.map,
-        trexOptions.nest,
-        trexOptions.pkg,
-      ],
+      options: [trexOptions.map, trexOptions.nest, trexOptions.pkg],
     },
     {
       name: "delete",
@@ -112,7 +114,6 @@ const completionSpec: Fig.Spec = {
       name: "upgrade",
       description: "Upgrade trex",
       options: [
-        trexOptions.help,
         {
           name: "--canary",
           description: "Install from dev branch",
@@ -128,7 +129,6 @@ const completionSpec: Fig.Spec = {
       name: "run",
       description: "Run a script alias in a file run.json",
       options: [
-        trexOptions.help,
         {
           name: ["-w", "--watch"],
           description: "Use reboot script alias protocol (rsap)",
@@ -161,7 +161,6 @@ const completionSpec: Fig.Spec = {
       name: "exec",
       description: "Execute a cli tool with out install then",
       options: [
-        trexOptions.help,
         {
           name: "--perms",
           description: "Specify cli permisions",
@@ -175,15 +174,22 @@ const completionSpec: Fig.Spec = {
       name: "check",
       description: "Check deno.land [std/x] dependencies updates",
       options: [
-        trexOptions.help,
         {
           name: ["-f", "--fix"],
-          description: "Update outdate dependencies",
+          description: "Update outdated dependencies",
         },
       ],
     },
   ],
-  options: [trexOptions.help, trexOptions.version, trexOptions.custom],
+  options: [
+    {
+      name: ["-h", "--help"],
+      description: "Print help info",
+      isPersistent: true,
+    },
+    trexOptions.version,
+    trexOptions.custom,
+  ],
   // Only uncomment if trex takes an argument
   // args: {}
 };
