8e889fb4b498cbb7e07fe905cbfbd8887e8acdd5
Tim Brewis <timbrewis27@gmail.com>
2022-03-02 12:02:42 +0000

    mdfind completion spec

---
+++ src/mdfind.ts
@@ -0,0 +1,70 @@
+const completionSpec: Fig.Spec = {
+  name: "mdfind",
+  description: "Finds files matching a given query",
+  parserDirectives: {
+    flagsArePosixNoncompliant: true,
+  },
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for mdfind",
+    },
+    {
+      name: "-0",
+      description: "Prints an ASCII NUL character after each result path",
+    },
+    {
+      name: "-live",
+      description:
+        "Provide live-updates to the number of files matching the query",
+    },
+    {
+      name: "-count",
+      description:
+        "Output the total number of matches, instead of the path to the matching items",
+    },
+    {
+      name: "-onlyin",
+      description: "Limit the scope of the search to <dir>",
+      args: {
+        name: "dir",
+        description: "Directory",
+        template: "folders",
+      },
+    },
+    {
+      name: "-name",
+      description: "Search for matching file names to <filename> only",
+      args: {
+        name: "filename",
+      },
+    },
+    {
+      name: "-reprint",
+      description: "Reprint results on live update",
+    },
+    {
+      name: "-s",
+      description: "Show contents of smart folder <folder>",
+      args: {
+        name: "folder",
+        description: "Smart folder",
+        template: "folders", // should ideally be a generator that only shows smart folders
+      },
+    },
+    {
+      name: "-literal",
+      description:
+        "Force the provided query string to be taken as a literal query string, without interpretation",
+    },
+    {
+      name: "-interpret",
+      description:
+        "Force the provided query string to be interpreted as if it had been typed into the Spotlight menu",
+    },
+  ],
+  args: {
+    name: "query",
+  },
+};
+export default completionSpec;
