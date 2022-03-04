8c936f4a26aee80591e538f1145e09bb7cfa9c98
yinchuandong <johnny.cd.yin@gmail.com>
2022-03-04 12:38:18 +1100

    feat(tgenv): add tgenv specs

---
+++ src/tgenv.ts
@@ -0,0 +1,73 @@
+const generators: Record<string, Fig.Generator> = {
+  installedVersions: {
+    script: "tgenv list",
+    postProcess: function (out) {
+      return out.split("\n").map((tfversion) => {
+        return { name: tfversion, description: "Version" };
+      });
+    },
+  },
+  allVersions: {
+    script: "tgenv list-remote",
+    postProcess: function (out) {
+      return out.split("\n").map(function (line) {
+        return { name: line, type: "option" };
+      });
+    },
+  },
+};
+
+const completionSpec: Fig.Spec = {
+  name: "tgenv",
+  description: "Terragrunt version manager",
+  subcommands: [
+    {
+      name: "install",
+      description: "Install a specific version of Terragrunt",
+      args: {
+        name: "version",
+        description: "Possible Terragrunt Version",
+        suggestions: ["latest", "min-required"],
+        generators: generators.allVersions,
+      },
+    },
+    {
+      name: "use",
+      description: "Switch to a version to use",
+      args: {
+        name: "version",
+        description: "Installed Terragrunt Version",
+        generators: generators.installedVersions,
+      },
+    },
+    {
+      name: "uninstall",
+      description: "Uninstall a specific version of Terragrunt",
+      args: {
+        name: "version",
+        description: "Installed Terragrunt Version",
+        generators: generators.installedVersions,
+      },
+    },
+    {
+      name: "list",
+      description: "List all installed versions",
+    },
+    {
+      name: "list-remote",
+      description: "List all installable versions",
+    },
+  ],
+  options: [
+    {
+      name: ["-v", "--version"],
+      description: "View your current tgenv version",
+    },
+    {
+      name: "--help",
+      description: "View commands",
+    },
+  ],
+};
+
+export default completionSpec;
