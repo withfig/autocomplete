bc5395ca4fb97a119dbf5671e56aef2a1a0b3ff3
Sean Sullivan <ssullivan61198@gmail.com>
2022-03-27 15:29:11 -0700

    remove unused version functions

--- src/node.ts
+++ src/node.ts
@@ -1,6 +1,6 @@
 import { filepaths } from "@fig/autocomplete-generators";
 
-const completionSpec: Fig.Spec = {
+const completionSpec: Fig.Subcommand = {
   name: "node",
   description: "Run the node interpreter",
   args: {
