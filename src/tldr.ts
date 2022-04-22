f2c96b49c93da991fbb44a5cfb83e110dd73e5d1
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-04-22 17:51:00 +0900

    Add tldr options

--- src/tldr.ts
+++ src/tldr.ts
@@ -1,14 +1,11 @@
-const generateTldrPages: Fig.Generator = {
+const wholeTldrPages: Fig.Generator = {
   script: () => {
-    const locale = Intl.DateTimeFormat().resolvedOptions().locale;
-    const lang = locale.split("-")[0];
-
-    const android = `~/.tldr/cache/pages.${lang}/android/`;
-    const common = `~/.tldr/cache/pages.${lang}/common/`;
-    const linux = `~/.tldr/cache/pages.${lang}/linux/`;
-    const osx = `~/.tldr/cache/pages.${lang}/osx/`;
-    const sunos = `~/.tldr/cache/pages.${lang}/sunos/`;
-    const windows = `~/.tldr/cache/pages.${lang}/windows/`;
+    const android = `~/.tldr/cache/pages/android/`;
+    const common = `~/.tldr/cache/pages/common/`;
+    const linux = `~/.tldr/cache/pages/linux/`;
+    const osx = `~/.tldr/cache/pages/osx/`;
+    const sunos = `~/.tldr/cache/pages/sunos/`;
+    const windows = `~/.tldr/cache/pages/windows/`;
     return `{
       ls -l ${android} 2>/dev/null | tr -s ' ' | cut -d ' ' -f 9 &
       ls -l ${common} 2>/dev/null | tr -s ' ' | cut -d ' ' -f 9 &
@@ -34,17 +31,133 @@ const generateTldrPages: Fig.Generator = {
   },
 };
 
+const linuxTldrPages: Fig.Generator = {
+  script: () => {
+    const linux = `~/.tldr/cache/pages/linux/`;
+    return `{
+      ls -l ${linux} 2>/dev/null | tr -s ' ' | cut -d ' ' -f 9
+    } | cat`;
+  },
+  postProcess: (out) => {
+    return out
+      .split("\n")
+      .filter((line) => {
+        return !(line.length == 0 || line.startsWith("/"));
+      })
+      .map((line) => {
+        return {
+          name: line.slice(0, -3),
+          description: "Tldr page",
+          icon: "fig://icon?type=string",
+        };
+      });
+  },
+};
+
+const osxTldrPages: Fig.Generator = {
+  script: () => {
+    const osx = `~/.tldr/cache/pages/osx/`;
+    return `{
+      ls -l ${osx} 2>/dev/null | tr -s ' ' | cut -d ' ' -f 9
+    } | cat`;
+  },
+  postProcess: (out) => {
+    return out
+      .split("\n")
+      .filter((line) => {
+        return !(line.length == 0 || line.startsWith("/"));
+      })
+      .map((line) => {
+        return {
+          name: line.slice(0, -3),
+          description: "Tldr page",
+          icon: "fig://icon?type=string",
+        };
+      });
+  },
+};
+
+const sunosTldrPages: Fig.Generator = {
+  script: () => {
+    const sunos = `~/.tldr/cache/pages/sunos/`;
+    return `{
+      ls -l ${sunos} 2>/dev/null | tr -s ' ' | cut -d ' ' -f 9
+    } | cat`;
+  },
+  postProcess: (out) => {
+    return out
+      .split("\n")
+      .filter((line) => {
+        return !(line.length == 0 || line.startsWith("/"));
+      })
+      .map((line) => {
+        return {
+          name: line.slice(0, -3),
+          description: "Tldr page",
+          icon: "fig://icon?type=string",
+        };
+      });
+  },
+};
+
 const completionSpec: Fig.Spec = {
   name: "tldr",
   description: "A simpler man page than the existing man page",
-  args: { generators: generateTldrPages },
+  args: { generators: wholeTldrPages },
   options: [
     {
-      name: "--update",
+      name: ["-h", "--help"],
+      description: "Display help for command",
+    },
+    {
+      name: ["-s", "--search"],
+      description: "Search all pages for the query",
+      args: {
+        name: "query",
+      },
+    },
+    {
+      name: "--linux",
+      description: "Show command page for Linux",
+      args: { generators: linuxTldrPages },
+    },
+    {
+      name: "--osx",
+      description: "Show command page for OSX",
+      args: { generators: osxTldrPages },
+    },
+    {
+      name: "--sunos",
+      description: "Show command page for SunOS",
+      args: { generators: sunosTldrPages },
+    },
+    {
+      name: ["-l", "--list"],
+      description: "Show all pages for current platform",
+    },
+    {
+      name: ["-a", "--list-all"],
+      description: "Show all available pages",
+    },
+    {
+      name: "--random",
+      description: "Show a page at random",
+    },
+    {
+      name: "--random-example",
+      description: "Show a single random example",
+    },
+    {
+      name: ["-m", "--markdown"],
+      description: "Show the original markdown format page",
+      args: { generators: wholeTldrPages },
+    },
+    {
+      name: ["-u", "--update"],
       description: "Download the latest pages and generate search index",
     },
     {
-      name: "--clear-cache",
+      name: ["-c", "--clear-cache"],
       description: "Delete the entire local cache",
     },
   ],
