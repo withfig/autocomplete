842b08657b847fad07534307161715b891511946
Brandon Leon <brandon.leon@servicenow.com>
2022-05-06 14:12:02 -0700

    linter updates.

--- src/mosh.ts
+++ src/mosh.ts
@@ -1,4 +1,4 @@
-import { knownHosts, configHosts } from "./ssh"
+import { knownHosts, configHosts } from "./ssh";
 
 const completionSpec: Fig.Spec = {
   name: "mosh",
@@ -14,60 +14,62 @@ const completionSpec: Fig.Spec = {
       description: "Show help for mosh",
     },
     {
-      name: ["--client"],
-      description: "mosh client on local machine (default: \"mosh-client\")",
+      name: "--client",
+      description: 'Mosh client on local machine (default: "mosh-client")',
     },
     {
-      name: ["--server"],
-      description: "mosh server on remote machine (default: \"mosh-server\")",
+      name: "--server",
+      description: 'Mosh server on remote machine (default: "mosh-server")',
     },
     {
-      name: ["--predict=adaptive"],
-      description: "local echo for slower links [default]",
+      name: "--predict=adaptive",
+      description: "Local echo for slower links [default]",
     },
     {
       name: ["--predict=always", "-a"],
-      description: "use local echo even on fast links",
+      description: "Use local echo even on fast links",
     },
     {
       name: ["--predict=never", "-n"],
-      description: "never use local echo",
+      description: "Never use local echo",
     },
     {
-      name: ["--predict=experimental"],
-      description: "aggressively echo even when incorrect",
+      name: "--predict=experimental",
+      description: "Aggressively echo even when incorrect",
     },
     {
       name: ["--family=inet", "-4"],
-      description: "use IPv4 only",
+      description: "Use IPv4 only",
     },
     {
       name: ["--family=inet6", "-6"],
-      description: "use IPv6 only",
+      description: "Use IPv6 only",
     },
     {
-      name: ["--family=auto"],
-      description: "autodetect network type for single-family hosts only",
+      name: "--family=auto",
+      description: "Autodetect network type for single-family hosts only",
     },
     {
-      name: ["--family=all"],
-      description: "try all network types",
+      name: "--family=all",
+      description: "Try all network types",
     },
     {
-      name: ["--family=prefer-inet"],
-      description: "use all network types, but try IPv4 first [default]",
+      name: "--family=prefer-inet",
+      description: "Use all network types, but try IPv4 first [default]",
     },
     {
-      name: ["--family=prefer-inet6"],
-      description: "use all network types, but try IPv6 first",
+      name: "--family=prefer-inet6",
+      description: "Use all network types, but try IPv6 first",
     },
     {
       name: ["--port", "-p"],
-      description: "server-side UDP port or range, (No effect on server-side SSH port)",
+      description:
+        "Server-side UDP port or range, (No effect on server-side SSH port)",
     },
     {
-      name: ["--bind-server="],
-      description: "{ssh|any|IP}, ask the server to reply from an IP address, (default: \"ssh\")",
+      name: "--bind-server",
+      description:
+        '{ssh|any|IP}, ask the server to reply from an IP address, (default: "ssh")',
     },
     {
       name: "--ssh",
@@ -76,23 +78,25 @@ const completionSpec: Fig.Spec = {
         name: "command",
         isCommand: true,
       },
-      description: "ssh command to run when setting up session, (example: \"ssh -p 2222\")",
+      description:
+        'Ssh command to run when setting up session, (example: "ssh -p 2222")',
     },
     {
-      name: ["--no-ssh-pty"],
-      description: "do not allocate a pseudo tty on ssh connection",
+      name: "--no-ssh-pty",
+      description: "Do not allocate a pseudo tty on ssh connection",
     },
     {
-      name: ["--no-init"],
-      description: "do not send terminal initialization string",
+      name: "--no-init",
+      description: "Do not send terminal initialization string",
     },
     {
-      name: ["--local"],
-      description: "run mosh-server locally without using ssh",
+      name: "--local",
+      description: "Run mosh-server locally without using ssh",
     },
     {
       name: "--experimental-remote-ip",
-      description: "Select the method for discovering the remote IP address to use for mosh",
+      description:
+        "Select the method for discovering the remote IP address to use for mosh",
       requiresSeparator: true,
       args: {
         name: "method",
@@ -101,7 +105,7 @@ const completionSpec: Fig.Spec = {
       },
     },
     {
-      name: ["--version"],
+      name: "--version",
       description: "Version and copyright information",
     },
   ],
