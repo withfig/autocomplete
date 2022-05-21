b49f3d1b4b0f5e4ef4bbd34c0f709c518d0ad650
Mark Mercado <mamercad@gmail.com>
2022-05-21 19:49:29 -0400

    Adding isDangerous-es :)

--- src/bc.ts
+++ src/bc.ts
@@ -11,30 +11,37 @@ const completionSpec: Fig.Spec = {
     {
       name: ["--help", "-h"],
       description: "Print the usage and exit",
+      isDangerous: false,
     },
     {
       name: ["--interactive", "-i"],
       description: "Force interactive mode",
+      isDangerous: false,
     },
     {
       name: ["--mathlib", "-l"],
       description: "Define the standard math library",
+      isDangerous: false,
     },
     {
       name: ["--warn", "-w"],
       description: "Give warnings for extensions to POSIX bc",
+      isDangerous: false,
     },
     {
       name: ["--standard", "-s"],
       description: "Process exactly the POSIX bc language",
+      isDangerous: false,
     },
     {
       name: ["--quiet", "-q"],
       description: "Do not print the normal GNU bc welcome",
+      isDangerous: false,
     },
     {
       name: ["--version", "-v"],
       description: "Print the version number and copyright and quit",
+      isDangerous: false,
     },
   ],
 };
