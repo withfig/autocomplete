05e3e29a2acbd74589f8e5895bd72a618bf8b0a3
Mark Mercado <mamercad@gmail.com>
2022-05-21 19:34:41 -0400

    Add bc completion

---
+++ src/bc.ts
@@ -0,0 +1,41 @@
+const completionSpec: Fig.Spec = {
+  name: "bc",
+  description: "An arbitrary precision calculator language",
+  args: {
+    name: "file",
+    isOptional: true,
+    isVariadic: true,
+    template: ["filepaths"],
+  },
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Print the usage and exit",
+    },
+    {
+      name: ["--interactive", "-i"],
+      description: "Force interactive mode",
+    },
+    {
+      name: ["--mathlib", "-l"],
+      description: "Define the standard math library",
+    },
+    {
+      name: ["--warn", "-w"],
+      description: "Give warnings for extensions to POSIX bc",
+    },
+    {
+      name: ["--standard", "-s"],
+      description: "Process exactly the POSIX bc language",
+    },
+    {
+      name: ["--quiet", "-q"],
+      description: "Do not print the normal GNU bc welcome",
+    },
+    {
+      name: ["--version", "-v"],
+      description: "Print the version number and copyright and quit",
+    },
+  ],
+};
+export default completionSpec;
