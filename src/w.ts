d768691e5de830a2009b2f66d433141712c3fdd4
Mark Mercado <mamercad@gmail.com>
2022-05-21 21:53:06 -0400

    Adding parserDirectives and args

--- src/w.ts
+++ src/w.ts
@@ -1,16 +1,27 @@
 const completionSpec: Fig.Spec = {
   name: "w",
   description: "Display who is logged in and what they are doing",
+  parserDirectives: {
+    optionsMustPrecedeArguments: true,
+  },
   options: [
     {
       name: "-h",
       description: "Suppress the heading",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-i",
       description: "Output is sorted by idle time",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
   ],
   args: {
