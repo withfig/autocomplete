fe5a000b7f0bc4d521fa610c09ab1be3030574cf
Mark Mercado <mamercad@gmail.com>
2022-05-21 21:54:52 -0400

    Adding parserDirectives and args

--- src/who.ts
+++ src/who.ts
@@ -1,42 +1,73 @@
 const completionSpec: Fig.Spec = {
   name: "who",
   description: "Display who is logged in",
+  parserDirectives: {
+    optionsMustPrecedeArguments: true,
+  },
   options: [
     {
       name: "-a",
       description: "Same as -bdlprTtu",
       insertValue: "-bdlprTtu",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-b",
       description: "Time of last system boot",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-d",
       description: "Print dead processes",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-H",
       description: "Write column headings above the regular output",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-l",
       description: "Print system login processes (unsupported)",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-m",
       description: "Only print information about the current terminal",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-p",
       description: "Print active processes spawned by launchd(8) (unsupported)",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-q",
@@ -57,34 +88,58 @@ const completionSpec: Fig.Spec = {
         "-t",
         "-u",
       ],
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-r",
       description: "Print the current runlevel",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-s",
       description:
         "List only the name, line and time fields (this is the default)",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-T",
       description:
         "Print a character after the user name indicating the state of the terminal line: '+' writable, '-' not writable, '?' bad",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-t",
       description: "Print last system clock change (unsupported)",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
     {
       name: "-u",
       description:
         "Print the idel time for each user and the associated process ID",
       isDangerous: false,
+      args: {
+        isDangerous: false,
+        isOptional: true,
+      },
     },
   ],
   args: {
