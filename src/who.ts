14ac41e3f5c69267c835f153e4172f97769565bd
Mark Mercado <mamercad@gmail.com>
2022-05-23 19:40:11 -0400

    Remove redundant isDangerous=false

--- src/who.ts
+++ src/who.ts
@@ -9,63 +9,49 @@ const completionSpec: Fig.Spec = {
       name: "-a",
       description: "Same as -bdlprTtu",
       insertValue: "-bdlprTtu",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-b",
       description: "Time of last system boot",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-d",
       description: "Print dead processes",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-H",
       description: "Write column headings above the regular output",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-l",
       description: "Print system login processes (unsupported)",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-m",
       description: "Only print information about the current terminal",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-p",
       description: "Print active processes spawned by launchd(8) (unsupported)",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
@@ -73,7 +59,6 @@ const completionSpec: Fig.Spec = {
       name: "-q",
       description:
         "'Quick mode': List only names and number of users currently logged on",
-      isDangerous: false,
       exclusiveOn: [
         "-a",
         "-b",
@@ -96,9 +81,7 @@ const completionSpec: Fig.Spec = {
     {
       name: "-r",
       description: "Print the current runlevel",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
@@ -106,9 +89,7 @@ const completionSpec: Fig.Spec = {
       name: "-s",
       description:
         "List only the name, line and time fields (this is the default)",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
@@ -116,18 +97,14 @@ const completionSpec: Fig.Spec = {
       name: "-T",
       description:
         "Print a character after the user name indicating the state of the terminal line: '+' writable, '-' not writable, '?' bad",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
     {
       name: "-t",
       description: "Print last system clock change (unsupported)",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
@@ -135,9 +112,7 @@ const completionSpec: Fig.Spec = {
       name: "-u",
       description:
         "Print the idel time for each user and the associated process ID",
-      isDangerous: false,
       args: {
-        isDangerous: false,
         isOptional: true,
       },
     },
@@ -146,7 +121,6 @@ const completionSpec: Fig.Spec = {
     name: "file",
     description:
       "By default, who gathers information from the file /var/run/utmpx; an alternative file may be specified",
-    isDangerous: false,
     isOptional: true,
   },
 };
