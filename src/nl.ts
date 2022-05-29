8953356424e34ce3e477ec0075d210555cbff11e
Mark Mercado <mamercad@gmail.com>
2022-05-29 13:18:28 -0400

    Refactoring

--- src/nl.ts
+++ src/nl.ts
@@ -7,24 +7,33 @@ const completionSpec: Fig.Spec = {
   options: [
     {
       name: "-b",
-      description: `Specify the logical page body lines to be numbered
-        'a' number all lines, 't' number only non-empty lines,
-        'n' no line numbering, 'pexpr' number only those lines that
-        contain the basic regular expression specified by 'expr'`,
+      description: "Specify the logical page body lines to be numbered",
       args: {
         name: "type",
-        suggestions: ["a", "t", "n", "pexpr"],
+        suggestions: [
+          {
+            name: "a",
+            description: "Number all lines",
+          },
+          {
+            name: "t",
+            description: "Number only non-empty lines",
+          },
+          {
+            name: "pexpr",
+            description:
+              "Only those lines that contain the basic regular expression specified by 'expr'",
+          },
+        ],
         default: "t",
       },
     },
     {
       name: "-d",
       description: `Specify the delimiter characters used to indicate the
-        start of a logical page section in the input file.  At
-        most two characters may be specified; if only one
-        character is specified, the first character is
-        replaced and the second character remains unchanged.
-        The default delim characters are "\\:".`,
+      start of a logical page section in the input file. At most two
+      characters may be specified; if only one character is specified,
+      the first character is replaced and the second character remains unchanged`,
       args: {
         name: "delim",
         suggestions: ["\\:"],
@@ -33,9 +42,8 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "-f",
-      description: `Specify the same as -b type except for logical page
-        footer lines.  The default type for logical page
-        footer lines is n.`,
+      description:
+        "Specify the same as -b type except for logical page footer lines",
       args: {
         name: "type",
         suggestions: ["n"],
@@ -44,9 +52,8 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "-h",
-      description: `Specify the same as -b type except for logical page
-        header lines.  The default type for logical page
-        header lines is n.`,
+      description:
+        "Specify the same as -b type except for logical page header lines",
       args: {
         name: "type",
         suggestions: ["n"],
@@ -55,8 +62,8 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "-i",
-      description: `Specify the increment value used to number logical
-        page lines.  The default incr value is 1.`,
+      description:
+        "Specify the increment value used to number logical page lines",
       args: {
         name: "incr",
         suggestions: ["1"],
@@ -66,11 +73,9 @@ const completionSpec: Fig.Spec = {
     {
       name: "-l",
       description: `If numbering of all lines is specified for the current
-        logical section using the corresponding -b a, -f a or
-        -h a option, specify the number of adjacent blank
-        lines to be considered as one.  For example, -l 2
-        results in only the second adjacent blank line being
-        numbered.  The default num value is 1.`,
+      logical section using the corresponding -b a, -f a or -h a option, specify
+      the number of adjacent blank lines to be considered as one. For example,
+      -l 2 results in only the second adjacent blank line being numbered`,
       args: {
         name: "num",
         suggestions: ["1"],
@@ -79,27 +84,36 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "-n",
-      description: `Specify the line numbering output format.
-        Recognized format arguments are:
-        'ln' Left justified,
-        'rn' Right justified (leading zeros suppressed),
-        'rz' Right justified (leading zeros kept).`,
+      description: "Specify the line numbering output format",
       args: {
         name: "format",
-        suggestions: ["ln", "rn", "rz"],
+        suggestions: [
+          {
+            name: "ln",
+            description: "Left justified",
+          },
+          {
+            name: "rn",
+            description: "Right justified (leading zeros suppressed)",
+          },
+          {
+            name: "rz",
+            description: "Right justified (leading zeros kept)",
+          },
+        ],
         default: "rz",
       },
     },
     {
       name: "-p",
-      description: `Specify that line numbering should not be restarted at
-        logical page delimiters.`,
+      description:
+        "Specify that line numbering should not be restarted at logical page delimiters",
     },
     {
       name: "-s",
       description: `Specify the characters used in separating the line
-        number and the corresponding text line.  The default
-        sep setting is a single tab character.`,
+      number and the corresponding text line.  The default
+      sep setting is a single tab character`,
       args: {
         name: "sep",
         suggestions: ["\\t"],
@@ -108,9 +122,8 @@ const completionSpec: Fig.Spec = {
     },
     {
       name: "-v",
-      description: `Specify the initial value used to number logical page
-        lines; see also the description of the -p option.  The
-        default startnum value is 1.`,
+      description:
+        "Specify the initial value used to number logical page lines; see also the description of the -p option",
       args: {
         name: "startnum",
         suggestions: ["1", "2", "3"],
@@ -120,9 +133,8 @@ const completionSpec: Fig.Spec = {
     {
       name: "-w",
       description: `Specify the number of characters to be occupied by the
-        line number; in case the width is insufficient to hold
-        the line number, it will be truncated to its width
-        least significant digits.  The default width is 6.`,
+      line number; in case the width is insufficient to hold the line number,
+      it will be truncated to its width least significant digits`,
       args: {
         name: "width",
         suggestions: ["6", "5", "4", "3", "2", "1"],
