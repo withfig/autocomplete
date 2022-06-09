f5f04a36ca02455cc2f604cdf13fb22749be7974
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-06-09 10:54:14 +1000

    Add SSID generator

--- src/wifi-password.ts
+++ src/wifi-password.ts
@@ -3,6 +3,12 @@ const completionSpec: Fig.Spec = {
   args: {
     name: "SSID",
     description: "The name for a Wi-Fi network",
+    generators: {
+      script:
+        "networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -listpreferredwirelessnetworks | tail -n +2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'",
+      // TODO: Replace this with `splitOn` once it's fixed
+      postProcess: (out) => out.split("\n").map((line) => ({ name: line })),
+    },
   },
   description:
     "People ask you for the Wi-Fi password. Answer quickly. macOS only",
@@ -14,10 +20,6 @@ const completionSpec: Fig.Spec = {
     {
       name: ["--quiet", "-q"],
       description: "Only output the password",
-      args: {
-        name: "SSID",
-        description: "The name for a Wi-Fi network",
-      },
     },
     {
       name: ["--version", "-V"],
