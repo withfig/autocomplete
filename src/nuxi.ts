e87f468d56b09e12ad96f0b20724df8b7a42a437
Christian Preston <christianpreston@ymail.com>
2022-06-22 16:21:25 -0400

    feat(nuxi): improve style and add more commands (#1306)

--- src/nuxi.ts
+++ src/nuxi.ts
@@ -1,158 +1,214 @@
+// Template Suggestions
+const TEMPLATES_SUGGESTIONS = [
+  "component",
+  "composable",
+  "layout",
+  "plugin",
+  "page",
+  "middleware",
+  "api",
+];
+
+// Default Root Dir Argument
+const defaultRootDirArg: Fig.Arg = {
+  name: "rootDir",
+  description: "The directory of the target application",
+  template: "folders",
+  isOptional: true,
+};
+
+// Main Nuxi options
+const mainNuxiOptions: Fig.Option[] = [
+  {
+    name: "--help",
+    description: "Show help",
+    isPersistent: true,
+  },
+];
+
+// Main nuxi subcommands
+const mainNuxiSubcommands: Fig.Subcommand[] = [
+  {
+    name: "info",
+    displayName: "Info",
+    icon: "📝",
+    description:
+      "The info command logs information about the current or specified Nuxt project",
+    args: defaultRootDirArg,
+  },
+  {
+    name: ["init", "create"],
+    description: "The init command initializes a fresh Nuxt project",
+    args: {
+      name: "dir",
+      description: "Name of the install directory",
+    },
+    options: [
+      {
+        name: ["--verbose", "-v"],
+        description: "Log information about the installation process",
+      },
+      {
+        name: ["--template", "-t"],
+        description: "Specify a Git repository to use as a template",
+      },
+    ],
+  },
+  {
+    name: ["add", "new"],
+    displayName: "Add",
+    icon: "fig://template?color=2ecc71&badge=+",
+    description: "Generate a entity into your Nuxt application",
+    options: [
+      {
+        name: "--cwd",
+        description: "The directory of the target application",
+      },
+      {
+        name: "--force",
+        description: "Force override file if it already exists",
+      },
+    ],
+    args: [
+      {
+        name: "TEMPLATE",
+        description: "Specify a template of the file to be generated",
+        suggestions: TEMPLATES_SUGGESTIONS.map((template) => ({
+          name: template,
+          description: `Generate a nuxt ${template}`,
+          icon: "fig://template?color=0079ff&badge=+",
+        })),
+      },
+      {
+        name: "NAME",
+        description: "Specify a name of the file that will be created",
+      },
+    ],
+  },
+  {
+    name: "analyze",
+    displayName: "Analyze",
+    icon: "✨",
+    description:
+      "The analyze command builds Nuxt and analyzes the production bundle (experimental)",
+    args: defaultRootDirArg,
+  },
+  {
+    name: "build",
+    displayName: "Build",
+    icon: "🛠",
+    description:
+      "The build command creates a .output directory with all your application, server and dependencies ready for production",
+    args: defaultRootDirArg,
+  },
+  {
+    name: "dev",
+    displayName: "Dev",
+    icon: "💻",
+    description:
+      "The dev command starts a development server with hot module replacement at http://localhost:3000",
+    args: defaultRootDirArg,
+    options: [
+      {
+        name: "--clipboard",
+        description: "Copy URL to clipboard",
+      },
+      {
+        name: ["--open", "-o"],
+        description: "Open URL in browser",
+      },
+      {
+        name: ["--port", "-p"],
+        description: "Port to listen",
+      },
+      {
+        name: ["--host", "-h"],
+        description: "Hostname of the server",
+      },
+      {
+        name: "--https",
+        description:
+          "Listen with https protocol with a self-signed certificate by default",
+      },
+      {
+        name: "--ssl-cert",
+        description: "Specify a certificate for https",
+      },
+      {
+        name: "--ssl-key",
+        description: "Specify the key for the https certificate",
+      },
+    ],
+  },
+  {
+    name: "prepare",
+    description: "Prepare nuxt for development/build",
+  },
+  {
+    name: "preview",
+    displayName: "Preview",
+    icon: "🔗",
+    description:
+      "The preview command starts a server to preview your Nuxt application after running the build command",
+    args: defaultRootDirArg,
+  },
+  {
+    name: "generate",
+    displayName: "Generate",
+    icon: "📦",
+    description: "Build Nuxt and prerender static routes",
+    args: defaultRootDirArg,
+  },
+  {
+    name: "test",
+    displayName: "Test",
+    icon: "🏁",
+    description: "Run the test files for your Nuxt application",
+    args: defaultRootDirArg,
+    options: [
+      {
+        name: "--dev",
+        description: "Run tests in development mode",
+      },
+      {
+        name: "--watch",
+        description: "Actively watch for changes and rerun tests",
+      },
+    ],
+  },
+  {
+    name: "upgrade",
+    displayName: "Upgrade",
+    icon: "🆕",
+    description: "The upgrade command upgrades Nuxt 3 to the latest version",
+    options: [
+      {
+        name: ["--force", "-f"],
+        description: "Removes node_modules and lock files before upgrade",
+      },
+    ],
+  },
+  {
+    name: "typecheck",
+    icon: "💬",
+    description:
+      "The typecheck command runs vue-tsc to check types throughout your app",
+    args: defaultRootDirArg,
+  },
+  {
+    name: "usage",
+    displayName: "Usage",
+    icon: "📊",
+    description: "The usage command shows the usage of the Nuxt CLI",
+  },
+];
+
 const completionSpec: Fig.Spec = {
   name: "nuxi",
-  description: "",
-  subcommands: [
-    {
-      name: "add",
-      description: "Scaffold an entity into your Nuxt application",
-      options: [
-        {
-          name: "--cwd",
-          description: "The directory of the target application",
-        },
-        {
-          name: "--force",
-          description: "Force override file if it already exists",
-        },
-      ],
-      args: [
-        {
-          name: "TEMPLATE",
-          description: "Specify a template of the file to be generated",
-          suggestions: [
-            "component",
-            "composable",
-            "layout",
-            "plugin",
-            "page",
-            "midleware",
-            "api",
-          ],
-        },
-        {
-          name: "NAME",
-          description: "Specify a name of the file that will be created",
-        },
-      ],
-    },
-    {
-      name: "analyze",
-      description: "Analyze the production bundle or your Nuxt applicaiton",
-      args: {
-        name: "rootDir",
-        description: "The directory of the target application",
-      },
-    },
-    {
-      name: "build",
-      description:
-        "The build command creates a .output directory with all your application, server and dependencies ready for production",
-      args: {
-        name: "rootDir",
-        description: "The root directory of the application to bundle",
-      },
-    },
-    {
-      name: "dev",
-      description:
-        "The dev command starts a development server with hot module replacement at http://localhost:3000",
-      args: {
-        name: "rootDir",
-        description: "The root directory of the application to serve",
-      },
-      options: [
-        {
-          name: "--clipboard",
-          description: "Copy URL to clipboard",
-        },
-        {
-          name: ["--open", "-o"],
-          description: "Open URL in browser",
-        },
-        {
-          name: ["--port", "-p"],
-          description: "Port to listen",
-        },
-        {
-          name: ["--host", "-h"],
-          description: "Hostname of the server",
-        },
-        {
-          name: "--https",
-          description:
-            "Listen with https protocol with a self-signed certificate by default",
-        },
-        {
-          name: "--ssl-cert",
-          description: "Specify a certificate for https",
-        },
-        {
-          name: "--ssl-key",
-          description: "Specify the key for the https certificate",
-        },
-      ],
-    },
-    {
-      name: "info",
-      description:
-        "The info command logs informations about the current or specified Nuxt project",
-      args: {
-        name: "rootDir",
-        description: "The directory of the target application",
-      },
-    },
-    {
-      name: ["init", "create"],
-      description: "The init command initializes a fresh Nuxt project",
-      args: {
-        name: "dir",
-        description: "Name of the install directory",
-      },
-      options: [
-        {
-          name: ["--verbose", "-v"],
-          description: "Log informations about the installation process",
-        },
-        {
-          name: ["--template", "-t"],
-          description: "Specify a Git repository to use as a template",
-        },
-      ],
-    },
-    {
-      name: "preview",
-      description:
-        "The preview command starts a server to preview your Nuxt application after running the build command",
-      args: {
-        name: "rootDir",
-        description: "The root directory of the application to preview",
-      },
-    },
-    {
-      name: "typecheck",
-      description:
-        "The typecheck command runs vue-tsc to check types throughout your app",
-      args: {
-        name: "rootDir",
-        description: "The directory of the target application",
-      },
-    },
-    {
-      name: "upgrade",
-      description: "The upgrade command upgrades Nuxt 3 to the latest version",
-      options: [
-        {
-          name: ["--force", "-f"],
-          description: "Removes node_modules and lock files before upgrade",
-        },
-      ],
-    },
-  ],
-  options: [
-    {
-      name: ["--help", "-h"],
-      description: "Show help for nuxi",
-    },
-  ],
+  description: "Nuxt 3 CLI",
+  icon: "https://v3.nuxtjs.org/icon.png",
+  subcommands: mainNuxiSubcommands,
+  options: mainNuxiOptions,
 };
+
 export default completionSpec;
