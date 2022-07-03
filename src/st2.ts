f920d1ca3c74e09028d16c1b149f196ae2d3ba09
Mark Mercado <mamercad@gmail.com>
2022-07-03 10:36:00 -0400

    Export and import `existingUsersandGroups`

--- src/st2.ts
+++ src/st2.ts
@@ -1,48 +1,4 @@
-// NOTE: Copied this generator from chown.ts
-const existingUsersandGroups: Fig.Generator = {
-  custom: async function (tokens, executeShellCommand) {
-    const colonAdded = tokens.find((token) => token.includes(":"));
-    const nFlagUsed = tokens.find((token) => /^-.*n.*/.test(token));
-
-    let shell: string;
-    // Using `:` as a trigger, check to see if a colon is added
-    // in the current command. If it is, get the system groups
-    // else retrieve the list of system users
-    if (colonAdded) {
-      shell = await executeShellCommand(
-        "dscl . -list /Groups PrimaryGroupID | tr -s ' '| sort -r"
-      );
-    } else {
-      shell = await executeShellCommand(
-        "dscl . -list /Users UniqueID | tr -s ' '| sort -r"
-      );
-    }
-
-    return (
-      shell
-        .split("\n")
-        // The shell command retrieves a table
-        // with rows that look like `user uid`
-        // so each row is split again to get the
-        // user/group and uid/gid
-        .map((line) => line.split(" "))
-        .map((value) => {
-          return {
-            // If the user has entered the option n
-            // suggest the uid/gid instead of user/group
-            name: nFlagUsed ? value[1] : value[0],
-            description: colonAdded
-              ? `Group - ${nFlagUsed ? value[0] : `gid: ${value[1]}`}`
-              : `User - ${nFlagUsed ? value[0] : `uid: ${value[1]}`}`,
-            icon: colonAdded ? "👥" : "👤",
-            priority: 90,
-          };
-        })
-    );
-  },
-  trigger: ":",
-  getQueryTerm: ":",
-};
+import { existingUsersandGroups } from "./chown";
 
 const helpOption: Fig.Option = {
   name: ["-h", "--help"],
