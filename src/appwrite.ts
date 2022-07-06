4387954819c23828b47dc6a7b0376758233282c3
Balli Asghar <ballihacker@outlook.com>
2022-07-06 12:52:09 +0100

    login & init ✅

---
+++ src/appwrite.ts
@@ -0,0 +1,53 @@
+const completionSpec: Fig.Spec = {
+  name: "appwrite",
+  description: "Appwrite - Open-Source End-to-End Backend Server",
+  subcommands: [
+    {
+      name: "login",
+      description: "Login to Appwrite",
+      options: [
+        {
+          name: ["-h", "--help"],
+          description: "Show help for this login command",
+        },
+      ],
+    },
+    {
+      name: "init",
+      description: "Initialize a new project",
+      subcommands: [
+        {
+          name: "project",
+          description: "Initialize a new project",
+        },
+        {
+          name: "function",
+          description: "Initialize a new cloud function",
+        },
+        {
+          name: "collection",
+          description: "Initialize a new collection",
+        },
+      ],
+      options: [
+        {
+          name: "--all",
+          description: "Initialize projects and collection",
+        },
+        {
+          name: ["-h", "--help"],
+          description: "Display help for init command",
+        },
+      ],
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for appwrite",
+    },
+  ],
+  // Only uncomment if appwrite takes an argument
+  // args: {}
+};
+export default completionSpec;
