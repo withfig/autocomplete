ebbf754286db8d93426b0acef4a578fc73fc5fef
Federico Ciardi <fed.ciardi@gmail.com>
2022-07-21 08:55:31 +0200

    fix(rushx): use correct arg directives

--- src/rushx.ts
+++ src/rushx.ts
@@ -1,4 +1,5 @@
-import { npmParserDirectives, npmScriptsGenerator } from "./npm";
+import { npmScriptsGenerator } from "./npm";
+import { yarnScriptParserDirectives } from "./yarn";
 
 const completionSpec: Fig.Spec = {
   name: "rushx",
@@ -8,7 +9,7 @@ const completionSpec: Fig.Spec = {
     name: "Scripts",
     description: "Script to run from your package.json",
     generators: npmScriptsGenerator,
-    parserDirectives: npmParserDirectives,
+    parserDirectives: yarnScriptParserDirectives,
     isCommand: true,
   },
   options: [
