d815f2b2f42b7b35ce981bf8b8e37d62285b250b
Johannes Rieke <johannes.rieke@gmail.com>
2022-08-05 16:50:29 +0200

    feat: add streamlit completion spec

---
+++ src/streamlit.ts
@@ -0,0 +1,74 @@
+const completionSpec: Fig.Spec = {
+  name: "streamlit",
+  description: "",
+  subcommands: [
+    {
+      name: "activate",
+      description: "Activate Streamlit by entering your email",
+    },
+    {
+      name: "cache",
+      description: "Manage the Streamlit cache",
+      subcommands: [
+        {
+          name: "clear",
+          description: "Clear st.cache, st.memo, and st.singleton caches",
+        },
+      ],
+    },
+    {
+      name: "config",
+      description: "Manage Streamlit's config settings",
+      subcommands: [
+        {
+          name: "show",
+          description: "Show all of Streamlit's config settings",
+        },
+      ],
+    },
+    {
+      name: "docs",
+      description: "Show help in browser",
+    },
+    {
+      name: "hello",
+      description: "Runs the Hello World script",
+    },
+    {
+      name: "help",
+      description: "Print the help message",
+    },
+    {
+      name: "run",
+      description: "Run a Python script, piping stderr to Streamlit",
+      args: {
+        name: "file",
+        description: "The Python script to run",
+        template: "filepaths",
+        suggestions: ["streamlit_app.py"],
+      },
+    },
+    {
+      name: "version",
+      description: "Print Streamlit's version number",
+    },
+  ],
+  options: [
+    {
+      name: "--log_level",
+      description: "Set the log level",
+      args: {
+        suggestions: ["error", "warning", "info", "debug"],
+      },
+    },
+    {
+      name: "--help",
+      description: "Show a help message and exit",
+    },
+    {
+      name: "--version",
+      description: "Show the version and exit",
+    },
+  ],
+};
+export default completionSpec;
