705783c01ab234b9394ac5935da2e64ec16685c9
Johannes Rieke <johannes.rieke@gmail.com>
2022-08-07 00:31:08 +0200

    Add emoji icons

--- src/streamlit.ts
+++ src/streamlit.ts
@@ -2,47 +2,56 @@ import { filepaths } from "@fig/autocomplete-generators";
 
 const completionSpec: Fig.Spec = {
   name: "streamlit",
-  description: "",
+  description: "Streamlit",
   subcommands: [
     {
       name: "activate",
       description: "Activate Streamlit by entering your email",
+      icon: "✉️",
     },
     {
       name: "cache",
       description: "Manage the Streamlit cache",
+      icon: "🗂",
       subcommands: [
         {
           name: "clear",
           description: "Clear st.cache, st.memo, and st.singleton caches",
+          icon: "🗑",
         },
       ],
     },
     {
       name: "config",
       description: "Manage Streamlit's config settings",
+      icon: "⚙️",
       subcommands: [
         {
           name: "show",
           description: "Show all of Streamlit's config settings",
+          icon: "👀",
         },
       ],
     },
     {
       name: "docs",
       description: "Show help in browser",
+      icon: "💡",
     },
     {
       name: "hello",
       description: "Runs the Hello World script",
+      icon: "👋,",
     },
     {
       name: "help",
       description: "Print the help message",
+      icon: "❓",
     },
     {
       name: "run",
       description: "Run a Python script, piping stderr to Streamlit",
+      icon: "🎈",
       args: {
         name: "file",
         description: "The Python script to run",
@@ -55,6 +64,7 @@ const completionSpec: Fig.Spec = {
     {
       name: "version",
       description: "Print Streamlit's version number",
+      icon: "💯",
     },
   ],
   options: [
@@ -62,7 +72,12 @@ const completionSpec: Fig.Spec = {
       name: "--log_level",
       description: "Set the log level",
       args: {
-        suggestions: ["error", "warning", "info", "debug"],
+        suggestions: [
+          { name: "error", icon: "🔥" },
+          { name: "warning", icon: "⚠️" },
+          { name: "info", icon: "ℹ️" },
+          { name: "debug", icon: "🐛" },
+        ],
       },
     },
     {
