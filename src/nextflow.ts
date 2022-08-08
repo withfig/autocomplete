c333bb1fe1b4ee818208210a18e1d2865afd37aa
Marcel Ribeiro-Dantas <mribeirodantas@seqera.io>
2022-08-08 10:27:14 -0300

    feat(Nextflow): Add Nextflow subcommands
    Signed-off-by: Marcel Ribeiro-Dantas <mribeirodantas@seqera.io>

---
+++ src/nextflow.ts
@@ -0,0 +1,65 @@
+const completionSpec: Fig.Spec = {
+  name: "nextflow",
+  description: "",
+  subcommands: [
+    {
+      name: "clean",
+      description: "Clean up project cache and work directories",
+    },
+    {
+      name: "clone",
+      description: "Clone a project into a folder",
+    },
+    {
+      name: "config",
+      description: "Print a project configuration",
+    },
+    {
+      name: "console",
+      description: "Launch Nextflow interactive console",
+    },
+    {
+      name: "drop",
+      description: "Delete the local copy of a project",
+    },
+    {
+      name: "help",
+      description: "Print the usage help for a command",
+    },
+    {
+      name: "info",
+      description: "Print project and system runtime information",
+    },
+    {
+      name: "kuberun",
+      description: "Execute a workflow in a Kubernetes cluster (experimental)",
+    },
+    {
+      name: "list",
+      description: "List all downloaded projects",
+    },
+    {
+      name: "log",
+      description: "Print executions log and runtime info",
+    },
+    {
+      name: "pull",
+      description: "Download or update a project",
+    },
+    {
+      name: "run",
+      description: "Execute a pipeline project",
+    },
+    {
+      name: "self-update",
+      description: "Update nextflow runtime to the latest available version",
+    },
+    {
+      name: "view",
+      description: "View project script file(s)",
+    },
+  ],
+  // Only uncomment if nextflow takes an argument
+  // args: {}
+};
+export default completionSpec;
