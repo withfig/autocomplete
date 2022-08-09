f235439d2dc25a61f18e197760885b55469b18c5
Simon Whitaker <sw@netcetera.org>
2022-08-09 01:19:41 +0100

    feat: add gibo completion spec (#1385)

---
+++ src/gibo.ts
@@ -0,0 +1,50 @@
+const boilerplates: Fig.Generator = {
+  script: "gibo list",
+  splitOn: "\n",
+};
+
+const completionSpec: Fig.Spec = {
+  name: "gibo",
+  description: "Easy access to gitignore boilerplates",
+  subcommands: [
+    {
+      name: "dump",
+      description: "Dump one or more boilerplates to STDOUT",
+      args: {
+        name: "boilerplate",
+        description: "Name of the boilerplate to dump",
+        generators: boilerplates,
+        isVariadic: true,
+      },
+    },
+    {
+      name: "search",
+      description: "Search for boilerplates",
+      args: {
+        name: "search term",
+        description: "Name of the language or framework to search for",
+      },
+    },
+    {
+      name: "root",
+      description: "Show the directory where gibo stores its boilerplates",
+    },
+    { name: "help", description: "Show help information" },
+    { name: "list", description: "Show the list of available boilerplates" },
+    {
+      name: "update",
+      description: "Update the list of available boilerplates",
+    },
+    {
+      name: "version",
+      description: "Show the current version of gibo installed",
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for gibo",
+    },
+  ],
+};
+export default completionSpec;
