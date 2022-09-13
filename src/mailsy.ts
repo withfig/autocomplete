a4d3bfb13f46c89281bbc04c164ccd4c71d2f1fa
Balli Asghar <ballihacker@outlook.com>
2022-09-13 04:16:21 +0100

    done

--- src/mailsy.ts
+++ src/mailsy.ts
@@ -1,5 +1,5 @@
 const completionSpec: Fig.Spec = {
-  name: "index",
+  name: "mailsy",
   description: "⚡️ Quickly generate a disposable email straight from terminal",
   subcommands: [
     {
@@ -18,6 +18,11 @@ const completionSpec: Fig.Spec = {
       name: "me",
       description: "Show details of the account",
     },
+    {
+      name: "help",
+      description: "Get help for a command",
+      args: { name: "command", isOptional: true, template: "help" },
+    },
   ],
   options: [
     { name: ["-v", "--version"], description: "Output the current version" },
@@ -25,6 +30,7 @@ const completionSpec: Fig.Spec = {
       name: ["-h", "--help"],
       description: "Display help for command",
       priority: 49,
+      isPersistent: true,
     },
   ],
 };
