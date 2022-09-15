8c1f93803e65c09f1c2fccdfc56da6464176ea04
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-09-15 11:21:58 +1000

    Remove suggestCurrentToken from the vims

--- src/vim.ts
+++ src/vim.ts
@@ -4,7 +4,7 @@ const completionSpec: Fig.Spec = {
     "Vi[m] is an one of two powerhouse text editors in the Unix world, the other being EMACS",
   args: {
     template: "filepaths",
-    suggestCurrentToken: true,
+    // suggestCurrentToken: true,
   },
   options: [
     {
