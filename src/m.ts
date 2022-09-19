fd37c22d27efae581d1973b87876579aba213d32
Balli Asghar <ballihacker@outlook.com>
2022-09-19 16:26:43 +0100

    manage User ✅

--- src/m.ts
+++ src/m.ts
@@ -1198,6 +1198,7 @@ const completionSpec: Fig.Spec = {
           args: {
             name: "username",
             description: "Username",
+            generators: generateUsers,
           },
         },
         {
@@ -1210,6 +1211,7 @@ const completionSpec: Fig.Spec = {
           args: {
             name: "username",
             description: "Username",
+            generators: generateUsers,
           },
         },
       ],
