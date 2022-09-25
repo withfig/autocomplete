d6d067433eb8e48eeb744ffb74dca6f4e9fba125
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-25 19:15:07 -0400

    Add 'fix' command

--- src/sqlfluff.ts
+++ src/sqlfluff.ts
@@ -119,6 +119,115 @@ const completionSpec: Fig.Spec = {
 				description: "Show help for sqlfluff" 
 			}
 		],
-	}],
+	},
+	{
+		name: "fix",
+		description: "Fix SQL files.",
+		options: [
+			{
+				name: ["--nocolor", "-n"], 
+				description: "No color - output will be without ANSI color codes." 
+			},
+			{
+				name: ["--ignore", "-i"], 
+				description: "Ignore particular families of errors so that they don’t cause a failed run. -–ignore behaves somewhat like noqa comments, except it applies globally."
+			},
+			{
+				name: ["--verbose", "-v"], 
+				description: "Verbosity, how detailed should the output be. This is stackable, so -vv is more verbose than -v. For the most verbose option try -vvvv or -vvvvv." 
+			},
+			{
+				name: ["--exclude_rules", "-e"], 
+				description: "Exclude specific rules. This could either be the allowlist, or the general set if there is no specific allowlist." 
+			},
+			{
+				name: ["--rules", "-r"], 
+				description: "Narrow the search to only specific rules." 
+			},
+			{
+				name: ["--templater", "-t"], 
+				description: "The templater to use (default=jinja)" 
+			},
+			{
+				name: ["--dialect", "-d"], 
+				description: "The dialect of SQL to lint",
+				args: [
+					{
+						name: "dialect",
+						description: "Name of dialect, eg. ANSI.",
+						suggestions: [
+							"ansi", "bigquery", "exasol", "hive", "mysql",
+							"oracle", "postgres", "redshift", "snowflake", 
+							"spark3", "sqllite", "teradata", "tsql"
+						]
+					}
+				],
+			},
+			{
+				name: ["--format", "-f"],
+				description: "What format to return the lint result in (default=human)."
+			},
+			{
+				name: ["--processes", "-p"],
+				description: "The number of parallel processes to run. Positive numbers work as expected. Zero and negative numbers will work as number_of_cpus - number. e.g -1 means all cpus except one. 0 means all cpus."
+			},
+			{
+				name: ["--fixed-suffix", "-x"],
+				description: "An optional suffix to add to fixed files."
+			},
+			{
+				name: "--version",
+				description: "Show the version and exit."
+			},
+			{
+				name: "--disable-noqa",
+				description: "Set this flag to ignore inline noqa comments."
+			},
+			{
+				name: "--bench",
+				description: "Set this flag to engage the benchmarking tool output."
+			},
+			{
+				name: "--logger",
+				description: "Choose to limit the logging to one of the loggers.",
+				args: [
+					{
+						name: "logger",
+						description: "Name of logger to limit to, eg. templater.",
+						suggestions: ["templater", "lexer", "parser", "linter", "rules"]
+					},
+				],
+			},
+			{
+				name: "--encoding",
+				description: "Specify encoding to use when reading and writing files. Defaults to autodetect."
+			},
+			{
+				name: "--ignore-local-config",
+				description: "Ignore config files in default search path locations. This option allows the user to lint with the default config or can be used in conjunction with –config to only reference the custom config file."
+			},
+			{
+				name: "--config",
+				description: "Include additional config file. By default the config is generated from the standard configuration files described in the documentation. This argument allows you to specify an additional configuration file that overrides the standard configuration files. N.B. cfg format is required."
+			},
+			{
+				name: "--write-output",
+				description: "Optionally provide a filename to write the results to, mostly used in tandem with –format. NB: Setting an output file re-enables normal stdout logging."
+			},
+			{
+				name: "--disable-progress-bar",
+				description: "Disables progress bars."
+			},
+			{
+				name: "--FIX-EVEN-UNPARSABLE",
+				description: "Enables fixing of files that have templating or parse errors."
+			},
+			{
+				name: "--show-lint-violations",
+				description: "Show lint violations."
+			},
+		]
+	},
+	],
 };
 export default completionSpec;
