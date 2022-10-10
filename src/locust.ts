2e4a19d5c2b8e2e4d43f4f477888ca14ca9f6d60
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2022-10-10 19:15:58 -0400

    Add: Locust
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- src/locust.ts
+++ src/locust.ts
@@ -107,6 +107,127 @@ const completionSpec: Fig.Spec = {
       description:
         "Enable select boxes in the web interface to choose from all available User classes and Shape classes",
     },
+    {
+      name: "--master",
+      description:
+        "Set locust to run in distributed mode with this process as master",
+    },
+    {
+      name: "--master-bind-host",
+      description:
+        "Interfaces (hostname, ip) that locust master should bind to",
+    },
+    {
+      name: "--master-bind-port",
+      description: "Port that locust master should bind to",
+    },
+    {
+      name: "--expect-workers",
+      description:
+        "How many workers master should expect to connect before starting the test (only when --headless/autostart is used)",
+    },
+    {
+      name: "--expect-workers-max-wait",
+      description:
+        "How long should the master wait for workers to connect before giving up. Defaults to wait forever",
+    },
+    {
+      name: "--worker",
+      description:
+        "Set locust to run in distributed mode with this process as worker",
+    },
+    {
+      name: "--master-host",
+      description:
+        "Host or IP address of locust master for distributed load testing. Only used when running with --worker. Defaults to 127.0.0.1",
+    },
+    {
+      name: "--master-port",
+      description: "Show list of possible User classes and exit",
+    },
+    {
+      name: "--csv",
+      description: "Store current request stats to files in CSV format",
+    },
+    {
+      name: "--csv-full-history",
+      description:
+        "Store each stats entry in CSV format to _stats_history.csv file. You must also specify the '--csv' argument to enable this",
+    },
+    {
+      name: "--print-stats",
+      description: "Print stats in the console",
+    },
+    {
+      name: "--only-summary",
+      description: "Only print the summary stats",
+    },
+    {
+      name: "--reset-stats",
+      description:
+        "Reset statistics once spawning has been completed. Should be set on both master and workers when running in distributed mode",
+    },
+    {
+      name: "--html",
+      description: "Store HTML report to file path specified",
+    },
+    {
+      name: "--skip-log-setup",
+      description:
+        "Disable Locust's logging setup. Instead, the configuration is provided by the Locust test or Python defaults. --loglevel LOGLEVEL, -L LOGLEVEL",
+    },
+    {
+      name: "--loglevel",
+      description:
+        "Choose between DEBUG/INFO/WARNING/ERROR/CRITICAL. Default is INFO",
+      args: {
+        name: "loglevel",
+        generators: keyValue({
+          values: ["INFO", "WARNING", "DEBUG", "ERROR", "CRITICAL"],
+        }),
+        description:
+          "Choose between DEBUG/INFO/WARNING/ERROR/CRITICAL. Default is INFO",
+      },
+    },
+    {
+      name: "--logfile",
+      description: "Path to log file. If not set, log will go to stderr",
+    },
+    {
+      name: "--show-task-ratio",
+      description:
+        "Print table of the User classes' task execution ratio. Use this with non-zero --user option if some classes define non-zero fixed_count attribute",
+    },
+    {
+      name: "--show-task-ratio-json",
+      description:
+        "Print json data of the User classes' task execution ratio. Use this with non-zero --user option if some classes define non-zero fixed_count attribute",
+    },
+    {
+      name: "--exit-code-on-error",
+      description:
+        "Sets the process exit code to use when a test result contain any failure or error",
+    },
+    {
+      name: "--equal-weights",
+      description:
+        "Use equally distributed task weights, overriding the weights specified in the locustfile",
+    },
+    {
+      name: "--enable-rebalancing",
+      description:
+        "Allow to automatically rebalance users if new workers are added or removed during a test run",
+    },
+    {
+      name: ["-T", "--tags"],
+      description:
+        "List of tags to include in the test, so only tasks with any matching tags will be executed",
+    },
+    {
+      name: ["--E", "--exclude-tags"],
+      description:
+        "List of tags to exclude from the test, so only tasks with no matching tags will be executed",
+    },
   ],
   subcommands: [
     {
