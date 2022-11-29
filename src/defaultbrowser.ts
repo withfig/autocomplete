de76f1f4ef9c1584367605e0a765ad7882f35147
jonasbn <jonasbn@gmail.com>
2022-11-29 17:55:18 +0100

    Cleaned code and added filtering out of the already set browser

--- src/defaultbrowser.ts
+++ src/defaultbrowser.ts
@@ -1,22 +1,23 @@
-const getBrowsers: Fig.Generator = {
+const getInstalledBrowsers: Fig.Generator = {
   script: "defaultbrowser",
-  splitOn: "\n",
   postProcess: function (out) {
-    const installedBrowsers: Fig.Suggestion[] = [];
-    for (let i = 0; i < lines.length; i++) {
-      var browser_name = lines[i].split(" ")[0];
-      installedBrowsers.push({
-        name: browser_name,
-      });
-    }
-    return installedBrowsers;
+    return out.split("\n").map((line) => {
+      /* We ignore the already set browser */
+      if (line.startsWith("*")) {
+        return {};
+      }
+      const browserName = line.trim();
+      return {
+        name: browserName,
+      };
+    });
   },
 };
 
 const completionSpec: Fig.Spec = {
   name: "defaultbrowser",
-  description: "Change your default browser",
-  args: { isOptional: true, generators: getBrowsers },
+  description: "Change your default browser from the CLI",
+  args: { isOptional: true, generators: getInstalledBrowsers },
 };
 
 export default completionSpec;
