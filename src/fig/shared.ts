37baa39b3920cd4a6319cdad420e92b9144463a7
grant0417 <grantgurvis@gmail.com>
2022-12-05 13:04:18 -0800

    fix: make templated paths work again

--- src/fig/shared.ts
+++ src/fig/shared.ts
@@ -24,7 +24,7 @@ const devCompletionsFolderGenerator: Fig.Generator = {
       return {
         name: paths.pop(),
         insertValue: folder,
-        icon: `fig://${folder}`,
+        icon: `fig://path/${folder}`,
       };
     }),
 };
