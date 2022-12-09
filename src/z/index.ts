4317bb45a14f4258d700597d1373490ac16d270a
grant0417 <grantgurvis@gmail.com>
2022-12-08 20:19:46 -0800

    change check

--- src/z/index.ts
+++ src/z/index.ts
@@ -3,8 +3,8 @@ const versionFiles = ["zoxide", "z-script"];
 export const getVersionCommand: Fig.GetVersionCommand = async (
   executeShellCommand
 ) => {
-  const out = await executeShellCommand("which z");
-  if (out.includes("zoxide")) {
+  const out = await executeShellCommand("which zoxide");
+  if (out.trim().endsWith("zoxide")) {
     return "zoxide";
   } else {
     return "z-script";
