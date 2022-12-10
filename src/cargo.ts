5cd7e98665a8f7f827443ed5bc62d8b49315f587
grant0417 <grantgurvis@gmail.com>
2022-12-09 19:43:54 -0800

    feat(cargo): add insta subcommand

--- src/cargo.ts
+++ src/cargo.ts
@@ -6956,6 +6956,273 @@ const completionSpec: (toolchain?: boolean) => Fig.Spec = (
       subcommands.push(fuzz);
     }
 
+    if (commands.includes("insta")) {
+      const commonOptions: Fig.Option[] = [
+        {
+          name: ["-h", "--help"],
+          description: "Print help information",
+        },
+        {
+          name: ["-V", "--version"],
+          description: "Print version information",
+        },
+        {
+          name: "--color",
+          description: "Coloring: auto, always, never",
+          args: {
+            name: "WHEN",
+            default: "auto",
+            suggestions: ["auto", "always", "never"],
+          },
+        },
+        {
+          name: "--manifest-path",
+          description: "Path to Cargo.toml",
+          args: {
+            name: "PATH",
+            generators: filepaths({ equals: "Cargo.toml" }),
+          },
+        },
+        {
+          name: "--workspace-root",
+          description: "Explicit path to the workspace root",
+          args: {
+            name: "PATH",
+            template: "folders",
+          },
+        },
+        {
+          name: ["-e", "--extensions"],
+          description: "Sets the extensions to consider.  Defaults to `.snap`",
+          args: {
+            name: "EXTENSIONS",
+            isVariadic: true,
+          },
+        },
+        {
+          name: "--all",
+          description: "Work on all packages in the workspace",
+        },
+        {
+          name: "--no-ignore",
+          description: "Also walk into ignored paths",
+        },
+      ];
+
+      const insta: Fig.Subcommand = {
+        name: "insta",
+        icon: "🛠",
+        description: "A `cargo` subcommand for snapshot testing",
+        subcommands: [
+          {
+            name: "review",
+            description: "Interactively review snapshots",
+            options: [
+              ...commonOptions,
+              {
+                name: "--snapshot",
+                description: "Limits the operation to one or more snapshots",
+                args: {
+                  name: "snapshot-filter",
+                  isVariadic: true,
+                },
+              },
+              {
+                name: ["-q", "--quiet"],
+                description: "Do not print to stdout",
+              },
+            ],
+          },
+          {
+            name: "reject",
+            description: "Rejects all snapshots",
+            options: [
+              ...commonOptions,
+              {
+                name: "--snapshot",
+                description: "Limits the operation to one or more snapshots",
+                args: {
+                  name: "snapshot-filter",
+                  isVariadic: true,
+                },
+              },
+              {
+                name: ["-q", "--quiet"],
+                description: "Do not print to stdout",
+              },
+            ],
+          },
+          {
+            name: "accept",
+            description: "Accepts all snapshots",
+            options: [
+              ...commonOptions,
+              {
+                name: "--snapshot",
+                description: "Limits the operation to one or more snapshots",
+                args: {
+                  name: "snapshot-filter",
+                  isVariadic: true,
+                },
+              },
+              {
+                name: ["-q", "--quiet"],
+                description: "Do not print to stdout",
+              },
+            ],
+          },
+          {
+            name: "test",
+            description: "Run tests and then reviews",
+            options: [
+              ...commonOptions,
+              {
+                name: "--snapshot",
+                description: "Limits the operation to one or more snapshots",
+                args: {
+                  name: "snapshot-filter",
+                  isVariadic: true,
+                },
+              },
+              {
+                name: ["-q", "--quiet"],
+                description: "Do not print to stdout",
+              },
+              {
+                name: ["-p", "--package"],
+                description: "Package to run tests for",
+                args: {
+                  name: "SPEC",
+                  filterStrategy: "fuzzy",
+                  generators: packageGenerator,
+                },
+              },
+              {
+                name: "--no-force-pass",
+                description: "Disable force-passing of snapshot tests",
+              },
+              {
+                name: "--fail-fast",
+                description: "Prevent running all tests regardless of failure",
+              },
+              {
+                name: "--features",
+                description: "Space-separated list of features to activate",
+                args: {
+                  name: "features",
+                },
+              },
+              {
+                name: ["-j", "--jobs"],
+                description: "Number of parallel jobs, defaults to # of CPUs",
+                args: {
+                  name: "jobs",
+                },
+              },
+              {
+                name: "--release",
+                description:
+                  "Build artifacts in release mode, with optimizations",
+              },
+              {
+                name: "--all-features",
+                description: "Activate all available features",
+              },
+              {
+                name: "--no-default-features",
+                description: "Do not activate the `default` feature",
+              },
+              {
+                name: "--review",
+                description: "Follow up with review",
+              },
+              {
+                name: "--accept",
+                description: "Accept all snapshots after test",
+              },
+              {
+                name: "--accept-unseen",
+                description: "Accept all new (previously unseen)",
+              },
+              {
+                name: "--keep-pending",
+                description: "Do not reject pending snapshots before run",
+              },
+              {
+                name: "--force-update-snapshots",
+                description:
+                  "Update all snapshots even if they are still matching",
+              },
+              {
+                name: "--delete-unreferenced-snapshots",
+                description: "Delete unreferenced snapshots after the test run",
+              },
+              {
+                name: "--glob-filter",
+                description: "Filters to apply to the insta glob feature",
+                args: {
+                  name: "glob-filter",
+                  isVariadic: true,
+                },
+              },
+              {
+                name: ["-Q", "--no-quiet"],
+                description: "Do not pass the quiet flag (`-q`) to tests",
+              },
+              {
+                name: "--test-runner",
+                description: "Picks the test runner",
+                args: {
+                  name: "test-runner",
+                },
+              },
+            ],
+          },
+          {
+            name: "pending",
+            description: "Print a summary of all pending snapshots",
+            options: [
+              ...commonOptions,
+              {
+                name: "--as-json",
+                description: "Changes the output from human readable to JSON",
+              },
+            ],
+          },
+          {
+            name: "show",
+            description: "Shows a specific snapshot",
+            options: commonOptions,
+            args: {
+              name: "path",
+              description: "The path to the snapshot file",
+              generators: filepaths({ extensions: ["snap"] }),
+            },
+          },
+        ],
+        options: [
+          {
+            name: ["-h", "--help"],
+            description: "Print help information",
+          },
+          {
+            name: ["-V", "--version"],
+            description: "Print version information",
+          },
+          {
+            name: "--color",
+            description: "Coloring: auto, always, never",
+            args: {
+              name: "WHEN",
+              default: "auto",
+              suggestions: ["auto", "always", "never"],
+            },
+          },
+        ],
+      };
+      subcommands.push(insta);
+    }
+
     return {
       name: "cargo",
       subcommands,
