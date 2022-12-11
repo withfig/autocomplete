9c1ae4ce48c2dc44327e50feee17b4d5d0420d49
Dominic Ruggiero <dominic@userexe.me>
2022-12-11 14:26:52 -0500

    oops
    didn't mean to commit this...

--- src/add-apt-repository.ts
+++
@@ -1,9 +0,0 @@
-const completionSpec: Fig.Spec = {
-  name: "add-apt-repository",
-  description:
-    "Adds a repository into the /etc/apt/sources.list or /etc/apt/sources.list.d or removes an existing one",
-  args: {
-    name: "repository",
-  },
-};
-export default completionSpec;
