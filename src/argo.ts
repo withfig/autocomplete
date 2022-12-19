a96502638ce3d99176276627d183f439597bbcc3
rupert648 <rupertcarr@hotmail.co.uk>
2022-12-19 14:33:08 +0000

    argo get done

--- src/argo.ts
+++ src/argo.ts
@@ -954,6 +954,50 @@ const completionSpec: Fig.Spec = {
         },
       ],
     },
+    {
+      name: "get",
+      description: "Display details about a workflow",
+      args: {
+        name: "WORKFLOW",
+      },
+      options: [
+        {
+          name: ["--help", "-h"],
+          description: "Help for get",
+        },
+        {
+          name: "--no-color",
+          description: "Disable colorized output",
+        },
+        {
+          name: "--no-utf8",
+          description: "Use plain 7-bits ascii characters",
+        },
+        {
+          name: "--node-field-selector",
+          description:
+            "Selector of node to display, Eg: --node-field-selector phase=abc",
+          args: {
+            name: "string",
+          },
+        },
+        {
+          name: ["--output", "-o"],
+          description: "Output format. One of: json|yaml|short|wide",
+          args: {
+            name: "string",
+          },
+        },
+        {
+          name: "--status",
+          description:
+            "Filter by status (Pending, Running, Succeeded, Skipped, Failed, Error)",
+          args: {
+            name: "string",
+          },
+        },
+      ],
+    },
 };
 
 export default completionSpec;
