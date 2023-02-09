05b98e8759c784192126b57e6304144fef165fc5
Marcel Ribeiro Dantas <mribeirodantas@seqera.io>
2023-02-09 16:51:35 -0300

    feat(nextflow): add session ID generator for `-resume` parameter (#1843)

--- src/nextflow.ts
+++ src/nextflow.ts
@@ -1,3 +1,18 @@
+const sessionid: Fig.Generator = {
+  script: "cat .nextflow/history | awk '{ print $7 }'",
+  postProcess: (output) => {
+    if (output == "") {
+      return [];
+    }
+    return output.split("\n").map((sessionid) => {
+      return {
+        name: sessionid.replace("*", "").trim(),
+        description: "Session ID",
+      };
+    });
+  },
+};
+
 const runname: Fig.Generator = {
   script: "cat .nextflow/history | awk '{ print $4 }'",
   postProcess: (output) => {
@@ -836,6 +851,11 @@ const completionSpec: Fig.Spec = {
           name: "-resume",
           description:
             "Execute the script using the cached results, useful to continue executions that was stopped by an error",
+          args: {
+            name: "session ID",
+            isOptional: true,
+            generators: sessionid,
+          },
         },
         {
           name: ["-r", "-revision"],
