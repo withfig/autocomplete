c3f04cc58a21a7ab2c43ae8273662a53a435371e
Johan Stickman <zayatelie27@gmail.com>
2023-03-12 09:12:13 +0100

    Add freetranscli.ts

---
+++ src/freetranscli.ts
@@ -0,0 +1,50 @@
+const completionSpec: Fig.Spec = {
+  name: "freetranscli",
+  description: "",
+  subcommands: [
+    {
+      name: ["download", "d", "dld", "down"],
+      description: "Télécharge depuis une url FreeTransfert",
+      icon: "📥",
+      args: {
+        name: "url",
+        description: "URL FreeTransfert",
+      },
+    },
+    {
+      name: "history",
+      description: "Affiche l'historique",
+      icon: "📜",
+    },
+    {
+      name: ["issue", "bug", "github"],
+      description: "Crée une issue GitHub",
+      icon: "🐞",
+    },
+    {
+      name: ["set", "config"],
+      description: "Paramétrer le CLI",
+      icon: "⚙️",
+    },
+    {
+      name: ["upload", "u", "up"],
+      description: "Upload un fichier",
+      icon: "📤",
+      args: {
+        template: "filepaths",
+      },
+    },
+    {
+      name: "uninstall",
+      description: "Désinstalle le CLI",
+      icon: "🚮",
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Afficher l'aide pour freetransCLI",
+    },
+  ],
+};
+export default completionSpec;
