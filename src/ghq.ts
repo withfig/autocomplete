55401d3020631e3cf7948d2b984a671b6200495f
t3yamoto <3yamoto.dev@gmail.com>
2023-03-25 20:50:26 +0900

    feat(ghq): implement subcommand 'list'

--- src/ghq.ts
+++ src/ghq.ts
@@ -2,6 +2,37 @@ const completionSpec: Fig.Spec = {
   name: "ghq",
   description:
     "'ghq' provides a way to organize remote repository clones, like go get does. When you clone a remote repository by ghq get, ghq makes a directory under a specific root directory (by default ~/ghq) using the remote repository URL’s host and path",
+  subcommands: [
+    {
+      name: "list",
+      description: "List local repositories",
+      options: [
+        {
+          name: ["-e", "--exact"],
+          description: "Perform an exact match (default: false)",
+        },
+        {
+          name: "--vcs",
+          description: "Specify vcs backend for matching",
+          args: { name: "vcs" },
+        },
+        {
+          name: ["-p", "--full-path"],
+          description: "Print full paths (default: false)",
+          exclusiveOn: ["--unique"],
+        },
+        {
+          name: "--unique",
+          description: "Print unique subpaths (default: false)",
+          exclusiveOn: ["--full-path"],
+        },
+      ],
+      args: {
+        name: "query",
+        isOptional: true,
+      },
+    },
+  ],
   options: [
     {
       name: ["-h", "--help", "h", "help"],
