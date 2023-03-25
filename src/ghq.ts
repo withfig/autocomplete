fe3f1985a1d98e9d3e24a581bf172d90dc5ad7f2
t3yamoto <3yamoto.dev@gmail.com>
2023-03-25 15:04:58 +0900

    feat: add ghq completion spec

---
+++ src/ghq.ts
@@ -0,0 +1,6 @@
+const completionSpec: Fig.Spec = {
+  name: "ghq",
+  description:
+    "'ghq' provides a way to organize remote repository clones, like go get does. When you clone a remote repository by ghq get, ghq makes a directory under a specific root directory (by default ~/ghq) using the remote repository URL’s host and path",
+};
+export default completionSpec;
