730c7b04eba22833a9cfbf20ce34fc436ee11c94
t3yamoto <3yamoto.dev@gmail.com>
2023-03-25 15:52:23 +0900

    feat(ghq): implement global options

--- src/ghq.ts
+++ src/ghq.ts
@@ -2,5 +2,18 @@ const completionSpec: Fig.Spec = {
   name: "ghq",
   description:
     "'ghq' provides a way to organize remote repository clones, like go get does. When you clone a remote repository by ghq get, ghq makes a directory under a specific root directory (by default ~/ghq) using the remote repository URL’s host and path",
+  options: [
+    {
+      name: ["-h", "--help", "h", "help"],
+      description: "Show help",
+      isPersistent: true,
+      exclusiveOn: ["--version"],
+    },
+    {
+      name: ["-v", "--version"],
+      description: "Print the version",
+      exclusiveOn: ["--help"],
+    },
+  ],
 };
 export default completionSpec;
