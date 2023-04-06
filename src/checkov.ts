45c7616ca91fbfe13c3d507062a82759e1acc337
Taylor <28880387+tsmithv11@users.noreply.github.com>
2023-04-06 10:25:11 -0700

    Update checkov for more options

--- src/checkov.ts
+++ src/checkov.ts
@@ -360,6 +360,199 @@ const completionSpec: Fig.Spec = {
         name: "SKIP_CVE_PACKAGE",
       },
     },
+    {
+      name: "--use-enforcement-rules",
+      description:
+        "Use the Enforcement rules configured in the platform for hard / soft fail logic.",
+      isRepeatable: false,
+      args: {
+        name: "USE_ENFORCEMENT_RULES",
+      },
+    },
+    {
+      name: "--support",
+      description:
+        "Enable debug logs and upload the logs to the server.",
+      isRepeatable: false,
+      dependsOn: ["--bc-api-key"],
+      args: {
+        name: "SUPPORT",
+      },
+    },
+    {
+      name: "--summary-position",
+      description:
+        "Chose whether the summary will be appended on top or on bottom",
+      isRepeatable: false,
+      args: {
+        name: "SUMMARY_POSITION",
+        suggestions: ["top", "bottom"],
+      },
+    },
+    {
+      name: "--skip-resources-without-violations",
+      description:
+        "Exclude extra resources (resources without violations).",
+      isRepeatable: false,
+      args: {
+        name: "SKIP_RESOURCES_WITHOUT_VIOLATIONS",
+      },
+    },
+    {
+      name: "--summary-position",
+      description:
+        "Chose whether the summary will be appended on top or on bottom",
+      isRepeatable: false,
+      args: {
+        name: "SUMMARY_POSITION",
+        suggestions: ["top","bottom"],
+      },
+    },
+    {
+      name: "--skip-resources-without-violations",
+      description:
+        "Exclude extra resources (resources without violations).",
+      isRepeatable: false,
+      args: {
+        name: "SKIP_RESOURCES_WITHOUT_VIOLATIONS",
+      },
+    },
+    {
+      name: "--skip-download",
+      description:
+        "Do not download any data from Prisma Cloud.",
+      isRepeatable: false,
+      args: {
+        name: "SKIP_DOWNLOAD",
+      },
+    },
+    {
+      name: "--secrets-history-timeout",
+      description:
+        "Maximum time to run the history scan.",
+      isRepeatable: false,
+      args: {
+        name: "SECRETS_HISTORY_TIMEOUT",
+      },
+    },
+    {
+      name: "--scan-secrets-history",
+      description:
+        "Will scan the history of commits for secrets.",
+      isRepeatable: false,
+      args: {
+        name: "SCAN_SECRETS_HISTORY",
+      },
+    },
+    {
+      name: "--prisma-api-url",
+      description:
+        "The Prisma Cloud API URL",
+      isRepeatable: false,
+      args: {
+        name: "PRISMA_API_URL",
+      },
+    },
+    {
+      name: "--policy-metadata-filter",
+      description:
+        "Comma separated key:value string to filter policies based on Prisma Cloud policy metadata.",
+      isRepeatable: false,
+      args: {
+        name: "POLICY_METADATA_FILTER",
+      },
+    },
+    {
+      name: "--output-file-path",
+      description:
+        "Name of the output folder to save the chosen output formats.",
+      isRepeatable: false,
+      args: {
+        name: "OUTPUT_FILE_PATH",
+      },
+    },
+    {
+      name: "--output-baseline-as-skipped",
+      description:
+        "Output checks that are skipped due to baseline file presence",
+      isRepeatable: false,
+      args: {
+        name: "OUTPUT_BASELINE_AS_SKIPPED",
+      },
+    },
+    {
+      name: "--openai-api-key",
+      description:
+        "Add an OpenAI API key to enhance finding guidelines. This will send Code to OpenAI.",
+      isRepeatable: false,
+      args: {
+        name: "OPENAI_API_KEY",
+      },
+    },
+    {
+      name: "--no-fail-on-crash",
+      description:
+        "Return exit code 0 instead of 2.",
+      isRepeatable: false,
+      args: {
+        name: "NO_FAIL_ON_CRASH",
+      },
+    },
+    {
+      name: "--mask",
+      description:
+        "Each entry in the list will be used for masking the desired attribute.",
+      isRepeatable: false,
+      args: {
+        name: "MASK",
+      },
+    },
+    {
+      name: "--include-all-checkov-policies",
+      description:
+        "When running with an API key, Checkov will omit any policies that do not exist in the Bridgecrew or Prisma Cloud platform.",
+      isRepeatable: false,
+      args: {
+        name: "INCLUDE_ALL_CHECKOV_POLICIES",
+      },
+    },
+    {
+      name: "--external-checks-git",
+      description:
+        "GitHub URL of external checks to be added.",
+      isRepeatable: false,
+      args: {
+        name: "EXTERNAL_CHECKS_GIT",
+      },
+    },
+    {
+      name: "--enable-secret-scan-all-files",
+      description:
+        "Enable secret scan for all files.",
+      isRepeatable: false,
+      args: {
+        name: "ENABLE_SECRET_SCAN_ALL_FILES",
+      },
+    },
+    {
+      name: "--deep-analysis",
+      description:
+        "Enable combine TF graph and TF Plan graph.",
+      isRepeatable: false,
+      args: {
+        name: "DEEP_ANALYSIS",
+      },
+    },
+    {
+      name: "--block-list-secret-scan",
+      description:
+        "List of files to filter out from the secret scanner.",
+      isRepeatable: false,
+      dependsOn: ["--repo-root-for-plan-enrichment"],
+      args: {
+        name: "BLOCK_LIST_SECRET_SCAN",
+      },
+    },
   ],
   // Only uncomment if checkov takes an argument
   // args: {}
