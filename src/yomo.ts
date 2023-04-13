b4b51809be0d8e21a2b07eacc8a2cad0ddfd948a
C.C <fanweixiao@gmail.com>
2023-04-13 21:22:39 +0800

    feat: add yomo completion spec

---
+++ src/yomo.ts
@@ -0,0 +1,73 @@
+const completionSpec: Fig.Spec = {
+  name: "yomo",
+  description: "CLI interface for YoMo",
+  subcommands: [
+    {
+      name: "init",
+      description: "Initial an example StreamFunction",
+      args: {
+        name: "function name",
+        description: "StreamFunction name to initialize locally",
+      },
+      options: [
+        {
+          name: "--rx",
+          description: "Generate Rx code template",
+        },
+      ],
+    },
+    {
+      name: "build",
+      description: "Build a StreamFunction to WebAssembly",
+      args: {
+        name: ".go file",
+        description: "The .go file to build",
+      },
+      options: [
+        {
+          name: "--target",
+          description: "Build to wasm or binary",
+          args: [
+            {
+              name: "wasm",
+            },
+            {
+              name: "binary",
+            },
+          ],
+        },
+        {
+          name: ["-m", "--modfile"],
+          description: "Custom go.mod filepath",
+        },
+      ],
+    },
+    {
+      name: "run",
+      description: "Build a StreamFunction to WebAssembly",
+      args: {
+        name: ".wasm file",
+        description: "The .wasm file to run",
+      },
+      options: [
+        {
+          name: ["-z", "--zipper"],
+          description: "Zipper endpoint this StreamFunction will connect to",
+        },
+        {
+          name: ["-n", "--name"],
+          description: "Specify the name of this StreamFunction",
+        },
+      ],
+    },
+  ],
+  options: [
+    {
+      name: ["--help", "-h"],
+      description: "Show help for yomo",
+    },
+  ],
+  // Only uncomment if yomo takes an argument
+  // args: {}
+};
+export default completionSpec;
