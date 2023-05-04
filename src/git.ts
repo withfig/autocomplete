533c7d892c1bed05765fca9262d8af120ecd480a
grant0417 <grantgurvis@gmail.com>
2023-05-03 20:09:58 -0700

    Update "@fig/autocomplete-generators" to version "^2.2.2" and improve the "git" generator - now it has a better prompt message and a fixed template addition.

--- src/git.ts
+++ src/git.ts
@@ -1,3 +1,5 @@
+import { ai } from "@fig/autocomplete-generators";
+
 const filterMessages = (out: string): string => {
   return out.startsWith("warning:") || out.startsWith("error:")
     ? out.split("\n").slice(1).join("\n")
@@ -4341,51 +4343,28 @@ const completionSpec: Fig.Spec = {
       options: [
         {
           name: ["-m", "--message"],
-          insertValue: "-m '{cursor}'",
+          // insertValue: "-m '{cursor}'",
           description: "Use the given message as the commit message",
           args: {
             name: "message",
-            generators: {
-              custom: async (_, executeShellCommand) => {
-                const diff = await executeShellCommand("git diff --staged");
-
-                if (diff.length === 0) {
-                  return [];
-                }
-
-                const body = {
-                  model: "gpt-3.5-turbo",
-                  messages: [
-                    {
-                      role: "system",
-                      content:
-                        'Generate a git commit message summary based on this git diff, the "summary" must be no more than 70-75 characters, and it must describe both what the patch changes, as well as why the patch might be necessary.  It is challenging to be both succinct and descriptive, but that is what a well-written summary should do.',
-                    },
-                    {
-                      role: "user",
-                      content: diff,
-                    },
-                  ],
-                };
-
-                const bodyJson = JSON.stringify(body);
-                const escapedBodyJson = bodyJson.replace(/'/g, "'\"'\"'");
-
-                const res = await executeShellCommand(
-                  `fig _ request --route /ai/chat --method POST --body '${escapedBodyJson}'`
+            generators: ai({
+              prompt: ({ executeShellCommand }) => {
+                const gitLogShortMessages = executeShellCommand(
+                  "git log --pretty=format:%s --abbrev-commit --max-count=10"
                 );
 
-                const json = JSON.parse(res);
-
-                return json.choices
-                  .map((c) => c?.message?.content)
-                  .filter(Boolean)
-                  .map((c) => ({
-                    icon: "🪄",
-                    name: c,
-                  }));
+                return (
+                  'Generate a git commit message summary based on this git diff, the "summary" must be no more ' +
+                  "than 70-75 characters, and it must describe both what the patch changes, as well as why the " +
+                  "patch might be necessary. It is challenging to be both succinct and descriptive, but that is " +
+                  "what a well-written summary should do.\n\n" +
+                  "EXAMPLE:\n" +
+                  gitLogShortMessages
+                );
               },
-            },
+              message: ({ executeShellCommand }) =>
+                executeShellCommand("git diff --staged"),
+            }),
           },
         },
         {
@@ -9733,7 +9712,7 @@ const completionSpec: Fig.Spec = {
     {
       name: "commit -m 'msg'",
       description: "Git commit shortcut",
-      insertValue: "commit -m '{cursor}'",
+      insertValue: "commit -m {cursor}",
       icon: "fig://template?color=2ecc71&badge=🔥",
       // type: "shortcut",
     },
