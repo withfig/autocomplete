3da16b314766f9a8b0d62e7dd0457dde05924c4e
t3yamoto <38431658+t3yamoto@users.noreply.github.com>
2023-05-11 03:33:01 +0900

    feat(cdk): add some subcommand specs (#1954)

--- src/cdk.ts
+++ src/cdk.ts
@@ -25,11 +25,35 @@ const completionSpec: Fig.Spec = {
       description: "Deploys the CDK toolkit stack into an AWS environment",
     },
     {
-      name: "synth",
+      name: ["synth", "synthesize"],
       description:
         "Synthesizes and prints the CloudFormation template for this stack",
     },
-    { name: "ls", description: "List all stacks in the app" },
+    { name: ["ls", "list"], description: "List all stacks in the app" },
+    {
+      name: "import",
+      description: "Import existing resource(s) into the given STACK",
+    },
+    {
+      name: "watch",
+      description: "Shortcut for 'deploy --watch'",
+    },
+    {
+      name: ["ack", "acknowledge"],
+      description: "Acknowledge a notice so that it does not show up anymore",
+    },
+    {
+      name: "notices",
+      description: "Returns a list of relevant notices",
+    },
+    {
+      name: "context",
+      description: "Manage cached context values",
+    },
+    {
+      name: ["doc", "docs"],
+      description: "Opens the reference documentation in a browser",
+    },
   ],
   options: [
     {
