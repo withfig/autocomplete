a8744df34bd37428c3c6f86a34beba2e06e1e1f4
Gautam Arora <gautamarora6248@gmail.com>
2023-05-17 23:59:59 +0530

    feat(cd): add suggestion to home directory (#1977)
    Co-authored-by: Federico Ciardi <fed.ciardi@gmail.com>

--- src/cd.ts
+++ src/cd.ts
@@ -18,6 +18,10 @@ const completionSpec: Fig.Spec = {
         description: "Switch to the last used folder",
         hidden: true,
       },
+      {
+        name: "~",
+        description: "Switch to the home directory",
+      },
     ],
   },
 };
