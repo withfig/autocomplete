455ab5f1f2cbd3f4f1d12e626249a87cdcb082ca
Duan Chen <duanchen2004@gmail.com>
2023-10-20 14:50:32 -0700

    Added pijul apply

--- src/pijul.ts
+++ src/pijul.ts
@@ -12,7 +12,11 @@ const completionSpec: Fig.Spec = {
       // }],
       name: "add",
       description:
-        "Adds a path to the tree. Pijul has an internal tree to represent the files currently tracked. This command adds files and directories to that tree.",
+        "Adds a path to the tree. Pijul has an internal tree to represent the files currently tracked. This command adds files and directories to that tree",
+      args: {
+        name: "<PATHS>...",
+        description: "Paths to add to the internal tree",
+      },
       options: [
         {
           name: ["-f", "--force"],
@@ -23,6 +27,35 @@ const completionSpec: Fig.Spec = {
         },
         {
           name: ["-r", "--recursive"],
+        },
+      ],
+    },
+    {
+      name: "apply",
+      description: "Applies changes to a channel",
+      args: {
+        name: "<CHANGE>...",
+        description:
+          "The change that need to be applied. If this value is missing, read the change in text format on the standard input",
+      },
+      options: [
+        {
+          name: "--channel <CHANNEL>",
+          description: "--deps-only",
+        },
+        {
+          name: "--deps-only",
+          description:
+            "Only apply the dependencies of the change, not the change itself. Only applicable for a single change",
+        },
+        {
+          name: ["-h", "--help"],
+          description: "Print help information",
+        },
+        {
+          name: "--repository <REPO_PATH>",
+          description:
+            "Set the repository where this command should run. Defaults to the first ancestor of the current directory that contains a .pijul directory",
         },
       ],
     },
