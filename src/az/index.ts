5f9920f2b5c74b2a6883a5ed33c6f4da3fb41c60
Grant Gurvis <grangurv@amazon.com>
2023-11-30 14:08:02 -0800

    Fix small issues

--- src/az/index.ts
+++ src/az/index.ts
@@ -2,5 +2,13 @@ import { createVersionedSpec } from "@fig/autocomplete-helpers";
 const versionFiles = ["2.53.0"];
 export const getVersionCommand: Fig.GetVersionCommand = async (
   executeShellCommand
-) => JSON.parse(await executeShellCommand("az version -o json"))["azure-cli"];
+) =>
+  JSON.parse(
+    (
+      await executeShellCommand({
+        command: "az",
+        args: ["version", "-o", "json"],
+      })
+    ).stdout
+  )["azure-cli"];
 export default createVersionedSpec("az", versionFiles);
