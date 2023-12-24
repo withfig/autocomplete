bfe57caec236c398822aef0cd36f8e0ec43a11ba
Fran Zekan <zekan.fran369@gmail.com>
2023-12-24 22:51:49 +0100

    Add more missing commands

--- src/pnpm.ts
+++ src/pnpm.ts
@@ -462,6 +462,14 @@ This is similar to yarn unlink, except pnpm re-installs the dependency after rem
     },
     description: `Generate a patch out of a directory`,
   },
+  {
+    name: "patch-remove",
+    args: {
+      name: "package",
+      isVariadic: true,
+      // TODO: would be nice to have a generator of all patched packages
+    },
+  },
 ];
 
 const SUBCOMMANDS_RUN_SCRIPTS: Fig.Subcommand[] = [
@@ -547,8 +555,14 @@ Details at: https://pnpm.io/cli/audit`,
         description: `Only print advisories with severity greater than or equal to <severity>`,
         args: {
           name: "Audit Level",
+          default: "low",
           suggestions: ["low", "moderate", "high", "critical"],
         },
+      },
+      {
+        name: "--fix",
+        description:
+          "Add overrides to the package.json file in order to force non-vulnerable versions of the dependencies",
       },
       {
         name: "--json",
