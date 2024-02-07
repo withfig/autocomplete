1e7c4ba6b96f28c63882796c88c73ebae00566f4
Yaron Uliel <5229872+yaronuliel@users.noreply.github.com>
2024-02-07 10:12:30 +0200

    [aws] Update list of AWS regions, and move to centralized file

--- src/aws/s3.ts
+++ src/aws/s3.ts
@@ -1,27 +1,5 @@
 // TODO: suggest available s3 endpoints
-const awsRegions = [
-  "af-south-1",
-  "eu-north-1",
-  "ap-south-1",
-  "eu-west-3",
-  "eu-west-2",
-  "eu-south-1",
-  "eu-west-1",
-  "ap-northeast-3",
-  "ap-northeast-2",
-  "me-south-1",
-  "ap-northeast-1",
-  "sa-east-1",
-  "ca-central-1",
-  "ap-east-1",
-  "ap-southeast-1",
-  "ap-southeast-2",
-  "eu-central-1",
-  "us-east-1",
-  "us-east-2",
-  "us-west-1",
-  "us-west-2",
-];
+import awsRegions from "./regions";
 
 const storageClasses = [
   "STANDARD",
