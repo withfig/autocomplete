5cffdfd148d7c71003ddc45f69aacdf9e8879b57
Tim Wanders <tim@clickwire.io>
2024-07-30 09:28:23 +0200

    fix promise resolving

--- src/php.ts
+++ src/php.ts
@@ -17,24 +17,24 @@ const completionSpec: Fig.Spec = {
     const subcommands = [];
 
     await Promise.all([
-      async () => {
+      (async () => {
         if (await fileExists(executeShellCommand, "artisan")) {
           subcommands.push({ name: "artisan", loadSpec: "php/artisan" });
         }
-      },
-      async () => {
+      })(),
+      (async () => {
         if (await fileExists(executeShellCommand, "please")) {
           subcommands.push({ name: "please", loadSpec: "php/please" });
         }
-      },
-      async () => {
-        if (await fileExists(executeShellCommand, "bin/console")) {
+      })(),
+      (async () => {
+        if (await fileExists(executeShellCommand, "anotherCommand")) {
           subcommands.push({
-            name: "bin/console",
-            loadSpec: "php/bin-console",
+            name: "anotherCommand",
+            loadSpec: "php/anotherCommand",
           });
         }
-      },
+      })(),
     ]);
 
     return {
