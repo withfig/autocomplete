814deda82ea7a8a6b5fcdb81cf003d531046e90f
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-08-07 20:41:53 +0530

    feat(networksetup): add list subcommands

---
+++ src/networksetup.ts
@@ -0,0 +1,23 @@
+const completionSpec: Fig.Spec = {
+  name: "networksetup",
+  description: "Configuration tool for network settings in System Preferences",
+  subcommands: [
+    {
+      name: "-listnetworkserviceorder",
+      description:
+        "Displays a list of network services in the order they are contacted for a connection, along with the corresponding port and device for each",
+    },
+    {
+      name: "-listallnetworkservices",
+      description:
+        "Displays a list of all the network services on the server's hardware ports",
+    },
+    {
+      name: "-listallhardwareports",
+      description:
+        "Displays list of hardware ports with corresponding device name and ethernet address",
+    },
+  ],
+};
+
+export default completionSpec;
