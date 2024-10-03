06cef0ba49caee675ad3c016011a92eff4e6fa2c
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-10-03 23:16:08 +0530

    feat(kotlinc): add kotlin spec jvm compiler options

--- src/kotlinc.ts
+++ src/kotlinc.ts
@@ -85,6 +85,81 @@ const completionSpec: Fig.Spec = {
         name: "annotation",
       },
     },
+    {
+      name: ["-classpath", "-cp"],
+      description:
+        "Search for class files in the specified paths. Separate elements of the classpath with system path separators (; on Windows, : on macOS/Linux). The classpath can contain file and directory paths, ZIP, or JAR files",
+      args: {
+        name: "path",
+        template: "filepaths",
+      },
+    },
+    {
+      name: "-d",
+      description:
+        "Place the generated class files into the specified location. The location can be a directory, a ZIP, or a JAR file",
+      args: {
+        name: "path",
+        template: "filepaths",
+      },
+    },
+    {
+      name: "-include-runtime",
+      description:
+        "Include the Kotlin runtime into the resulting JAR file. Makes the resulting archive runnable on any Java-enabled environment",
+    },
+    {
+      name: "-jdk-home",
+      description:
+        "Use a custom JDK home directory to include into the classpath if it differs from the default JAVA_HOME",
+      args: {
+        name: "path",
+        template: "folders",
+      },
+    },
+    {
+      name: "-jvm-target",
+      description:
+        "Specify the target version of the generated JVM bytecode. Possible values are 1.8, 9, 10, ..., 21. The default value is 1.8",
+      args: {
+        name: "version",
+      },
+    },
+    {
+      name: "-java-parameters",
+      description:
+        "Generate metadata for Java 1.8 reflection on method parameters",
+    },
+    {
+      name: "-module-name",
+      description: "Set a custom name for the generated .kotlin_module file",
+      args: {
+        name: "name",
+      },
+    },
+    {
+      name: "-no-jdk",
+      description:
+        "Don't automatically include the Java runtime into the classpath",
+    },
+    {
+      name: "-no-reflect",
+      description:
+        "Don't automatically include the Kotlin reflection (kotlin-reflect.jar) into the classpath",
+    },
+    {
+      name: "-no-stdlib",
+      description:
+        "Don't automatically include the Kotlin/JVM stdlib (kotlin-stdlib.jar) and Kotlin reflection (kotlin-reflect.jar) into the classpath",
+    },
+    {
+      name: "-script-templates",
+      description:
+        "Script definition template classes. Use fully qualified class names and separate them with commas (,)",
+      args: {
+        name: "classnames[,]",
+      },
+    },
   ],
 };
 
