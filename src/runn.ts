e3e43fdec350f75209b7749b46785af56513bce7
nacal <nacal@pepabo.com>
2025-02-15 23:21:49 +0900

    add "coverage" subcommand

--- src/runn.ts
+++ src/runn.ts
@@ -52,6 +52,161 @@ const completionSpec: Fig.Spec = {
         },
       ],
     },
+    {
+      name: "coverage",
+      description:
+        "Show coverage for paths/operations of OpenAPI spec and methods of protocol buffers",
+      options: [
+        {
+          name: "--cache-dir",
+          description: "Specify cache directory for remote runbooks",
+          args: {
+            template: "folders",
+          },
+        },
+        {
+          name: "--debug",
+          description: "Debug",
+        },
+        {
+          name: "--env-file",
+          description: "Load environment variables from a file",
+          args: {
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--format",
+          description: "Format of result output",
+          args: {
+            suggestions: ["json", "none"],
+          },
+        },
+        {
+          name: "--grpc-buf-config",
+          description: "Set the path to buf.yaml for gRPC runners",
+          args: {
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--grpc-buf-dir",
+          description: "Set the path to the buf directory for gRPC runners",
+          args: {
+            template: "folders",
+          },
+        },
+        {
+          name: "--grpc-buf-lock",
+          description: "Set the path to buf.lock for gRPC runners",
+          args: {
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--grpc-buf-module",
+          description:
+            'Set the buf modules for gRPC runners ("buf.build/owner/repository" or "buf.build/owner/repository/tree/branch-or-commit")',
+          args: {
+            name: "buf modules",
+          },
+        },
+        {
+          name: "--grpc-import-path",
+          description:
+            "Set the path to the directory where proto sources can be imported for gRPC runners",
+          args: {
+            template: "filepaths",
+          },
+        },
+        {
+          name: "--grpc-no-tls",
+          description: "Disable TLS use in all gRPC runners",
+        },
+        {
+          name: "--grpc-proto",
+          description: "Set the name of proto source for gRPC runners",
+          args: {
+            name: "proto source",
+          },
+        },
+        {
+          name: ["-h", "--help"],
+          description: "Help for coverage",
+        },
+        {
+          name: "--http-openapi3",
+          description:
+            'Set the path to the OpenAPI v3 document for HTTP runners ("path/to/spec.yml" or "key:path/to/spec.yml")',
+          args: {
+            name: "filepaths",
+          },
+        },
+        {
+          name: "--id",
+          description:
+            "Run the matching runbooks in order if there is only one runbook with a forward matching ID",
+          args: {
+            name: "id",
+          },
+        },
+        {
+          name: "--label",
+          description: "Run all runbooks matching the label specification",
+          args: {
+            name: "label",
+          },
+        },
+        {
+          name: ["-l", "-long"],
+          description: "Long format",
+        },
+        {
+          name: "--overlay",
+          description: "Overlay values on the runbook",
+          args: {
+            name: "values",
+          },
+        },
+        {
+          name: "--retain-cache-dir",
+          description: "Retain cache directory for remote runbooks",
+        },
+        {
+          name: "--run",
+          description:
+            "Run all runbooks with a matching file path, treating the value passed to the option as an unanchored regular expression",
+          args: {
+            name: "path regex",
+          },
+        },
+        {
+          name: "--runner",
+          description: 'Set runner to runbook("key:dsn")',
+          args: {
+            name: "runner",
+          },
+        },
+        {
+          name: "--skip-included",
+          description: "Skip running the included runbook by itself",
+        },
+        {
+          name: "--underlay",
+          description: "Lay values under the runbook",
+          args: {
+            name: "values",
+          },
+        },
+        {
+          name: "--var",
+          description: 'Set var to runbook("key:value")',
+          args: {
+            name: "value",
+          },
+        },
+      ],
+    },
   ],
   options: [
     {
